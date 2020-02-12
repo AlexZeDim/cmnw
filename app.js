548de450daa2284bb9d9a4bbe1c4e7686b9ab035
Boralus Whale <alexzedim@mail.ru>
2020-02-12 18:04:21 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+//const players = require('./routes/api/players');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+//app.use('/api/players', players);
+
+module.exports = app;

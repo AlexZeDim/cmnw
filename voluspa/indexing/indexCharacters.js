1e219e6f36cfd3bad694c19b6d6027ed9849321e
Boralus Whale <alexzedim@mail.ru>
2020-02-14 10:31:23 +0300

    pre-cursor

--- voluspa/indexing/indexCharacters.js
+++ voluspa/indexing/indexCharacters.js
@@ -1,17 +1,31 @@
 const characters_db = require("../../db/characters_db");
 const getCharacter = require('../getCharacter');
 
-let myCursor = characters_db.find({}).cursor({batchSize:50});
+let myCursor = characters_db.find({}).lean().cursor({batchSize:50});
 
 myCursor.eachAsync( async (player) => {
     try {
-        let character = await getCharacter(player.realm, player.name.toLowerCase());
+        let character = await getCharacter(player.realm, player.name.toLowerCase(), void 0);
+        let { _id } = character;
         if (character.hasOwnProperty('guild')) {
-            character.guild_history = { _id: character.guild, rank: character.guild_rank }
+            //TODO we are not sure
+            character.guild_history = { _id: character.guild }
         }
         character.source = 'VOLUSPA-index';
-        //console.log(character)
+        await characters_db.findByIdAndUpdate(
+            {
+                _id: _id
+            },
+            character,
+            {
+                upsert : true,
+                new: true,
+                setDefaultsOnInsert: true,
+                lean: true
+            }
+        ).exec();
+        console.log(character)
     } catch (e) {
-        console.log('error')
+        console.log(e)
     }
 });

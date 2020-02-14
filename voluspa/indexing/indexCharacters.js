170b8eb89f8643d80542b8a4aa52d00a3a46b78a
Boralus Whale <alexzedim@mail.ru>
2020-02-14 10:59:16 +0300

    cursor

--- voluspa/indexing/indexCharacters.js
+++ voluspa/indexing/indexCharacters.js
@@ -1,8 +1,33 @@
 const characters_db = require("../../db/characters_db");
+const guilds_db = require("../../db/guilds_db");
 const getCharacter = require('../getCharacter');
 
-let myCursor = characters_db.find({}).lean().cursor({batchSize:50});
+async function indexCharacters () {
+    try {
+        const allPlayers = [];
+        const cursor = guilds_db.find({}).lean().cursor({batchSize: 50});
+        cursor.on('data', (characterData) => {
+            allPlayers.push(characterData);
+            console.log(allPlayers.length);
+        });
+        cursor.on('error', error => {
+            console.error(error);
+            cursor.close();
+        });
+        cursor.on('close', () => {
+            console.log('closing...');
+            cursor.close();
+        });
+    } catch (err) {
+        console.error(`${indexCharacters.name},${err}`);
+    }
+}
 
+indexCharacters();
+
+//let myCursor = characters_db.find({}).lean().cursor({batchSize:50});
+
+/*
 myCursor.eachAsync( async (player) => {
     try {
         let character = await getCharacter(player.realm, player.name.toLowerCase(), void 0);
@@ -28,4 +53,4 @@ myCursor.eachAsync( async (player) => {
     } catch (e) {
         console.log(e)
     }
-});
+});*/

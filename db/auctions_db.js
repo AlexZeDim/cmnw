46fa6f64ac9a27e002cb3d7bfa6387890a29a270
Boralus Whale <alexzedim@mail.ru>
2020-02-28 10:26:51 +0300

    db indexing

---
+++ db/auctions_db.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    bufferMaxEntries: 0,
+    retryWrites: true,
+    w: "majority",
+    family: 4
+});
+
+mongoose.Promise = global.Promise;
+
+let schema = new mongoose.Schema({
+    id: {
+        type: Number
+    },
+    item: {
+        type: Object
+    },
+    connected_realm_id: {
+        type: Number
+    },
+    quantity: {
+        type: Number
+    },
+    bid: {
+        type: Number
+    },
+    buyout: {
+        type: Number
+    },
+    unit_price: {
+        type: Number
+    },
+    timeLeft: {
+        type: String
+    },
+    lastModified: {
+        type: Number
+    }
+},{
+    timestamps: true
+});
+
+let auctions_db = mongoose.model('auctions', schema);
+
+module.exports = auctions_db;

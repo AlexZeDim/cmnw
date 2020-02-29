ae847ca83a6450f5305fad2f735cc135860b5af9
Sasha <AlexZeDim@mail.ru>
2020-02-29 12:43:32 +0300

    VOLUSPA orchestation (#3)
    * getCharacter
    
    * 6s delay
    
    * promise.all
    
    * pre-cursor
    
    * cursor
    
    * VOLUSPA-logs
    
    * wip promise.all
    
    * wip promise.all v1
    
    * promise.all finally
    
    * realms_db
    
    * indexRealms
    
    * indexGuilds.js
    
    * dd//mm/yy
    
    * almost fucking done
    
    * indexGuilds
    
    * starting orchestra
    
    * orchestra
    
    * orchestration
    
    * package.json

---
+++ ecosystem.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  apps : [{
+    name: "getToken",
+    script: "./bin/getToken.js",
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '100M',
+    cron_restart: '* * * * *',
+    time: true
+  },{
+    name: 'indexCharacters',
+    script: './voluspa/indexing/indexCharacters.js',
+    //args: '--b 12',
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    cron_restart: '0 13,01 * * *',
+    time: true
+  },{
+    name: "indexGuilds",
+    script: "./voluspa/indexing/indexGuilds.js",
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    cron_restart: '0 */2 * * *',
+    time: true
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};

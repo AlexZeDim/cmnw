5b9414cb4ed9b9c950df053e11c50935177a8367
Boralus Whale <alexzedim@mail.ru>
2020-02-29 18:20:17 +0300

    new format for token

--- bin/getTokens.js
+++ bin/getTokens.js
@@ -8,8 +8,8 @@ async function getTokens () {
             const {access_token, expires_in} = await axios.get(`https://eu.battle.net/oauth/token?grant_type=client_credentials&client_id=${auth._id}&client_secret=${auth.secret}`).then(res => {
                 return res.data;
             });
-            await keys_db.updateOne({_id: auth._id},{token: access_token, expired_in: expires_in});
-            console.info(`${getTokens.name},U,${auth._id},${expires_in}`);
+            let token = await keys_db.updateOne({_id: auth._id},{token: access_token, expired_in: expires_in});
+            if (token) console.info(`U,${getTokens.name},${token._id}`);
         }
     } catch (e) {
         console.error(e);

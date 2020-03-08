e28811f9e6f4b0cb746a980891cfc41cdcbe301a
Boralus Whale <alexzedim@mail.ru>
2020-03-09 02:41:58 +0300

    initial commit

---
+++ discord/bluratella.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const fs = require('fs');
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('message', async message => {
+    if (message.author.bot) return;
+    let command = message.content.split(/(?<=^\S+)\s/)[0];
+    let args = message.content.split(/(?<=^\S+)\s/)[1];
+    if (!client.commands.has(command)) return;
+    try {
+        client.commands.get(command).execute(message, args, client);
+    } catch (error) {
+        console.error(error);
+        await message.reply('there was an error trying to execute that command!');
+    }
+});
+
+client.login(process.env.token);

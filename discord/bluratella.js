324597726602967b561bcc7a953a28901f1ed5fc
Boralus Whale <alexzedim@mail.ru>
2020-03-09 14:08:14 +0300

    test area konstantin path fix

--- discord/bluratella.js
+++ discord/bluratella.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
+const path = require('path');
 const fs = require('fs');
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(path.normalize(`${__dirname}\\commands\\`)).filter(file => file.endsWith('.js'));
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    const command = require(path.normalize(`${__dirname}\\commands\\${file}`));
     client.commands.set(command.name, command);
 }
 

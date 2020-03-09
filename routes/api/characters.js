3b63527126a6603ebf118ac2ee641a785b996c8d
Boralus Whale <alexzedim@mail.ru>
2020-03-09 13:19:40 +0300

    api point

---
+++ routes/api/characters.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const realms_db = require("../../db/realms_db");
+const characters_db = require("../../db/characters_db");
+
+router.get('/:name@:realm', async function(req, res) {
+    try {
+        let { slug } = await realms_db.findOne({$or: [
+                { 'name': req.params.realm },
+                { 'slug': req.params.realm },
+                { 'name_locale': req.params.realm },
+                { 'ticker': req.params.realm },
+                { '_id': req.params.realm },
+            ]});
+        let characterData = characters_db.findById(`${req.params.name}@${slug}`);
+        console.log(characterData);
+        /***
+         * TODO checkDB
+         * TODO if not res
+         * TODO then send req
+         * TODO return result
+         */
+        //let data = await checkPlayer(req.params.name, req.params.realm);
+        res.status(200).json(characterData);
+    } catch (e) {
+        res.status(404).json(e);
+    }
+});
+
+module.exports = router;

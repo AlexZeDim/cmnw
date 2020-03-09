b4886db14a558cc6827775b3e614109ada0abfba
Boralus Whale <alexzedim@mail.ru>
2020-03-09 21:48:33 +0300

    TODO commands

---
+++ discord/commands/eva.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'eva',
+    description: 'This command will find evaluate item price',
+    execute(message) {
+        message.channel.send(`This command will find evaluate item price`);
+    },
+};

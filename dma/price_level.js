9bf938ead5b99e5315f5d2c03c8f26b2bcc6847a
Boralus Whale <alexzedim@mail.ru>
2020-03-15 02:34:23 +0300

    Sample Variance evaluation

--- dma/price_level.js
+++ dma/price_level.js
@@ -2,12 +2,12 @@ const auctions_db = require("../db/auctions_db");
 
 async function price_level () {
     try {
-        let priceArray = [];
         let counter = 0;
-        let docs = await auctions_db.distinct('unit_price', { "item.id": 152512}).lean();
+        let docs = await auctions_db.distinct('unit_price', { "item.id": 152512, connected_realm_id: 1602}).lean();
         console.log(docs);
         for (let i = 0; i < docs.length; i++) {
-            counter += docs[i];
+            console.log(docs[i],(1/docs.length*(docs[i]*docs[i]))-((1/docs.length*docs[i])*(1/docs.length*docs[i])));
+            /*counter += docs[i];
             if (i > Math.round(docs.length*0.85)-1) {
                 if ((counter-docs[i])/docs[i-1] > (counter/docs[i])) {
                     console.info(`Tail strike: ${docs[i]} previous: ${docs[i-1]}`)
@@ -16,9 +16,8 @@ async function price_level () {
                 }
             } else {
                 priceArray.push(docs[i])
-            }
+            }*/
         }
-        console.log(priceArray)
     } catch (err) {
         console.log(err);
     }

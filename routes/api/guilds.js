9418857ea8c6381468ab5bba4e6d24ddff34998a
Sasha <AlexZeDim@mail.ru>
2020-03-14 18:27:29 +0300

    API => master (#7)
    * new getCharacter (#5)
    
    * characters schema fix
    
    * index characters fix
    
    * error statusCode
    
    * error statusCode
    
    * error statusCode
    
    * media included
    
    * fixed for now
    
    * getCharacter errorCodes
    
    * +1 TODO
    
    * guild_db crest and statusCode
    
    * guild_db lastModified
    
    * indexLogs fix
    
    * guild fix
    
    * cluster balance
    
    * character test fix
    
    * create on request API guild and char
    
    * realm request to uppercase

--- routes/api/guilds.js
+++ routes/api/guilds.js
@@ -7,20 +7,25 @@ const guilds_db = require("../../db/guilds_db");
 router.get('/:name@:realm', async function(req, res) {
     try {
         let { slug } = await realms_db.findOne({$or: [
-                { 'name': req.params.realm },
+                { 'name': (req.params.realm).replace(/^\w/, c => c.toUpperCase()) },
                 { 'slug': req.params.realm },
-                { 'name_locale': req.params.realm },
+                { 'name_locale': (req.params.realm).replace(/^\w/, c => c.toUpperCase()) },
                 { 'ticker': req.params.realm },
             ]});
-        console.log(slug);
         let guildData = await guilds_db.findById(`${req.params.name.toLowerCase()}@${slug}`);
-        console.log(guildData);
-        /***
-         * TODO checkDB
-         * TODO if not res
-         * TODO then send req
-         * TODO return result
-         */
+        if (!guildData) {
+            const getGuild = require('../../voluspa/getGuild');
+            const keys_db = require("../../db/keys_db");
+            const { token } = await keys_db.findOne({tags: `VOLUSPA-indexGuilds`});
+            guildData = await getGuild(slug, req.params.name.toLowerCase(), token);
+            guildData.createdBy = `VOLUSPA-userInput`;
+            guildData.updatedBy = `VOLUSPA-userInput`;
+            if (guildData.statusCode === 200) {
+                await guilds_db.create(guildData).then(ch => console.info(`C,${ch._id}`));
+                guildData.updatedAt = Date.now();
+                guildData.updatedAt = Date.now();
+            }
+        }
         res.status(200).json(guildData);
     } catch (e) {
         res.status(404).json(e);

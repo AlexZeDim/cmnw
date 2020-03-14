525cf4aa196d0557083ee9ca033d99f8f2066dc1
Boralus Whale <alexzedim@mail.ru>
2020-03-15 02:21:59 +0300

    AlexZeDim remove tail, but instead maximum on high populated array with low pricetick. But it's ok for realm-side

--- dma/price_level.js
+++ dma/price_level.js
@@ -3,10 +3,20 @@ const auctions_db = require("../db/auctions_db");
 async function price_level () {
     try {
         let priceArray = [];
+        let counter = 0;
         let docs = await auctions_db.distinct('unit_price', { "item.id": 152512}).lean();
         console.log(docs);
         for (let i = 0; i < docs.length; i++) {
-            if (docs[i-1] !== undefined) console.log(docs[i]/docs[i-1])
+            counter += docs[i];
+            if (i > Math.round(docs.length*0.85)-1) {
+                if ((counter-docs[i])/docs[i-1] > (counter/docs[i])) {
+                    console.info(`Tail strike: ${docs[i]} previous: ${docs[i-1]}`)
+                } else {
+                    priceArray.push(docs[i])
+                }
+            } else {
+                priceArray.push(docs[i])
+            }
         }
         console.log(priceArray)
     } catch (err) {

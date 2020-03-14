bb4cb7d20ea1c04836152e964007798a0fbba5fd
Boralus Whale <alexzedim@mail.ru>
2020-03-15 02:20:33 +0300

    Bluratella simple version with divide, no more then x1.5

--- dma/price_level.js
+++ dma/price_level.js
@@ -3,24 +3,10 @@ const auctions_db = require("../db/auctions_db");
 async function price_level () {
     try {
         let priceArray = [];
-        let counter = 0;
         let docs = await auctions_db.distinct('unit_price', { "item.id": 152512}).lean();
         console.log(docs);
         for (let i = 0; i < docs.length; i++) {
-            counter += docs[i];
-            console.log(docs[i], counter/docs[i])
-            /*
-
-            if (i > Math.round(docs.length*0.85)-1) {
-                if ((counter-docs[i])/docs[i-1] > (counter/docs[i])) {
-                    console.info(`Tail strike: ${docs[i]} previous: ${docs[i-1]}`)
-                } else {
-                    priceArray.push(docs[i])
-                }
-            } else {
-                priceArray.push(docs[i])
-            }*/
-            // if (docs[i+1] !== undefined) deltaArray.push(parseFloat((docs[i+1]-docs[i]).toFixed(2))); console.log(`${docs[i]}-${docs[i+1]} D: ${}`);
+            if (docs[i-1] !== undefined) console.log(docs[i]/docs[i-1])
         }
         console.log(priceArray)
     } catch (err) {

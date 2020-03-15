68a3bdd14e5f945f7523d4a5a3165e41d5a68a25
Boralus Whale <alexzedim@mail.ru>
2020-03-15 03:52:18 +0300

    sampleVariable

--- dma/price_level.js
+++ dma/price_level.js
@@ -5,14 +5,14 @@ async function price_level () {
     try {
         console.time(`DMA-${price_level.name}`);
         let priceArray = [];
-        let SampleDev, prevSampleDev = 0;
+        let sampleVariable, sampleVariable_prev = 0;
         let docs = await auctions_db.distinct('unit_price', { "item.id": 152512, connected_realm_id: 1602}).lean();
         for (let i = 1; i < docs.length; i++) {
             //TODO should we start from 1 cause we ignore zeros?
-            if (i > 1) prevSampleDev = SampleDev; else prevSampleDev = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
-            SampleDev = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
-            console.log(docs[i], prevSampleDev, SampleDev);
-            if (prevSampleDev*2 < SampleDev) console.log(docs[i]); else priceArray.push(docs[i]);
+            if (i > 1) sampleVariable_prev = sampleVariable; else sampleVariable_prev = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
+            sampleVariable = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
+            console.log(docs[i], sampleVariable_prev, sampleVariable);
+            if (sampleVariable_prev*2 < sampleVariable) console.log(docs[i]); else priceArray.push(docs[i]);
             /***
              * If sampleDev is twice or x1.5 bigger then, oh my gosh, ban this shit from array
              * Price tick for chart is Pmax-Pmin/length and round to 0.25

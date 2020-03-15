61439c5957bf390e0dfa8f0cd70164b3cd463e8b
Boralus Whale <alexzedim@mail.ru>
2020-03-16 01:37:17 +0300

    we dive deeper

--- dma/price_level.js
+++ dma/price_level.js
@@ -6,23 +6,39 @@ async function price_level () {
         console.time(`DMA-${price_level.name}`);
         let priceArray = [];
         let sampleVariable, sampleVariable_prev = 0;
-        let docs = await auctions_db.distinct('unit_price', { "item.id": 152512, connected_realm_id: 1602}).lean();
+        let docs = await auctions_db.distinct('unit_price', { "item.id": 168487, connected_realm_id: 1602}).lean();
+        let ts = await auctions_db.distinct('lastModified', { "item.id": 168487, connected_realm_id: 1602}).lean();
+        console.log(ts);
         for (let i = 1; i < docs.length; i++) {
             //TODO should we start from 1 cause we ignore zeros?
             if (i > 1) sampleVariable_prev = sampleVariable; else sampleVariable_prev = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
             sampleVariable = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
-            console.log(docs[i], sampleVariable_prev, sampleVariable);
+            //console.log(docs[i], sampleVariable_prev, sampleVariable);
             if (sampleVariable_prev*2 < sampleVariable) console.log(docs[i]); else priceArray.push(docs[i]);
             /***
              * If sampleDev is twice or x1.5 bigger then, oh my gosh, ban this shit from array
              * Price tick for chart is Pmax-Pmin/length and round to 0.25
              */
         }
-        let roundNearQtr = (number) => {
-            return (Math.round(number * 4) / 4).toFixed(2);
-        };
+        let roundNearQtr = (number) => parseFloat((Math.round(number * 4) / 4).toFixed(2));
+
         console.log(priceArray);
+
+        let start = (roundNearQtr(priceArray[0]));
+        let stop = (roundNearQtr(priceArray[priceArray.length-1]));
+        let step = (roundNearQtr((priceArray[priceArray.length-1]-priceArray[0])/priceArray.length));
+
+        console.log(roundNearQtr(priceArray[0]));
+        console.log(roundNearQtr(priceArray[priceArray.length-1]));
         console.log(roundNearQtr((priceArray[priceArray.length-1]-priceArray[0])/priceArray.length));
+
+        const range = (start, stop, step = 1) => Array(Math.ceil((stop + step - start) / step)).fill(start).map((x, y) => x + y * step);
+        let priceRange_array = await range(start, stop, step);
+        console.log(priceRange_array);
+        let cursor = await auctions_db.find( { "item.id": 168487, connected_realm_id: 1602}).lean().cursor({batchSize: 20});
+        for (let order = await cursor.next(); order != null; order = await cursor.next()) {
+            //console.log(order)
+        }
         connection.close();
         console.timeEnd(`DMA-${price_level.name}`);
     } catch (err) {

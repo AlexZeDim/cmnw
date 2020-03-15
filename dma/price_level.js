847376c0f54de2bb6eb6ded70fba119382a0ceff
Boralus Whale <alexzedim@mail.ru>
2020-03-15 03:33:21 +0300

    this is not even my final form price tick!

--- dma/price_level.js
+++ dma/price_level.js
@@ -2,22 +2,23 @@ const auctions_db = require("../db/auctions_db");
 
 async function price_level () {
     try {
-        let counter = 0;
-        let docs = await auctions_db.distinct('unit_price', { "item.id": 152512, connected_realm_id: 1602}).lean();
+        let priceArray = [];
+        let SampleDev, prevSampleDev = 0;
+        let docs = await auctions_db.distinct('unit_price', { "item.id": 152512}).lean();
         console.log(docs);
         for (let i = 0; i < docs.length; i++) {
-            console.log(docs[i],(1/docs.length*(docs[i]*docs[i]))-((1/docs.length*docs[i])*(1/docs.length*docs[i])));
-            /*counter += docs[i];
-            if (i > Math.round(docs.length*0.85)-1) {
-                if ((counter-docs[i])/docs[i-1] > (counter/docs[i])) {
-                    console.info(`Tail strike: ${docs[i]} previous: ${docs[i-1]}`)
-                } else {
-                    priceArray.push(docs[i])
-                }
-            } else {
-                priceArray.push(docs[i])
-            }*/
+            if (i > 0) prevSampleDev = SampleDev; else prevSampleDev = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
+            SampleDev = (1/docs.length*(Math.pow(docs[i],2)))-(Math.pow((1/docs.length*docs[i]),2));
+            if (prevSampleDev*1.5 < SampleDev) console.log(docs[i]); else priceArray.push(docs[i]);
+            /***
+             * If sampleDev is twice or x1.5 biggerm then oh my gosh, ban this shit from array
+             * Price tick for chart is Pmax-Pmin/length and round to 0.25
+             */
         }
+        let roundNearQtr = (number) => {
+            return (Math.round(number * 4) / 4).toFixed(2);
+        };
+        console.log(roundNearQtr((priceArray[priceArray.length-1]-priceArray[0])/priceArray.length));
     } catch (err) {
         console.log(err);
     }

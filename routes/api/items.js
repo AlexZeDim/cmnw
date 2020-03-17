77dbb10a7245cd160264143458d668422d6c99b5
Boralus Whale <alexzedim@mail.ru>
2020-03-17 15:07:48 +0300

    API wip

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const chart = require("../../dma/price_level");
+const realms_db = require("../../db/realms_db");
+const items_db = require("../../db/items_db");
 
-router.get('/:id', async function(req, res) {
+const charts = require("../../dma/charts");
+
+router.get('/:name@:realm', async function(req, res) {
     try {
-        let x = await chart();
+        let { connected_realm_id } = await realms_db.findOne({$or: [
+                { 'name': (req.params.realm).replace(/^\w/, c => c.toUpperCase()) },
+                { 'slug': req.params.realm },
+                { 'name_locale': (req.params.realm).replace(/^\w/, c => c.toUpperCase()) },
+                { 'ticker': req.params.realm },
+            ]});
+        let x = await charts(_id ,connected_realm_id);
         res.status(200).json(x);
     } catch (e) {
         res.status(404).json(e);

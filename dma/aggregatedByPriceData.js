cf9b43834562175c0ce6f3b678b4bd2131af88f9
Boralus Whale <alexzedim@mail.ru>
2020-03-20 02:20:08 +0300

    new data

--- dma/aggregatedByPriceData.js
+++ dma/aggregatedByPriceData.js
@@ -1,6 +1,6 @@
 const auctions_db = require("../db/auctions_db");
 
-async function auctionsData (item_id = 168487, connected_realm_id = 1602) {
+async function aggregatedByPriceData (item_id = 168487, connected_realm_id = 1602) {
     try {
         const {lastModified} = await auctions_db.findOne({ "item.id": item_id, connected_realm_id: connected_realm_id}).sort({lastModified: -1});
         let test = await auctions_db.aggregate([
@@ -13,27 +13,23 @@ async function auctionsData (item_id = 168487, connected_realm_id = 1602) {
             },
             {
                 $project: {
-                    _id: "$lastModified",
                     id: "$id",
                     quantity: "$quantity",
                     price: { $ifNull: [ "$buyout", { $ifNull: [ "$bid", "$unit_price" ] } ] },
                 }
             },{
                 $group: {
-                    _id: "$_id",
+                    _id: "$price",
                     quantity: {$sum: "$quantity"},
                     open_interest: {$sum: { $multiply: [ "$price", "$quantity" ] }},
-                    min: {$min: "$price"},
-                    avg: {$avg: "$price"},
-                    min_size: {$min: {$cond: [{$gte: ["$quantity", 200]}, "$price", {$min: "$price"}]}},
                     orders: {$addToSet: "$id"},
                 }
             }
-            ]);
+        ]);
         console.log(test)
     } catch (error) {
         console.error(error)
     }
 }
 
-auctionsData();
+aggregatedByPriceData();

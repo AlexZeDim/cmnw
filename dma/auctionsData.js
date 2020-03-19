0042064ff0c4640a73fde3a369d213c6ddb85124
Boralus Whale <alexzedim@mail.ru>
2020-03-19 23:34:32 +0300

    at last

--- dma/auctionsData.js
+++ dma/auctionsData.js
@@ -1,6 +1,6 @@
 const auctions_db = require("../db/auctions_db");
 
-async function auctionsData (item_id = 152510, connected_realm_id = 1602) {
+async function auctionsData (item_id = 168487, connected_realm_id = 1602) {
     try {
         const {lastModified} = await auctions_db.findOne({ "item.id": item_id, connected_realm_id: connected_realm_id}).sort({lastModified: -1});
         let test = await auctions_db.aggregate([
@@ -12,18 +12,24 @@ async function auctionsData (item_id = 152510, connected_realm_id = 1602) {
                 }
             },
             {
-                $group: {
+                $project: {
                     _id: "$lastModified",
-                    open_interest: { $cond: [{ "$eq": [ "$buyout", null ] }, {$sum: { $multiply: [ "$unit_price", "$quantity" ] }}, {$sum: "$buyout"} ] },
-/*                    quantity: {$sum: "$quantity"},
-                    min: {$min: "$price"},
-                    min_size: {$min: {$cond: [{$gte: ["$quantity", 200]}, "$price", "$min:$price"]}},
-                    avg: {$avg: "$price"},
-                    max: {$max: "$price"},
-                    max_size: {$max: {$cond: [{$gte: ["$quantity", 200]}, "$price", "$max:$price"]}},
-                    cp: {$addToSet: "$owner"},*/
+                    id: "$id",
+                    quantity: "$quantity",
+                    price: { $ifNull: [ "$buyout", { $ifNull: [ "$bid", "$unit_price" ] } ] },
                 }
-            }]);
+            },{
+                $group: {
+                    _id: "$_id",
+                    quantity: {$sum: "$quantity"},
+                    open_interest: {$sum: { $multiply: [ "$price", "$quantity" ] }},
+                    min: {$min: "$price"},
+                    avg: {$avg: "$price"},
+                    min_size: {$min: {$cond: [{$gte: ["$quantity", 200]}, "$price", {$min: "$price"}]}},
+                    orders: {$addToSet: "$id"},
+                }
+            }
+            ]);
         console.log(test)
     } catch (error) {
 

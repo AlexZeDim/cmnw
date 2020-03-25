c75dfe55c9e5e1b3c636fa4aef1946e38d421623
AlexZeDim <alexzedim@mail.ru>
2020-03-25 21:53:23 +0300

    findAll 2 fields

--- discord/commands/findAll.js
+++ discord/commands/findAll.js
@@ -13,13 +13,12 @@ module.exports = {
         let embed = new MessageEmbed();
         let character = await axios.get(encodeURI(`http://${process.env.localhost}:3030/api/findAll/${params[0]}@${params[1]}`)).then(({data}) => {
             console.log(data);
-            let { _id, checksum, Hash_A } = data;
+            let { _id, checksum, match } = data;
             embed.setTitle(_id.toUpperCase());
             embed.setURL('https://discord.js.org/');
-            embed.setAuthor(checksum.pets, '', 'https://discord.js.org');
-            for (let i = 0; i < Hash_A.length; i++) {
-                let {guild, guild_rank} = Hash_A[i];
-                embed.addField(`Match: ${i+1}`, `${Hash_A[i]._id}\n${guild} ,R${guild_rank}`, true);
+            for (let i = 0; i < match.length; i++) {
+                let {guild, guild_rank} = match[i];
+                embed.addField(`Match: ${i+1}`, `${match[i]._id}\n${guild}, R${guild_rank}`, true);
             }
             return embed
         });

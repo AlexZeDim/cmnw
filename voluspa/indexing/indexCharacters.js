eddc133a27ea4f0ad02abce55c85e9b76a1f5174
AlexZeDim <alexzedim@mail.ru>
2020-03-30 09:39:32 +0300

    indexCharacters refractions rc

--- voluspa/indexing/indexCharacters.js
+++ voluspa/indexing/indexCharacters.js
@@ -17,34 +17,29 @@ async function indexCharacters (queryFind = '', queryKeys = {tags: `VOLUSPA-${in
     try {
         console.time(`VOLUSPA-${indexCharacters.name}`);
         let documentBulk = [];
+        let token;
+        token = await keys_db.findOne(queryKeys).then(({token}) => {return token});
         const cursor = characters_db.find(queryFind).lean().cursor({batchSize: bulkSize});
-        cursor.on('data', async (documentData) => {
-            documentBulk.push(documentData);
+        cursor.on('data', async ({_id}) => {
+            documentBulk.push(getCharacter((_id).split('@')[1], (_id).split('@')[0], token).then(u_character => {
+                u_character.updatedBy = `VOLUSPA-${indexCharacters.name}`;
+                characters_db.findByIdAndUpdate(
+                    {
+                        _id: u_character._id
+                    },
+                    u_character,
+                    {
+                        upsert : true,
+                        new: true,
+                        setDefaultsOnInsert: true,
+                        lean: true
+                    }
+                ).then(({_id, statusCode}) => console.info(`U,${_id}:${statusCode}`))
+            }).catch(e=>(e)));
             if (documentBulk.length >= bulkSize) {
                 cursor.pause();
                 console.time(`========================`);
-                const { token } = await keys_db.findOne(queryKeys);
-                const promises = documentBulk.map(async (req) => {
-                    try {
-                        let upd_char = await getCharacter((req._id).split('@')[1], (req._id).split('@')[0], token);
-                        upd_char.updatedBy = `VOLUSPA-${indexCharacters.name}`;
-                        await characters_db.findByIdAndUpdate(
-                            {
-                                _id: req._id
-                            },
-                            upd_char,
-                            {
-                                upsert : true,
-                                new: true,
-                                setDefaultsOnInsert: true,
-                                lean: true
-                            }
-                        ).then(({_id, statusCode}) => console.info(`U,${_id}:${statusCode}`));
-                    } catch (error) {
-                        console.log(error)
-                    }
-                });
-                await Promise.all(promises);
+                await Promise.all(documentBulk);
                 documentBulk = [];
                 cursor.resume();
                 console.timeEnd(`========================`);

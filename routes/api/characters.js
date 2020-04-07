d9c34b0b0406e12ac371d7bc703520db92cbceb5
AlexZeDim <AlexZeDim@mail.ru>
2020-04-07 19:28:48 +0300

    API minor rework

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -4,16 +4,17 @@ const router = express.Router();
 const realms_db = require("../../db/realms_db");
 const characters_db = require("../../db/characters_db");
 
-router.get('/:name@:realm', async function(req, res) {
+router.get('/:n@:r', async function(req, res) {
     try {
         //TODO if not @ then
-        let { slug } = await realms_db.findOne({$text:{$search: req.params.realm}});
-        let characterData = await characters_db.findById(`${req.params.name.toLowerCase()}@${slug}`);
+        const {n, r} = req.params;
+        let { slug } = await realms_db.findOne({$text:{$search: r}});
+        let characterData = await characters_db.findById(`${n.toLowerCase()}@${slug}`);
         if (!characterData) {
             const getCharacter = require('../../osint/getCharacter');
             const keys_db = require("../../db/keys_db");
             const { token } = await keys_db.findOne({tags: `OSINT-indexCharacters`});
-            characterData = await getCharacter(slug, req.params.name.toLowerCase(), token, true);
+            characterData = await getCharacter(slug, n.toLowerCase(), token, true);
             characterData.createdBy = `OSINT-userInput`;
             characterData.updatedBy = `OSINT-userInput`;
             if (characterData.statusCode === 200) {
--- routes/api/guilds.js
+++ routes/api/guilds.js
@@ -4,15 +4,16 @@ const router = express.Router();
 const realms_db = require("../../db/realms_db");
 const guilds_db = require("../../db/guilds_db");
 
-router.get('/:name@:realm', async function(req, res) {
+router.get('/:g@:r', async function(req, res) {
     try {
-        let { slug } = await realms_db.findOne({$text:{$search: req.params.realm}});
-        let guildData = await guilds_db.findById(`${req.params.name.toLowerCase().replace(/\s/g,"-")}@${slug}`);
+        const {g, r} = req.params;
+        let { slug } = await realms_db.findOne({$text:{$search: r}});
+        let guildData = await guilds_db.findById(`${g.toLowerCase().replace(/\s/g,"-")}@${slug}`);
         if (!guildData) {
             const getGuild = require('../../osint/getGuild');
             const keys_db = require("../../db/keys_db");
             const { token } = await keys_db.findOne({tags: `OSINT-indexGuilds`});
-            guildData = await getGuild(slug, req.params.name.toLowerCase().replace(/\s/g,"-"), token);
+            guildData = await getGuild(slug, g.toLowerCase().replace(/\s/g,"-"), token);
             guildData.createdBy = `OSINT-userInput`;
             guildData.updatedBy = `OSINT-userInput`;
             if (guildData.statusCode === 200) {

1f5152c1af8939a66865f4d8c3c4a4bed5729a7b
AlexZeDim <alexzedim@mail.ru>
2020-04-07 18:58:42 +0300

    WIP API

--- db/realms_db.js
+++ db/realms_db.js
@@ -75,7 +75,7 @@ schema.index({ slug: 1 },{name: 'Slug', collation: { strength: 3 }});
 schema.index({ name_locale: 1 },{name: 'NameLocale', collation: { strength: 3 }});
 schema.index({ ticker: 1 },{name: 'Ticker', collation: { strength: 3 }});
 schema.index({ connected_realm_id: 1 },{name: 'ConnectedRealms'});
-schema.index({ name: "text", slug: "text", name_locale: "text" },{name: 'SearchQuery'});
+schema.index({ name: "text", slug: "text", name_locale: "text", ticker: "text" },{name: 'SearchQuery'});
 
 let realms_db = mongoose.model('realms', schema);
 

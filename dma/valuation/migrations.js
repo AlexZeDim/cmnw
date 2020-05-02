73b8b21f6ff97784c17fc4b616c97af6dad4bf75
AlexZeDim <alexzedim@mail.ru>
2020-05-02 20:14:10 +0300

    migrations fix

--- dma/valuation/migrations.js
+++ dma/valuation/migrations.js
@@ -10,12 +10,6 @@ async function migrations() {
     try {
         let encn = await pricing_methods.updateMany({ profession: "ENCH", item_quantity: 0 },{ item_quantity: 1 });
         console.info(encn);
-        let type_added = await pricing_methods.updateMany({},{
-            //type: 'primary',
-            createdBy: 'DMA-indexProfessions',
-            updatedBy: 'DMA-indexProfessions'
-        });
-        console.info(type_added);
         connection.close();
     } catch (err) {
         console.error(err);

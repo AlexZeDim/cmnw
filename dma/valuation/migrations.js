c24559abd0d08fb54f264aea929264793dc29682
AlexZeDim <alexzedim@mail.ru>
2020-05-03 01:22:08 +0300

    expulsom to migrations

--- dma/valuation/migrations.js
+++ dma/valuation/migrations.js
@@ -8,8 +8,243 @@ const {connection} = require('mongoose');
 
 async function migrations() {
     try {
-        let encn = await pricing_methods.updateMany({ profession: "ENCH", item_quantity: 0 },{ item_quantity: 1 });
-        console.info(encn);
+        let enchant = await pricing_methods.updateMany({ profession: "ENCH", item_quantity: 0 },{ item_quantity: 1 });
+        console.info(enchant);
+        let expulsom = [
+            {
+                _id: 'P38737:1',
+                description: {
+                    en_GB: 'Scrap Monel-Hardened Armguards into Expulsom',
+                    ru_RU: 'Распылить укрепленные монелитом боевые наручи в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'BSMT',
+                spell_id: 253183,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 38737,
+                reagents: [
+                    { _id: 161887, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'P38737:2',
+                description: {
+                    en_GB: 'Scrap Monel-Hardened Armguards into Expulsom',
+                    ru_RU: 'Распылить укрепленные монелитом боевые наручи в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'BSMT',
+                spell_id: 253183,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 38737,
+                reagents: [
+                    { _id: 152809, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'P39083:1',
+                description: {
+                    en_GB: 'Scrap Tidespray Linen Bracers into Expulsom',
+                    ru_RU: 'Распылить Наручи из морского льна в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'CLTH',
+                spell_id: 257103,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39083,
+                reagents: [
+                    { _id: 161984, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'P39083:2',
+                description: {
+                    en_GB: 'Scrap Tidespray Linen Bracers into Expulsom',
+                    ru_RU: 'Распылить Наручи из морского льна в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'CLTH',
+                spell_id: 257103,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39083,
+                reagents: [
+                    { _id: 154692, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'P39045:1',
+                description: {
+                    en_GB: 'Scrap Shimmerscale Armguards into Expulsom',
+                    ru_RU: 'Распылить Наручи из морского льна в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'LTHR',
+                spell_id: 256757,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39045,
+                reagents: [
+                    { _id: 161960, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'P39045:2',
+                description: {
+                    en_GB: 'Scrap Shimmerscale Armguards into Expulsom',
+                    ru_RU: 'Распылить Наручи из морского льна в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'LTHR',
+                spell_id: 256757,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39045,
+                reagents: [
+                    { _id: 154153, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'P39031:1',
+                description: {
+                    en_GB: 'Scrap Coarse Leather Armguards into Expulsom',
+                    ru_RU: 'Распылить Боевые наручи из шершавой кожи в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'LTHR',
+                spell_id: 256756,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39031,
+                reagents: [
+                    { _id: 161945, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'I39031:2',
+                description: {
+                    en_GB: 'Scrap Coarse Leather Armguards into Expulsom',
+                    ru_RU: 'Распылить Боевые наручи из шершавой кожи в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'LTHR',
+                spell_id: 256756,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39031,
+                reagents: [
+                    { _id: 154145, quantity: 6.5 }
+                ]
+            },
+            {
+                _id: 'P39538:1',
+                description: {
+                    en_GB: 'Scrap Coarse Leather Armguards into Expulsom',
+                    ru_RU: 'Распылить Мрачный жезл зачаровывателя в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'ENCH',
+                spell_id: 265106,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39538,
+                reagents: [
+                    { _id: 161925, quantity: 5 }
+                ]
+            },
+            {
+                _id: 'P39538:2',
+                description: {
+                    en_GB: 'Scrap Coarse Leather Armguards into Expulsom',
+                    ru_RU: 'Распылить Мрачный жезл зачаровывателя в Дистилиум'
+                },
+                expansion: 'BFA',
+                item_id: 152668,
+                name: {
+                    en_GB: 'Expulsom',
+                    ru_RU: 'Дистилиум'
+                },
+                profession: 'ENCH',
+                spell_id: 265106,
+                item_quantity: 1,
+                createdBy: 'DMA-migrations',
+                type: 'primary',
+                updatedBy: 'DMA-migrations',
+                recipe_id: 39538,
+                reagents: [
+                    { _id: 152872, quantity: 5 }
+                ]
+            },
+        ];
+        for (let method of expulsom) {
+            await pricing_methods.create(method).then(doc => console.info(doc))
+        }
         connection.close();
     } catch (err) {
         console.error(err);

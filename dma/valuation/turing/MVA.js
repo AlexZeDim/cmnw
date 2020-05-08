8a60b3620d3243de6374c62568f4ede82059c62a
AlexZeDim <AlexZeDim@mail.ru>
2020-05-08 13:45:40 +0300

    XVA Model Turing

---
+++ dma/valuation/turing/MVA.js
@@ -0,0 +1,71 @@
+
+/**
+ * TODO RECURSIVE CONTROL
+ * @param method
+ * @param connected_realm_id
+ * @constructor
+ */
+
+async function methodValuationAdjustment (method = {}, connected_realm_id = 1602) {
+    const itemValuationAdjustment = require('./IVA');
+    try {
+        /**
+         * Asset Class hierarchy map
+         * @type {Map<unknown, unknown>}
+         */
+        const assetClassMap = new Map([
+            ['VENDOR,REAGENT,ITEM', 0],
+            ['CONST,REAGENT,ITEM', 1],
+            ['REAGENT,MARKET,ITEM', 2],
+            ['REAGENT,MARKET,MARKET', 3],
+            ['CAP,MARKET,DERIVATIVE', 4],
+            ['CAP,PREMIUM,DERIVATIVE', 5],
+            ['PREMIUM,REAGENT,DERIVATIVE', 6],
+            ['PREMIUM,MARKET,ITEM', 7],
+            ['PREMIUM,REAGENT,ITEM', 8],
+        ]);
+        /**
+         * Sort reagent_items according to map
+         */
+        method.reagent_items.sort((a, b) => assetClassMap.get(a.v_class) - assetClassMap.get(b.v_class));
+        /**
+         * Init production cost and premium
+         * @type {number}
+         */
+        let queue_cost = 0;
+        let premium_items = [];
+        let reagent_items = [];
+        for (let reagent_item of method.reagent_items) {
+            /**
+             * Check Reagent.value, if there is not add to cost 0 but premium
+             * premium += (Price market (if exist) - quene_cost)
+             * */
+            if (reagent_item.v_class.some(v_class => v_class === 'PREMIUM')) {
+                premium_items.push(reagent_item)
+            } else {
+                let x = await itemValuationAdjustment(reagent_item, connected_realm_id);
+                Object.assign(reagent_item, {
+                    price: x.reagent.value,
+                    value: parseFloat((x.reagent.value * reagent_item.quantity).toFixed(2))
+                });
+                reagent_items.push(reagent_item);
+                queue_cost += Number((x.reagent.value * reagent_item.quantity).toFixed(2));
+            }
+        }
+        /**
+         * End of loop
+         */
+        return {
+            _id: method._id,
+            queue_cost: Number(queue_cost.toFixed(2)),
+            queue_quantity: method.item_quantity,
+            nominal_value: Number((queue_cost / method.item_quantity).toFixed(2)),
+            reagent_items: reagent_items,
+            premium_items: premium_items
+        };
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+module.exports = methodValuationAdjustment;
---
+++ dma/valuation/turing/XVA.js
@@ -0,0 +1,31 @@
+const items_db = require("../../../db/items_db");
+const itemValuationAdjustment = require('./IVA');
+const {connection} = require('mongoose');
+
+
+async function XVA () {
+    try {
+        console.time(`DMA-${XVA.name}`); //v_class: ['REAGENT', 'MARKET', 'DERIVATIVE'], profession_class: "INSC",
+        let cursor = await items_db.find({expansion: "BFA", _id: 168310}).limit(10).cursor({batchSize: 10});
+        cursor.on('data', async item_ => {
+            cursor.pause();
+            await itemValuationAdjustment(item_, 1602);
+            cursor.resume();
+        });
+        cursor.on('error', error => {
+            console.error(`E,DMA-${XVA.name},${error}`);
+            cursor.close();
+        });
+        cursor.on('close', async () => {
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            connection.close();
+            console.timeEnd(`DMA-${XVA.name}`);
+        });
+    } catch (err) {
+        console.error(`${XVA.name},${err}`);
+    }
+}
+
+XVA();
+
+module.exports = XVA;

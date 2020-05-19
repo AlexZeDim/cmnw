41ed4c05409831e2df92d3f0bf85144a7231990a
Sasha <AlexZeDim@mail.ru>
2020-05-19 23:41:50 +0300

    guilds WIP

--- osint/getGuild.js
+++ osint/getGuild.js
@@ -1,40 +1,70 @@
 const battleNetWrapper = require('battlenet-api-wrapper');
 const moment = require('moment');
+const guild_db = require("../db/guilds_db");
+const characters_db = require("../db/characters_db");
 
 const clientId = '530992311c714425a0de2c21fcf61c7d';
 const clientSecret = 'HolXvWePoc5Xk8N28IhBTw54Yf8u2qfP';
 
-async function getGuild (realmSlug, nameSlug, token = '') {
+async function getGuild (realmSlug, nameSlug, token = '', updatedBy = `DMA-${getGuild.name}`) {
     try {
         const bnw = new battleNetWrapper();
         await bnw.init(clientId, clientSecret, token, 'eu', 'en_GB');
-        const [{id, name, faction, achievement_points, member_count, realm, crest, created_timestamp, lastModified, statusCode }, {members}] = await Promise.all([
-            bnw.WowProfileData.getGuildSummary(realmSlug, nameSlug),
-            bnw.WowProfileData.getGuildRoster(realmSlug, nameSlug),
-        ]);
-        console.info(`U,${getGuild.name},${nameSlug}@${realmSlug}:${id},${member_count}`);
-        return ({
+        let guild = new guild_db({
             _id: `${nameSlug}@${realmSlug}`,
-            id: id,
-            name: name,
-            slug: nameSlug,
-            faction: faction.name,
-            achievement_points: achievement_points,
-            member_count: member_count,
-            realm: realm.name,
-            realm_slug: realmSlug,
-            crest: crest,
-            lastModified: moment(lastModified).toISOString(true),
-            created_timestamp: moment(created_timestamp).toISOString(true),
-            members: members,
-            statusCode: statusCode
-        });
+            statusCode: 400,
+            updatedBy: updatedBy,
+            isWatched: false
+        })
+        await Promise.all([
+            bnw.WowProfileData.getGuildSummary(realmSlug, nameSlug).then(({id, name, faction, achievement_points, member_count, realm, crest, created_timestamp, lastModified, statusCode }) => {
+                guild.id = id;
+                guild.name = name;
+                guild.faction = faction.name;
+                guild.realm_slug = realmSlug;
+                guild.realm = realm.name;
+                guild.crest = crest;
+                guild.achievement_points = achievement_points;
+                guild.member_count = member_count;
+                guild.lastModified = moment(lastModified).toISOString(true);
+                guild.created_timestamp = moment(created_timestamp).toISOString(true);
+                guild.statusCode = statusCode;
+            }),
+            bnw.WowProfileData.getGuildRoster(realmSlug, nameSlug).then(async ({members}) => {
+                for (let member of members) {
+                    let {character, rank} = member
+                    //TODO find char if not exists, getChar character.realm.slug
+                    guild.members_latest.push({
+                        character_id: character.id,
+                        character_name: character.name,
+                        character_rank: rank,
+                        character_hash_A: '',
+                        character_hash_B: ''
+                    })
+                }
+            }),
+        ])
+        if (guild.statusCode === 200) {
+            /**
+             * Detective:IndexDB
+             * TODO check for faction/realm/name
+             */
+            let guild_check = await guild_db.findOne({
+                id: guild.id,
+            }).lean();
+        }
+        let isCreated = await guild_db.findById(`${nameSlug}@${realmSlug}`).lean();
+        if (!isCreated) {
+            guild.createdBy = updatedBy;
+        }
+        if (guild.members_latest.length > 0 && isCreated) {
+
+        }
     } catch (error) {
-        let statusCode = 400;
-        if (/\d/g.test(error.toString())) statusCode = error.toString().match(/[0-9]+/g)[0];
         console.error(`E,${getGuild.name},${nameSlug}@${realmSlug},${error}`);
-        return {_id: `${nameSlug}@${realmSlug}`, slug: nameSlug, realm_slug: realmSlug, statusCode: statusCode}
     }
 }
+
+getGuild('gordunni','депортация','EUUFsZ2i2A1Lrp2fMWdCO24Sk9q1Hr3cP5').then(r=>console.log(r))
 
 module.exports = getGuild;

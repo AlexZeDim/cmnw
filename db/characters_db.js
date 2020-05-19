5d4795aae111761d6c8ddd931f73e3c175eff4c5
Sasha <AlexZeDim@mail.ru>
2020-05-19 23:40:43 +0300

    TODO

--- db/characters_db.js
+++ db/characters_db.js
@@ -54,7 +54,7 @@ let schema = new mongoose.Schema({
         type: String
     },
     history: [{
-        action: String,
+        action: String, //TODO store previous value!
         before: Date,
         after: Date
     }],

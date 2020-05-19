0b3ac43e221bb24eac644b58183adf922f2b6b7e
Sasha <AlexZeDim@mail.ru>
2020-05-19 13:36:36 +0300

    items_db $text index

--- db/items_db.js
+++ db/items_db.js
@@ -105,12 +105,18 @@ let schema = new mongoose.Schema({
     }
 });
 
-schema.index({ ticker: "text", "name.en_GB": "text", "name.ru_RU": "text" },{
-    weights: {
-        ticker: 10,
-        "name.en_GB": 5,
-        "name.ru_RU": 1
-    },name: 'SearchQuery'});
+schema.index({
+        "ticker": "text",
+        "name.en_GB": "text",
+        "name.ru_RU": "text"
+    },{
+        weights: {
+            "ticker": 10,
+            "name.en_GB": 1,
+            "name.ru_RU": 1
+        },
+        name: 'SearchQuery'
+});
 
 let items_db = mongoose.model('items', schema);
 

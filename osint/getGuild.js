c51931f43a937846441211fca2b7633e20e12329
Sasha <AlexZeDim@mail.ru>
2020-05-20 08:29:36 +0300

    hash_ex

--- osint/getGuild.js
+++ osint/getGuild.js
@@ -2,6 +2,7 @@ const battleNetWrapper = require('battlenet-api-wrapper');
 const moment = require('moment');
 const guild_db = require("../db/guilds_db");
 const characters_db = require("../db/characters_db");
+const getCharacter = require('./getCharacter');
 
 const clientId = '530992311c714425a0de2c21fcf61c7d';
 const clientSecret = 'HolXvWePoc5Xk8N28IhBTw54Yf8u2qfP';
@@ -16,35 +17,104 @@ async function getGuild (realmSlug, nameSlug, token = '', updatedBy = `DMA-${get
             updatedBy: updatedBy,
             isWatched: false
         })
-        await Promise.all([
-            bnw.WowProfileData.getGuildSummary(realmSlug, nameSlug).then(({id, name, faction, achievement_points, member_count, realm, crest, created_timestamp, lastModified, statusCode }) => {
-                guild.id = id;
-                guild.name = name;
-                guild.faction = faction.name;
-                guild.realm_slug = realmSlug;
-                guild.realm = realm.name;
-                guild.crest = crest;
-                guild.achievement_points = achievement_points;
-                guild.member_count = member_count;
-                guild.lastModified = moment(lastModified).toISOString(true);
-                guild.created_timestamp = moment(created_timestamp).toISOString(true);
-                guild.statusCode = statusCode;
-            }),
-            bnw.WowProfileData.getGuildRoster(realmSlug, nameSlug).then(async ({members}) => {
+
+        await bnw.WowProfileData.getGuildSummary(realmSlug, nameSlug).then(({id, name, faction, achievement_points, member_count, realm, crest, created_timestamp, lastModified, statusCode }) => {
+            guild.id = id;
+            guild.name = name;
+            guild.faction = faction.name;
+            guild.realm_slug = realmSlug;
+            guild.realm = realm.name;
+            guild.crest = crest;
+            guild.achievement_points = achievement_points;
+            guild.member_count = member_count;
+            guild.lastModified = moment(lastModified).toISOString(true);
+            guild.created_timestamp = moment(created_timestamp).toISOString(true);
+            guild.statusCode = statusCode;
+        })
+        /** Check request status is OK */
+        let guild_old = await guild_db.findById(`${nameSlug}@${realmSlug}`).lean();
+        if (!guild_old) {
+            guild.createdBy = updatedBy;
+        }
+        if (guild.statusCode === 200) {
+            /**
+             * Request membership list of this guild
+             */
+            await bnw.WowProfileData.getGuildRoster(realmSlug, nameSlug).then(async ({members}) => {
+                /** Members loop */
                 for (let member of members) {
                     let {character, rank} = member
-                    //TODO find char if not exists, getChar character.realm.slug
-                    guild.members_latest.push({
+                    /** Is every guild member is in OSINT-DB? */
+                    let character_ = await characters_db.findById(`${(character.name).toLowerCase()}@${guild.realm_slug}`);
+                    /** guild_member object for array.push */
+                    let guild_member = {
                         character_id: character.id,
                         character_name: character.name,
                         character_rank: rank,
-                        character_hash_A: '',
-                        character_hash_B: ''
-                    })
+                        character_date: guild.lastModified,
+                    };
+                    /** Check if data from guild roster not character current guild */
+                    if (character_) {
+                        /** Check hash existence and add it to guild_members */
+                        if ("a" in character_.hash) {
+                            Object.assign(guild_member, {character_hash_a: character_.hash.a})
+                        }
+                        if ("b" in character_.hash) {
+                            Object.assign(guild_member, {character_hash_b: character_.hash.b})
+                        }
+                        /** If current character guild_name != guild name from OSINT and new data upcoming update guild_data */
+                        if (character_.guild !== guild.name && moment(character_.lastModified).isSameOrAfter(guild.lastModified)) {
+                            character_.guild = guild.name;
+                            character_.guild_rank = rank;
+                            character_.save();
+                        }
+                        /** If character guild_name = guild name from OSINT then update guild_rank */
+                        if (character_.guild === guild.name) {
+                            character_.guild_rank = rank;
+                            character_.save();
+                        }
+                    } else  {
+                        /** If new name in OSINT then getCharacter will handles it */
+
+                        const playable_class = new Map([
+                            [1, 'Warrior'],
+                            [2, 'Paladin'],
+                            [3, 'Hunter'],
+                            [4, 'Rogue'],
+                            [5, 'Priest'],
+                            [6, 'Death Knight'],
+                            [7, 'Shaman'],
+                            [8, 'Mage'],
+                            [9, 'Warlock'],
+                            [10, 'Monk'],
+                            [11, 'Druid'],
+                            [12, 'Demon Hunter'],
+                        ]);
+                        let character_Object = {
+                            id: character.id,
+                            name: character.name,
+                            realm: guild.realm,
+                            realm_slug: character.realm.slug,
+                            guild: guild.name,
+                            guild_rank: rank,
+                            faction: guild.faction,
+                            level: character.level,
+                        }
+                        if (character.hasOwnProperty('playable_class')) {
+                            Object.assign(character_Object, {character_class: playable_class.get(character.playable_class.id)})
+                        }
+                        await getCharacter(realmSlug, character.name.toLowerCase(), character_Object, token, updatedBy)
+                    }
+                    /** Push to guild.members */
+                    guild.members_latest.push(guild_member)
                 }
-            }),
-        ])
-        if (guild.statusCode === 200) {
+                /** End of Members loop */
+            })
+
+            if (guild.members_latest.length && guild_old) {
+
+            }
+
             /**
              * Detective:IndexDB
              * TODO check for faction/realm/name
@@ -53,13 +123,7 @@ async function getGuild (realmSlug, nameSlug, token = '', updatedBy = `DMA-${get
                 id: guild.id,
             }).lean();
         }
-        let isCreated = await guild_db.findById(`${nameSlug}@${realmSlug}`).lean();
-        if (!isCreated) {
-            guild.createdBy = updatedBy;
-        }
-        if (guild.members_latest.length > 0 && isCreated) {
-
-        }
+        /** End of request status*/
     } catch (error) {
         console.error(`E,${getGuild.name},${nameSlug}@${realmSlug},${error}`);
     }

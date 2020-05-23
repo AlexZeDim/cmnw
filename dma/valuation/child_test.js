8c28e956a36147936f66ad3fc6c7bcbd35b05d43
Sasha <AlexZeDim@mail.ru>
2020-05-23 16:34:28 +0300

    parent - child final preparation

--- dma/valuation/child_test.js
+++ dma/valuation/child_test.js
@@ -1,14 +1,14 @@
 async function f (number = 1602) {
     try {
-        console.log(number)
         for (let i = 0; i < 6; i++) {
             await new Promise(resolve => setTimeout(resolve, 1000));
-            console.log(i)
+            console.log(`${number}-${i}-${Date.now()}`)
         }
+        process.exit(0)
     } catch (e) {
         console.error(e)
     }
 }
 
-f(process.argv[process.argv.findIndex(arg => arg === 'connected_realm_id') + 1])
+f(process.argv[process.argv.findIndex(arg => arg === 'connected_realm_id') + 1]).then(r => r)
 

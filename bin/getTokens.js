8dce63c0ee1eb43d92dbd20793c11dc4e7ee7b92
Sasha <AlexZeDim@mail.ru>
2020-05-23 17:25:32 +0300

    connection management for getToken

--- bin/getTokens.js
+++ bin/getTokens.js
@@ -1,6 +1,39 @@
+/**
+ * Connection with DB
+ */
+
+const {connect, connection} = require('mongoose');
+require('dotenv').config();
+connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    bufferMaxEntries: 0,
+    retryWrites: true,
+    useCreateIndex: true,
+    w: "majority",
+    family: 4
+});
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => console.log('Connected to database on ' + process.env.hostname));
+
+/**
+ * Model importing
+ */
+
 const keys_db = require("../db/keys_db");
+
+/**
+ * Modules
+ */
+
 const axios = require('axios');
-const {connection} = require('mongoose');
+
+/**
+ * Update tokens from Blizzard API as a separate task
+ * @returns {Promise<void>}
+ */
 
 async function getTokens () {
     try {

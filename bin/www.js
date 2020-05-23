8bea396453403baee400c8dfc8b2e9bbad825cef
Sasha <AlexZeDim@mail.ru>
2020-05-24 01:48:16 +0300

    graceful shutdown

--- bin/www.js
+++ bin/www.js
@@ -46,6 +46,8 @@ const server = http.createServer(app);
 server.listen(port, "0.0.0.0");
 server.on('error', onError);
 server.on('listening', onListening);
+process.on('beforeExit', shutDown);
+process.on('SIGINT', shutDown);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -105,4 +107,17 @@ function onListening() {
         ? 'pipe ' + addr
         : 'port ' + addr.port;
     console.log('Listening on ' + bind);
+}
+
+/**
+ * Event listener for HTTP server "shutdown" event.
+ */
+
+function shutDown() {
+    server.close( () => {
+        console.log('Closed out remaining connections');
+        connection.close(false, () => {});
+        console.log('DB connection closed.');
+        process.exit(0);
+    });
 }

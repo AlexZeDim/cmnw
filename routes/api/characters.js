bc8a1766a76e9897283d8ecd1bedc93753f46676
Sasha <AlexZeDim@mail.ru>
2020-05-24 00:51:25 +0300

    API refactoring

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -1,34 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Model importing
+ */
+
 const realms_db = require("../../db/realms_db");
 const characters_db = require("../../db/characters_db");
 
-//TODO refactoring
+/**
+ * Modules
+ */
+
+const moment = require('moment');
 
 router.get('/:n@:r', async function(req, res) {
     try {
         let {n, r} = req.params;
-        n = n.toLowerCase();
         let { slug } = await realms_db.findOne({$text:{$search: r}});
-        let characterData = await characters_db.findById(`${n}@${slug}`);
-        //TODO if char was updated long ago, 3D+ days
-        if (!characterData) {
-            const getCharacter = require('../../osint/getCharacter');
-            const keys_db = require("../../db/keys_db");
-            const { token } = await keys_db.findOne({tags: `OSINT-indexCharacters`});
-            characterData = await getCharacter(slug, n, token, true);
-            characterData.createdBy = `OSINT-userInput`;
-            characterData.updatedBy = `OSINT-userInput`;
-            if (characterData.statusCode === 200) {
-                await characters_db.create(characterData).then(ch => console.info(`C,${ch._id}`));
-                characterData.createdAt = Date.now();
-                characterData.updatedAt = Date.now();
+        if (n && slug) {
+            let characterData = await characters_db.findById(`${n}@${slug}`).lean();
+            if (!characterData || moment(characterData.lastModified).isAfter(moment().subtract(3, 'days'))) {
+                const getCharacter = require('../../osint/getCharacter');
+                const keys_db = require("../../db/keys_db");
+                const { token } = await keys_db.findOne({tags: `OSINT-indexCharacters`});
+                characterData = await getCharacter(slug, n, {}, token, `OSINT-userInput`, true);
             }
+            res.status(200).json(characterData);
+        } else {
+            res.status(404).json({error: "not found"});
         }
-        res.status(200).json(characterData);
     } catch (e) {
-        res.status(404).json(e);
+        res.status(500).json(e);
     }
 });
 
--- routes/api/guilds.js
+++ routes/api/guilds.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Model importing
+ */
+
 const realms_db = require("../../db/realms_db");
 const guilds_db = require("../../db/guilds_db");
 
-//TODO refactoring
+/**
+ * Modules
+ */
+
+const moment = require('moment');
 
 router.get('/:g@:r', async function(req, res) {
     try {
         let {g, r} = req.params;
-        g = g.toLowerCase().replace(/\s/g,"-");
         let { slug } = await realms_db.findOne({$text:{$search: r}});
-        let guildData = await guilds_db.findById(`${g}@${slug}`);
-        if (!guildData) {
-            const getGuild = require('../../osint/getGuild');
-            const keys_db = require("../../db/keys_db");
-            const { token } = await keys_db.findOne({tags: `OSINT-indexGuilds`});
-            guildData = await getGuild(slug, g, token);
-            guildData.createdBy = `OSINT-userInput`;
-            guildData.updatedBy = `OSINT-userInput`;
-            if (guildData.statusCode === 200) {
-                await guilds_db.create(guildData).then(g => console.info(`C,${g._id}`));
-                guildData.createdAt = Date.now();
-                guildData.updatedAt = Date.now();
+        if (g && slug) {
+            let guildData = await guilds_db.findById(`${g}@${slug}`).lean();
+            if (!guildData || moment(guildData.lastModified).isAfter(moment().subtract(5, 'days'))) {
+                const getGuild = require('../../osint/getGuild');
+                const keys_db = require("../../db/keys_db");
+                const { token } = await keys_db.findOne({tags: `OSINT-indexGuilds`});
+                guildData = await getGuild(slug, g, token, `OSINT-userInput`);
             }
+            res.status(200).json(guildData);
+        } else {
+            res.status(404).json({error: "not found"});
         }
-        res.status(200).json(guildData);
     } catch (e) {
-        res.status(404).json(e);
+        res.status(500).json(e);
     }
 });
 

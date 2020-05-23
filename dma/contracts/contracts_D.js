f31c5f39b188350f58df953e829f7e503099e2af
Sasha <AlexZeDim@mail.ru>
2020-05-23 14:57:38 +0300

    connection management for contracts D & M

--- dma/contracts/contracts_D.js
+++ dma/contracts/contracts_D.js
@@ -1,6 +1,7 @@
 /**
  * Connection with DB
  */
+
 const {connect, connection} = require('mongoose');
 require('dotenv').config();
 connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`, {
@@ -13,6 +14,9 @@ connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.ho
     w: "majority",
     family: 4
 });
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => console.log('Connected to database on ' + process.env.hostname));
 
 /**
  * Model importing

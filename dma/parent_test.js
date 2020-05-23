d30a71a1ff84fb1d7638bc58e43c82946bee9eda
Sasha <AlexZeDim@mail.ru>
2020-05-23 16:34:28 +0300

    parent - child final preparation

--- dma/parent_test.js
+++ dma/parent_test.js
@@ -1,19 +1,30 @@
 const pm2 = require('pm2');
 const path = require('path');
 
-let array = [1602,1658]
-for (let process_ins of array) {
-    pm2.start({
-        name: `test-${process_ins}`,
-        args: `connected_realm_id ${process_ins}`,
-        script: `${path.dirname(require.main.filename) + '/valuation/'}child_test.js`,
-        exec_mode: 'fork',
-        instances: 1,
-        max_memory_restart: '100M',
-        autorestart: false,
-    }, function(err, apps) {
-        pm2.disconnect();
-        if (err) throw err
-    });
+async function f (array = [1602,1658]) {
+    try {
+        for (let process_ins of array) {
+            console.log('wait for it')
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            console.log('10s')
+            await pm2.start({
+                name: `test-${process_ins}`,
+                args: `connected_realm_id ${process_ins}`,
+                script: `${path.dirname(require.main.filename) + '/valuation/child_test.js'}`,
+                exec_mode: 'fork',
+                instances: 1,
+                max_memory_restart: '100M',
+                autorestart: false,
+            }, function(err, apps) {
+                pm2.disconnect();
+                if (err) throw err
+            });
+        }
+    } catch (e) {
+        console.error(e)
+    }
 }
+
+
+f()
 

ade3721cbf019f888b812c6ef41a088fa9ff0a15
Sasha <AlexZeDim@mail.ru>
2020-05-24 00:49:38 +0300

    add old DB schemas

---
+++ db/guilds_old.js
@@ -0,0 +1,119 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+/*require('dotenv').config();
+mongoose.connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    bufferMaxEntries: 0,
+    retryWrites: true,
+    useCreateIndex: true,
+    w: "majority",
+    family: 4
+});*/
+
+let schema = new mongoose.Schema({
+    _id: {
+        type: String
+    },
+    id: {
+        type: Number
+    },
+    name: {
+        type: String
+    },
+    slug: {
+        type: String
+    },
+    realm: {
+        type: String
+    },
+    realm_slug: {
+        type: String
+    },
+    faction: {
+        type: String
+    },
+    crest: {
+        emblem: Object,
+        border: Object,
+        background: Object
+    },
+    guild_log: {
+        join: [{
+            character_name: String,
+            character_id: Number,
+            character_rank: Number,
+            character_date: Date,
+            character_checksum: String,
+        }],
+        promote: [{
+            character_name: String,
+            character_id: Number,
+            character_rank: Number,
+            character_date: Date,
+            character_checksum: String,
+        }],
+        demote: [{
+            character_name: String,
+            character_id: Number,
+            character_rank: Number,
+            character_date: Date,
+            character_checksum: String,
+        }],
+        leave: [{
+            character_name: String,
+            character_id: Number,
+            character_rank: Number,
+            character_date: Date,
+            character_checksum: String,
+        }],
+    },
+    members_latest: [{
+        character_name: String,
+        character_id: Number,
+        character_rank: Number,
+        character_date: Date,
+        character_checksum: String,
+    }],
+    members_prev: [{
+        character_name: String,
+        character_id: Number,
+        character_rank: Number,
+        character_date: Date,
+        character_checksum: String,
+    }],
+    achievement_points: {
+        type: Number
+    },
+    member_count: {
+        type: Number
+    },
+    createdBy: {
+        type: String
+    },
+    updatedBy: {
+        type: String
+    },
+    lastModified: {
+        type: Date
+    },
+    created_timestamp: {
+        type: Date
+    },
+    statusCode: {
+        type: Number
+    },
+    isWatched: {
+        type: Boolean
+    }
+},{
+    timestamps: true
+});
+
+schema.index({ name: 1 },{name: 'Name'});
+
+let guild_db = mongoose.model('guilds_olds', schema);
+
+module.exports = guild_db;

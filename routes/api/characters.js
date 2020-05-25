146e60dc2e5c9090f9371cb35193074d38be2703
AlexZeDim <AlexZeDim@mail.ru>
2020-05-25 15:23:43 +0300

    API endpoints

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -19,20 +19,22 @@ router.get('/:n@:r', async function(req, res) {
         let {n, r} = req.params;
         let { slug } = await realms_db.findOne({$text:{$search: r}});
         if (n && slug) {
-            let characterData = await characters_db.findById(`${n}@${slug}`).lean();
+            let characterData = await characters_db.findOne({
+                "name": n.toLowerCase(),
+                "realm.slug": slug
+            }).lean();
             if (!characterData || moment(characterData.lastModified).isBefore(moment().subtract(3, 'days'))) {
-                console.log('nok')
                 const getCharacter = require('../../osint/getCharacter');
                 const keys_db = require("../../db/keys_db");
                 const { token } = await keys_db.findOne({tags: `OSINT-indexCharacters`});
                 characterData = await getCharacter(slug, n, {}, token, `OSINT-userInput`, true);
             }
-            res.status(200).json(characterData);
+            await res.status(200).json(characterData);
         } else {
-            res.status(404).json({error: "not found"});
+            await res.status(404).json({error: "not found"});
         }
     } catch (e) {
-        res.status(500).json(e);
+        await res.status(500).json(e);
     }
 });
 

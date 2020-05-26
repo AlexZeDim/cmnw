78355adaeba8ac16a1bf1eea7f96f41ecbc94383
Sasha <AlexZeDim@mail.ru>
2020-05-26 15:51:33 +0300

    items eva added

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,49 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Model importing
+ */
+
 const items_db = require("../../db/items_db");
 const realms_db = require("../../db/realms_db");
+const contracts_db = require("../../db/contracts_db");
+const valuations_db = require("../../db/valuations_db");
+
+/**
+ * Modules
+ */
+
 const charts = require("../../dma/charts.js");
 const auctionsData = require("../../dma/auctions/auctionsQuotes.js");
-const contracts_db = require("../../db/contracts_db");
+
 
 router.get('/:i@:r', async function(req, res) {
     try {
         const {i, r} = req.params;
-        //TODO search as textfields, not sure about index use, if realm
         let api = {};
         let requestPromises = [];
         let asyncPromises = [];
         isNaN(i) ? (requestPromises.push(items_db.findOne({$text:{$search: i}},{score:{$meta:"textScore"}}).sort({score:{$meta:"textScore"}}).lean().exec())) : (requestPromises.push(items_db.findById(i).lean().exec()));
         isNaN(r) ? (requestPromises.push(realms_db.findOne({$text:{$search: r}}).lean().exec())) : (requestPromises.push(realms_db.findById(r).lean().exec()));
         let [item, {connected_realm_id}] = await Promise.all(requestPromises);
-        let {_id, is_auctionable, is_commdty, asset_class, expansion} = item;
+        let {_id, is_auctionable, is_commdty, expansion} = item;
         if (is_auctionable && connected_realm_id) {
             asyncPromises.push(auctionsData(_id, connected_realm_id).then(d => { return {quotes: d[0] , market: d[1]} }));
-            if (is_commdty) {
+            /**
+             * Chart for commodities
+             */
+            if (is_commdty === true) {
                 asyncPromises.push(charts(_id, connected_realm_id).then(r => { return {chart: r} }));
-                if (expansion === 'BFA' && asset_class === 'COMMDTY') {
-                    asyncPromises.push(
-                        contracts_db.find({item_id: _id, connected_realm_id: connected_realm_id, type: 'D'},{
-                            "_id": 1,
-                            "code": 1
-                        }).sort({updatedAt: -1}).limit(5).lean().then(c => { return {contracts_d: c}} )
-                    );
-                }
             } else {
-                //TODO chart if buyout and bid
+                //TODO chart for non commodity
             }
-            if (asset_class !== 'COMMDTY' && asset_class) {
-                
+            /**
+             * Contracts and Valuations
+             */
+            if (is_commdty === true && expansion === 'BFA') {
+                asyncPromises.push(
+                    contracts_db.find({item_id: _id, connected_realm_id: connected_realm_id, type: 'D'},{
+                        "_id": 1,
+                        "code": 1
+                    }).sort({updatedAt: -1}).limit(5).lean().then(c => { return {contracts_d: c}} ),
+                    valuations_db.findById(`${_id}@${connected_realm_id}`).lean().then(v => { return {valuation: v}} )
+                );
+
             }
             for await (let promise of asyncPromises) {
                 Object.assign(api, promise)
             }
         }
         Object.assign(api, {item: item});
-        res.status(200).json(api);
+        await res.status(200).json(api);
     } catch (e) {
-        res.status(404).json(e);
+        await res.status(404).json(e);
     }
 });
 

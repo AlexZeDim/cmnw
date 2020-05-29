450b725777a7a5dea5e87fe199f0e43bb8b4e236
Sasha <AlexZeDim@mail.ru>
2020-05-30 00:40:11 +0300

    WIP

--- routes/api/findAll.js
+++ routes/api/findAll.js
@@ -11,44 +11,42 @@ router.get('/:queryFind', async function(req, res) {
         if (queryFind.includes('@')) {
             const [n, r] = queryFind.split('@');
             let { slug } = await realms_db.findOne({$text:{$search: r}});
-            let character_ = await characters_db.findById(`${n.toLowerCase()}@${slug}`).lean();
-            if (!character_) {
+            let character = await characters_db.findById(`${n.toLowerCase()}@${slug}`).lean();
+            if (!character) {
                 const getCharacter = require('../../osint/getCharacter');
                 const keys_db = require("../../db/keys_db");
                 const { token } = await keys_db.findOne({tags: `OSINT-indexCharacters`});
-                character_ = await getCharacter(slug, n.toLowerCase(), token, true);
-                character_.createdBy = `OSINT-userInput`;
-                character_.updatedBy = `OSINT-userInput`;
-                if (character_.statusCode === 200) {
-                    character_ = await characters_db.create(character_).then(ch => {console.info(`C,${ch._id}`); return ch});
+                character = await getCharacter(slug, n.toLowerCase(), token, true);
+                if (!character) {
+                    await res.status(404).json({error: "not found"});
                 }
-                //TODO what is char not found?
             }
-            let {checksum} = character_;
-            if (checksum["pets"] && checksum["mounts"]) {
+            let {hash} = character;
+            let findQuery = [];
+            if (hash) {
+                for (const [key, value] of Object.entries(hash)) {
+                    findQuery.push({[key]: value})
+                }
                 findAll.match = await characters_db.find({
                     $and: [
-                        {$or: [
-                            {"checksum.pets": checksum.pets},
-                            {"checksum.mounts": checksum.mounts}
-                        ]},
-                        {_id: {$ne: `${n.toLowerCase()}@${slug}`}},
+                        {$or: findQuery},
+                        {_id: {$ne: `${character._id}`}},
                     ]
                 }).lean();
-                findAll._id = character_._id;
-            } else {
-                //TODO char found but no checksum on primary
+                findAll._id = character._id;
             }
         } else {
             findAll.match = await characters_db.find({$or: [
-                {"checksum.pets": queryFind},
-                {"checksum.mounts": queryFind}
-            ]}).lean();
+                    {"hash.a": queryFind},
+                    {"hash.b": queryFind},
+                    {"hash.c": queryFind},
+                ]
+            }).lean();
         }
         findAll._id = queryFind;
-        res.status(200).json(findAll);
+        await res.status(200).json(findAll);
     } catch (e) {
-        res.status(404).json(e);
+        await res.status(404).json(e);
     }
 });
 
--- routes/api/guilds.js
+++ routes/api/guilds.js
@@ -19,17 +19,64 @@ router.get('/:g@:r', async function(req, res) {
         let {g, r} = req.params;
         let { slug } = await realms_db.findOne({$text:{$search: r}});
         if (g && slug) {
-            let guildData = await guilds_db.findOne({
-                "name": g.toLowerCase(),
-                "realm.slug": slug
-            }).lean();
+            let guildData = await guilds_db.findById(`${g.toLowerCase()}@${slug}`).lean();
             if (!guildData || moment(guildData.lastModified).isBefore(moment().subtract(5, 'days'))) {
                 const getGuild = require('../../osint/getGuild');
                 const keys_db = require("../../db/keys_db");
                 const { token } = await keys_db.findOne({tags: `OSINT-indexGuilds`});
                 guildData = await getGuild(slug, g, token, `OSINT-userInput`);
             }
-            await res.status(200).json(guildData);
+            let [json] = await guilds_db.aggregate([
+                {
+                    $match: {
+                        _id: guildData._id
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "characters",
+                        let: {
+                            members: "$members"
+                        },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $in: [
+                                            "$_id",
+                                            "$$members._id"
+                                        ]
+                                    }
+                                }
+                            },
+                            {
+                                $addFields: {
+                                    rank: {
+                                        $reduce: {
+                                            input: "$$members",
+                                            initialValue: null,
+                                            in: {
+                                                $cond: [
+                                                    {
+                                                        $eq: [
+                                                            "$$this._id",
+                                                            "$_id"
+                                                        ]
+                                                    },
+                                                    "$$this.rank",
+                                                    "$$value"
+                                                ]
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        ],
+                        as: "members"
+                    }
+                },
+            ])
+            await res.status(200).json(json);
         } else {
             await res.status(404).json({error: "not found"});
         }

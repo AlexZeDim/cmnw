da888c929f4e276f4755a22a7f83b42cf86526b3
AlexZeDim <AlexZeDim@mail.ru>
2020-06-10 13:06:47 +0300

    fix API response for item

--- dma/auctions/auctionsQuotes.js
+++ dma/auctions/auctionsQuotes.js
@@ -2,34 +2,38 @@ const auctions_db = require("../../db/auctions_db");
 
 async function auctionsQuotes (item_id = 168487, connected_realm_id = 1602) {
     try {
-        const {lastModified} = await auctions_db.findOne({ "item.id": item_id, connected_realm_id: connected_realm_id}).sort({lastModified: -1});
-        return await auctions_db.aggregate([
-            {
-                $match: {
-                    lastModified: lastModified,
-                    "item.id": item_id,
-                    connected_realm_id: connected_realm_id,
+        const latest_lot = await auctions_db.findOne({ "item.id": item_id, connected_realm_id: connected_realm_id}).sort({lastModified: -1});
+        if (latest_lot && latest_lot.lastModified) {
+            return await auctions_db.aggregate([
+                {
+                    $match: {
+                        lastModified: latest_lot.lastModified,
+                        "item.id": item_id,
+                        connected_realm_id: connected_realm_id,
+                    }
+                },
+                {
+                    $project: {
+                        id: "$id",
+                        quantity: "$quantity",
+                        price: { $ifNull: [ "$buyout", { $ifNull: [ "$bid", "$unit_price" ] } ] },
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$price",
+                        quantity: {$sum: "$quantity"},
+                        open_interest: {$sum: { $multiply: [ "$price", "$quantity" ] }},
+                        orders: {$addToSet: "$id"},
+                    }
+                },
+                {
+                    $sort : { "_id": 1 }
                 }
-            },
-            {
-                $project: {
-                    id: "$id",
-                    quantity: "$quantity",
-                    price: { $ifNull: [ "$buyout", { $ifNull: [ "$bid", "$unit_price" ] } ] },
-                }
-            },
-            {
-                $group: {
-                    _id: "$price",
-                    quantity: {$sum: "$quantity"},
-                    open_interest: {$sum: { $multiply: [ "$price", "$quantity" ] }},
-                    orders: {$addToSet: "$id"},
-                }
-            },
-            {
-                $sort : { "_id": 1 }
-            }
-        ]);
+            ]);
+        } else {
+            return void 0
+        }
     } catch (error) {
         console.error(error)
     }

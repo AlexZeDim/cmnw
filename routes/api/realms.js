02ada706bccc8457ac73196d02fb24e205cf8261
AlexZeDim <AlexZeDim@mail.ru>
2020-07-13 12:58:00 +0300

    realms API endpoint and find fix

---
+++ routes/api/realms.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * Model importing
+ */
+
+const realms_db = require("../../db/realms_db");
+
+/**
+ * Modules
+ */
+
+router.get('/:locale', async function(req, res) {
+    try {
+        let { locale } = req.params;
+        let realms = await realms_db.find({locale: locale});
+        if (realms) {
+            await res.status(200).json(realms);
+        } else {
+            await res.status(404).json({error: "not found"});
+        }
+    } catch (e) {
+        await res.status(500).json(e);
+    }
+});
+
+module.exports = router;

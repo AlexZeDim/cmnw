0f83c71738a9a782fc467d5aa1a8d1a2f2f294a2
AlexZeDim <AlexZeDim@mail.ru>
2020-07-18 21:34:15 +0300

    core OAuth 2.0 Battle.net strategy successful

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const passport = require('passport');
+const util = require('util');
+const session = require('express-session');
+
+const BnetStrategy = require('passport-bnet').Strategy;
+const BNET_ID = process.env.BNET_ID
+const BNET_SECRET = process.env.BNET_SECRET
+
+console.log(BNET_ID, BNET_SECRET)
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+// Use the BnetStrategy within Passport.
+passport.use(new BnetStrategy({
+    clientID: BNET_ID,
+    clientSecret: BNET_SECRET,
+    callbackURL: "http://127.0.0.1:3030/auth/bnet/callback",
+    region: "eu"
+}, function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
 const indexRouter = require('./routes/index');
 const characters = require('./routes/api/characters');
 const guilds = require('./routes/api/guilds');
@@ -20,6 +48,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'blizzard',
+    saveUninitialized: true,
+    resave: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/bnet', passport.authenticate('bnet'));
+
+app.get('/auth/bnet/callback',
+    passport.authenticate('bnet', { failureRedirect: '/' }),
+    function(req, res){
+        res.redirect('/');
+    });
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
 app.use('/', indexRouter);
 app.use('/api/characters', characters);

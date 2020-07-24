0f49efae6d03e7af89d34aef7ceedb2496114c26
AlexZeDim <AlexZeDim@mail.ru>
2020-07-24 19:42:19 +0300

    Test path and dotenv

---
+++ uploads/path.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const t = async (p, a) => {
+    try {
+        console.log(`process.env.import`)
+        console.log(process.env.import)
+        console.log(`__dirname`)
+        console.log(__dirname)
+        console.log(`path.normalize(__dirname)`)
+        console.log(path.normalize(__dirname))
+        console.log(`p as process.env.import`)
+        console.log(p)
+        console.log(`a as process.env.a`)
+        console.log(a)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+t(process.env.import, process.env.a)

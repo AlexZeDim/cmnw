70565c50bf01e39d95ebcfbc44dd74e6ef8948d5
AlexZeDim <AlexZeDim@mail.ru>
2020-07-24 20:52:09 +0300

    %oath% test

--- uploads/path.js
+++ uploads/path.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
-const t = async (p, a = 1) => {
+const t = async (a = "test") => {
     try {
+        let path_;
+
+        if (!a.endsWith(".csv")) {
+            a = a + '.csv'
+        }
+
+        if (process.env.PWD) {
+            path_ = path.normalize(`${process.env.PWD}/uploads/${a}`)
+        } else {
+            path_ = __dirname
+        }
+        console.log(path_, a)
         console.log(`process.env.PWD`)
         console.log(process.env.PWD)
+        console.log(__dirname)
         const args = process.argv.slice(2);
         console.log(args);
     } catch (e) {
@@ -11,4 +24,4 @@ const t = async (p, a = 1) => {
     }
 }
 
-t(process.argv.slice(2)[0], process.argv.slice(2)[1])
+t(process.argv.slice(2)[0])

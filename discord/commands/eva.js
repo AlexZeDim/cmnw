69ef7f06b179a97c45d69fb478d73db134c0b042
AlexZeDim <AlexZeDim@mail.ru>
2020-07-27 20:24:17 +0300

    eva endpoint and bot fix

--- discord/commands/eva.js
+++ discord/commands/eva.js
@@ -11,102 +11,66 @@ module.exports = {
         let embed = new MessageEmbed();
         let valuation = await axios.get(encodeURI(`http://${process.env.localhost}:3030/api/eva/${item}@${realm}`)).then(({data}) => {
             let {
-                _id,
-                last_modified,
-                asset_class,
-                derivative,
-                market,
-                reagent,
-                vendor,
                 item,
                 realm,
-                updatedAt
+                valuations
             } = data;
             embed.setTitle(asset_class.toString().replace(/,/g, ' '));
             embed.setAuthor(`${item.name.en_GB}@${realm.name}`.toUpperCase(), '', encodeURI(`https://${process.env.domain}/item/${realm.slug}/${item.name.en_GB}`));
 
             let descriptionString = '';
+            let market_counter = 0;
+            let derivative_counter = 0;
+            let premium_counter = 0;
 
-            if (market.orders && market.orders.length) {
-                descriptionString = descriptionString.concat(`*Market pricing is based on ${market.orders.length} ${market.orders.length === 1 ? 'order' : 'orders'}*\n _ _`)
-            }
             if (derivative && derivative.length) {
                 descriptionString = descriptionString.concat(`*Derivative pricing is based on ${derivative.length} ${derivative.length === 1 ? 'method' : 'methods'}*\n _ _`)
             }
             if (reagent.premium && reagent.premium.length) {
                 descriptionString = descriptionString.concat(`*Premium pricing is based on ${reagent.premium.length} ${reagent.premium.length === 1 ? 'method' : 'methods'}*\n _ _`)
             }
-            embed.setDescription(descriptionString);
+
             embed.setURL(encodeURI(`https://${process.env.domain}/item/${realm.slug}/${item.name.en_GB}`));
             if ("icon" in item) {
                 embed.setThumbnail(item.icon);
             }
-            if (asset_class.some(ac => ac === 'VENDOR') || asset_class.some(ac => ac === 'CONST')) {
-                embed.addFields(
-                    { name: 'Sell', value: `${vendor.sell_price} g`, inline: true },
-                    { name: 'Buy', value: `${vendor.buy_price} g`, inline: true },
-                    { name: '\u200B', value: "\u200B", inline: true },
-                    { name: 'Yield to Market', value: `${vendor.yieldMarket} %`, inline: true },
-                    { name: 'Yield to Reagent', value: `${vendor.yieldReagent} %`, inline: true },
-                );
+
+            for (let valuation of valuations) {
+                if (valuation.type === "MARKET") {
+                    if (valuation.details && valuation.details.orders && valuation.details.orders.length) {
+                        market_counter = valuation.details.orders.length
+                    }
+                }
+                if (valuation.type === "DERIVATIVE") {
+                    derivative_counter += 1;
+                }
+
+                if (valuation.type === "PREMIUM") {
+                    premium_counter += 1;
+                }
+
+                embed.addField(`┌─────────────┐`, `
+                    Name: ${valuation.name}
+                    Type: ${valuation.type}
+                    Value: ${valuation.value}
+                    └─────────────┘
+                    `, true);
             }
-            if ("last_modified" in market) {
-                embed.addField('\u200B', '\u200B');
-                embed.addField('Price', `${market.price} g`, true);
-                embed.addField('Price Size', `${market.price_size} g`, true);
-                embed.addField('Quantity', market.quantity.toLocaleString('en-GB'),true);
-                embed.addField('Open Interest', `${market.open_interest.toLocaleString('ru-RU')} g`, true);
-                if ("yieldReagent" in market) {
-                    embed.addField('Yield to Reagent', `${market.yieldReagent} %`, true);
-                } else {
-                    embed.addField('\u200B', '\u200B', true);
-                }
-                if ("yieldVendor" in market) {
-                    embed.addField('Yield to Vendor', `${market.yieldVendor} %`, true);
-                } else {
-                    embed.addField('\u200B', '\u200B', true);
-                }
+
+            if (derivative_counter) {
+                descriptionString = descriptionString.concat(`*Derivative pricing is based on ${derivative_counter} ${derivative_counter === 1 ? 'method' : 'methods'}*\n _ _`)
             }
-            if (derivative.length) {
-                embed.addField('\u200B', '\u200B');
-                let m;
-                if ("index" in reagent) {
-                    m = derivative[reagent.index]
-                } else {
-                    m = derivative.reduce((prev, curr) => prev.nominal_value < curr.nominal_value ? prev : curr);
-                }
-                embed.addFields(
-                    { name: 'Method', value: `[${m._id}](https://discordapp.com)`, inline: true },
-                    { name: 'Queue Cost', value: `${m.queue_cost} g`, inline: true },
-                    { name: 'Nominal Value', value: `${m.nominal_value} g`, inline: true },
-                );
-                if ("yieldMarket" in m) {
-                    embed.addField('Yield to Market', `${m.yieldMarket} %`, true);
-                } else {
-                    embed.addField('\u200B', '\u200B', true);
-                }
-                if ("yieldVendor" in m) {
-                    embed.addField('Yield to Vendor', `${m.yieldVendor} %`, true);
-                } else {
-                    embed.addField('\u200B', '\u200B', true);
-                }
-                if ("Premium" in m) {
-                    embed.addField('Premium', `${m.premium}`, true);
-                } else {
-                    embed.addField('\u200B', '\u200B', true);
-                }
+
+            if (premium_counter) {
+                descriptionString = descriptionString.concat(`*Premium pricing is based on ${premium_counter} ${premium_counter === 1 ? 'method' : 'methods'}*\n _ _`)
             }
-            if ("value" in reagent) {
-                embed.addField('\u200B', '\u200B');
-                embed.addField('CTD', reagent.name.replace(/^[a-z]/i, str => str.toUpperCase()), true);
-                embed.addField('Value', `${reagent.value} g`, true);
-                if (reagent.premium.length && reagent.name !== "premium") {
-                    embed.addField('Premium Value', `${reagent.p_value}`, true);
-                } else {
-                    embed.addField('\u200B', '\u200B', true);
-                }
+
+            if (market_counter) {
+                descriptionString = descriptionString.concat(`*Market pricing is based on ${market_counter} ${market_counter === 1 ? 'order' : 'orders'}*\n _ _`)
             }
-            embed.setTimestamp(last_modified);
+
+            embed.setDescription(descriptionString);
+            embed.setTimestamp(realm.auctions);
             embed.setFooter(`DMA-XVA`);
             return embed
         });
--- routes/api/eva.js
+++ routes/api/eva.js
@@ -1,25 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Model importing
+ */
+
 const items_db = require("../../db/items_db");
 const realms_db = require("../../db/realms_db");
+const valuations_db = require("../../db/valuations_db");
 
-const itemValuationAdjustment = require("../../dma/valuation/turing/IVA");
+/**
+ * Modules
+ */
+
+const iva = require("../../dma/valuation/eva/IVA");
 
 router.get('/:i@:r', async function(req, res) {
     try {
         const {i, r} = req.params;
+        let response = {};
         let requestPromises = [];
         isNaN(i) ? (requestPromises.push(items_db.findOne({$text:{$search: i}},{score:{$meta:"textScore"}}).sort({score:{$meta:"textScore"}}).lean().exec())) : (requestPromises.push(items_db.findById(i).lean().exec()));
         isNaN(r) ? (requestPromises.push(realms_db.findOne({$text:{$search: r}}).lean().exec())) : (requestPromises.push(realms_db.findById(r).lean().exec()));
         let [item, realm] = await Promise.all(requestPromises);
         if (item && realm) {
-            let iva = await itemValuationAdjustment(item, realm.connected_realm_id);
-            Object.assign(iva, {item: item});
-            Object.assign(iva, {realm: realm});
-            await res.status(200).json(iva);
+            let valuations = await valuations_db.find({item_id: item._id, connected_realm_id: realm.connected_realm_id, last_modified: realm.auctions}).sort("-value").limit(30)
+            if (!valuations.length) {
+                await iva(item, realm.connected_realm_id, realm.auctions, 0, 0)
+                valuations = await valuations_db.find({item_id: item._id, connected_realm_id: realm.connected_realm_id, last_modified: realm.auctions}).sort("-value").limit(30)
+            }
+            Object.assign(response, {valuations: valuations})
+            Object.assign(response, {item: item});
+            Object.assign(response, {realm: realm});
+            await res.status(200).json(response);
         } else {
-            await res.status(404).json({error:"not found"});
+            await res.status(404).json({error:"Not found"});
         }
     } catch (e) {
         await res.status(500).json(e);

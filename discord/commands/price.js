3eecf14d53ef63de06a2ded542fb6dfad14a5531
AlexZeDim <AlexZeDim@mail.ru>
2020-07-31 13:59:28 +0300

    Discord bot refactoring

--- discord/commands/price.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'price',
-    description: 'This command will find price for item',
-    execute(message) {
-        message.channel.send(`This command will find price for item`);
-    },
-};

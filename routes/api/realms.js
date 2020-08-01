ddbae7be49505c28be3be8bb3a908e9d55eebc48
AlexZeDim <AlexZeDim@mail.ru>
2020-08-01 19:39:28 +0300

    minor fix in realms endpoint

--- routes/api/realms.js
+++ routes/api/realms.js
@@ -15,10 +15,10 @@ router.get('/:locale', async function(req, res) {
     try {
         let { locale } = req.params;
         let realms = await realms_db.find({locale: locale});
-        if (realms) {
+        if (realms && realms.length) {
             await res.status(200).json(realms);
         } else {
-            await res.status(404).json({error: "not found"});
+            await res.status(404).json({error: "Not found"});
         }
     } catch (e) {
         await res.status(500).json(e);

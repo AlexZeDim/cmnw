34e9dac80b4371a2c5703d46c5ff413f80093cb2
AlexZeDim <AlexZeDim@mail.ru>
2020-08-01 19:39:07 +0300

    guild and character endpoint

--- routes/api/guilds.js
+++ routes/api/guilds.js
@@ -13,37 +13,46 @@ const {toSlug} = require("../../db/setters");
  * Modules
  */
 
-router.get('/:g@:r', async function(req, res) {
+router.get('/:guildSlug@:realmSlug', async function(req, res) {
     try {
-        let {g, r} = req.params;
-        let { slug } = await realms_db.findOne({$text:{$search: r}});
-        if (g && slug) {
-            let guildData = await guilds_db.findById(`${toSlug(g)}@${slug}`).lean();
-            if (!guildData) {
-                const getGuild = require('../../osint/getGuild');
-                const keys_db = require("../../db/keys_db");
-                const { token } = await keys_db.findOne({tags: `OSINT-indexGuilds`});
-                guildData = await getGuild(slug, toSlug(g), token, `OSINT-userInput`);
+        let {guildSlug, realmSlug} = req.params;
+        guildSlug = toSlug(guildSlug);
+
+        let guildData = await guilds_db.findById(`${guildSlug}@${realmSlug}`).lean();
+
+        if (!guildData) {
+            let realm = await realms_db.findOne({$text:{$search: realmSlug}});
+            if (realm) {
+                guildData = await guilds_db.findById(`${guildSlug}@${realm.slug}`).lean();
+                if (!guildData) {
+                    const getGuild = require('../../osint/getGuild');
+                    const keys_db = require("../../db/keys_db");
+                    const { token } = await keys_db.findOne({tags: `OSINT-indexGuilds`});
+                    await getGuild(realm.slug, guildSlug, token, `OSINT-userInput`);
+                    guildData = await guilds_db.findById(`${guildSlug}@${realm.slug}`).lean();
+                    if (!guildData) {
+                        await res.status(404).json({error: "Not found"});
+                    }
+                }
             }
-            let [json] = await guilds_db.aggregate([
-                {
-                    $match: {
-                        _id: guildData._id
-                    }
-                },
-                {
-                    $lookup: {
-                        from: "characters",
-                        localField: "members._id",
-                        foreignField: "_id",
-                        as: "members"
-                    }
-                },
-            ])
-            await res.status(200).json(json);
-        } else {
-            await res.status(404).json({error: "not found"});
         }
+        /** $lookup join for members */
+        let [json] = await guilds_db.aggregate([
+            {
+                $match: {
+                    _id: guildData._id
+                }
+            },
+            {
+                $lookup: {
+                    from: "characters",
+                    localField: "members._id",
+                    foreignField: "_id",
+                    as: "members"
+                }
+            },
+        ])
+        await res.status(200).json(json);
     } catch (e) {
         await res.status(500).json(e);
     }

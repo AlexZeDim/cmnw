1f855d082435718473587e86bc275170590b2e28
AlexZeDim <AlexZeDim@mail.ru>
2020-08-01 19:39:07 +0300

    guild and character endpoint

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -14,24 +14,33 @@ const characters_db = require("../../db/characters_db");
 
 const moment = require('moment');
 
-router.get('/:n@:r', async function(req, res) {
+router.get('/:nameSlug@:realmSlug', async function(req, res) {
     try {
-        let {n, r} = req.params;
-        let { slug } = await realms_db.findOne({$text:{$search: r}});
-        if (n && slug) {
-            let characterData = await characters_db.findOne({
-                "name": n.toLowerCase(),
-                "realm.slug": slug
-            }).lean();
-            if (!characterData || moment(characterData.lastModified).isBefore(moment().subtract(30, 'days'))) {
-                const getCharacter = require('../../osint/getCharacter');
-                const keys_db = require("../../db/keys_db");
-                const { token } = await keys_db.findOne({tags: `OSINT-indexCharacters`});
-                characterData = await getCharacter(slug, n, {}, token, `OSINT-userInput`, true);
+        let { nameSlug, realmSlug } = req.params;
+        nameSlug = nameSlug.toLowerCase()
+
+        if (nameSlug && realmSlug) {
+            let characterData = await characters_db.findById(`${nameSlug}@${realmSlug}`).lean();
+            if (!characterData) {
+                let realm = await realms_db.findOne({ $text: { $search: realmSlug } }).sort({ score: { $meta: "textScore" } });
+                if (realm) {
+                    let outdated = false;
+                    characterData = await characters_db.findById(`${nameSlug}@${realm.slug}`).lean();
+                    if (characterData && moment(characterData.lastModified).isBefore(moment().subtract(30, 'days'))) {
+                        outdated = true;
+                    }
+                    if (!characterData || outdated) {
+                        const getCharacter = require('../../osint/getCharacter');
+                        const keys_db = require("../../db/keys_db");
+                        const { token } = await keys_db.findOne({tags: `OSINT-indexCharacters`});
+                        await getCharacter(realm.slug, nameSlug, {}, token, `OSINT-userInput`, true);
+                        characterData = await characters_db.findById(`${nameSlug}@${realm.slug}`).lean();
+                    }
+                }
             }
             await res.status(200).json(characterData);
         } else {
-            await res.status(404).json({error: "not found"});
+            await res.status(404).json({error: "Not found"});
         }
     } catch (e) {
         await res.status(500).json(e);

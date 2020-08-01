a7b10b337264939bea0847263fbfc5b232b78a99
AlexZeDim <AlexZeDim@mail.ru>
2020-08-01 19:39:42 +0300

    wowtoken endpoint formatting

--- routes/api/wowtoken.js
+++ routes/api/wowtoken.js
@@ -14,11 +14,11 @@ const wowtoken_db = require("../../db/wowtoken_db");
 router.get('/:region', async function(req, res) {
     try {
         let { region } = req.params;
-        let wowtoken = await wowtoken_db.findOne({region: region}).lean().sort({_id: -1});
+        let wowtoken = await wowtoken_db.findOne({ region: region }).sort({ _id: -1 }).lean();
         if (wowtoken) {
             await res.status(200).json(wowtoken);
         } else {
-            await res.status(404).json({error: "not found"});
+            await res.status(404).json({error: "Not found"});
         }
     } catch (e) {
         await res.status(500).json(e);

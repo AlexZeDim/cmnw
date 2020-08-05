24e937c0d5df444f0995df0d588a7b24c6fe609c
AlexZeDim <AlexZeDim@mail.ru>
2020-08-05 16:44:33 +0300

    CORE API middleware itemRealmQuery test passed

---
+++ routes/api/middleware.js
@@ -0,0 +1,43 @@
+/**
+ * Model importing
+ */
+
+const items_db = require("../../db/items_db");
+const realms_db = require("../../db/realms_db");
+
+/**
+ *
+ * @param item
+ * @param realm
+ * @returns {Promise<[{_id: number},{connected_realm_id: number, auctions: number}]>}
+ * @constructor
+ */
+const itemRealmQuery = async (item, realm) => {
+    try {
+
+        let itemQuery, realmQuery;
+
+        if (isNaN(item)) {
+            itemQuery = items_db.findOne({ $text: { $search: item } }).lean();
+        } else {
+            itemQuery = items_db.findById(parseInt(item)).lean();
+        }
+
+        if (isNaN(realm)) {
+            realmQuery = realms_db.findOne({ $text: { $search: realm } }).lean();
+        } else {
+            realmQuery = realms_db.findById(parseInt(realm)).lean();
+        }
+
+        return await Promise.all([
+            await itemQuery,
+            await realmQuery
+        ])
+
+    } catch (error) {
+        console.error(error);
+        return void 0
+    }
+}
+
+module.exports = itemRealmQuery;

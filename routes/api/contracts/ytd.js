d612f7c5436b3600172ec3e69072e50a97198b6d
AlexZeDim <AlexZeDim@mail.ru>
2020-08-05 16:44:01 +0300

    CORE api contract endpoints

---
+++ routes/api/contracts/ytd.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * Model importing
+ */
+
+const contracts_db = require("../../../db/contracts_db");
+
+/**
+ * Modules
+ */
+
+const moment = require('moment');
+const itemRealmQuery = require('../middleware')
+
+router.get('/:itemName@:realmSlug', async (req, res) => {
+    try {
+        let { itemName, realmSlug } = req.params;
+
+        const [item, realm] = await itemRealmQuery(itemName, realmSlug)
+
+        if (item && realm) {
+            let contracts = await contracts_db.find({item_id: item._id, connected_realm_id: realm.connected_realm_id, "date.day": moment().subtract(1,'day').get('date')}).lean();
+            await res.status(200).json({
+                item: item,
+                realm: realm,
+                contracts: contracts
+            });
+        }
+    } catch (e) {
+        await res.status(404).json(e);
+    }
+});
+
+module.exports = router;

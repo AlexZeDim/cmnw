09de598dc8908560f7652a74160c8dd85515c7e7
AlexZeDim <AlexZeDim@mail.ru>
2020-08-05 18:11:37 +0300

    CORS enables for eva and API fix

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const character = require('./routes/api/characters/character');
 const character_logs = require('./routes/api/characters/character_logs');
 const guild = require('./routes/api/guilds/guild');
 const guild_logs = require('./routes/api/guilds/guild_logs');
-const items = require('./routes/api/items');
+const items = require('./routes/api/items/item');
 const find = require('./routes/api/find');
-const contracts = require('./routes/api/contracts/tod');
-const eva = require('./routes/api/eva');
+const contracts_tod = require('./routes/api/contracts/tod');
+const contracts_ytd = require('./routes/api/contracts/ytd');
+const contracts_week = require('./routes/api/contracts/week');
+const contracts_lastWeek = require('./routes/api/contracts/last_week');
+const contracts_month = require('./routes/api/contracts/month');
+const contracts_lastMonth = require('./routes/api/contracts/last_month');
+const eva = require('./routes/api/items/eva');
 const realms = require('./routes/api/realms');
 const wowtoken = require('./routes/api/wowtoken');
 const golds = require('./routes/api/golds');
@@ -21,13 +27,19 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 /** Contracts */
 
-app.use('/api/contracts/tod', contracts);
+app.use('/api/contracts/tod', contracts_tod);
+app.use('/api/contracts/ytd', contracts_ytd);
+app.use('/api/contracts/week', contracts_week);
+app.use('/api/contracts/last_week', contracts_lastWeek);
+app.use('/api/contracts/month', contracts_month);
+app.use('/api/contracts/last_month', contracts_lastMonth);
 
 /** Characters */
 
@@ -39,10 +51,17 @@ app.use('/api/characters/character_logs', character_logs);
 app.use('/api/guilds/guild', guild);
 app.use('/api/guilds/guild_logs', guild_logs);
 
+/** Items */
 
-app.use('/api/items', items);
+app.use('/api/items/item', items);
+app.use('/api/items/eva', cors(), eva);
+
+/** Find */
+
 app.use('/api/find', find);
-app.use('/api/eva', eva);
+
+/** Others */
+
 app.use('/api/realms', realms);
 app.use('/api/wowtoken', wowtoken);
 app.use('/api/golds', golds);

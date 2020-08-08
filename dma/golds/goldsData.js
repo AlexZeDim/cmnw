ceb7a5035e58c1cedde55821e5aac43419cc8e9c
AlexZeDim <AlexZeDim@mail.ru>
2020-08-08 12:55:43 +0300

    auctions and gold endpoint data to one standard

--- dma/golds/goldsData.js
+++ dma/golds/goldsData.js
@@ -1,4 +1,5 @@
 const golds_db = require("../../db/golds_db");
+const realms_db = require("../../db/realms_db");
 
 /**
  * @param connected_realm_id
@@ -7,14 +8,14 @@ const golds_db = require("../../db/golds_db");
 
 async function goldsQuotes (connected_realm_id = 1602) {
     try {
-        const t = await golds_db.findOne({ connected_realm_id: connected_realm_id}).select('last_modified').lean().sort({last_modified: -1});
+        const t = await realms_db.findOne({ connected_realm_id: connected_realm_id }).select('golds').lean();
         if (t) {
             return await golds_db.aggregate([
                 {
                     $match: {
                         status: "Online",
                         connected_realm_id: connected_realm_id,
-                        last_modified: t.last_modified,
+                        last_modified: t.golds,
                     }
                 },
                 {
@@ -22,17 +23,28 @@ async function goldsQuotes (connected_realm_id = 1602) {
                         id: "$id",
                         quantity: "$quantity",
                         price: "$price",
+                        owner: "$owner"
                     }
                 },
                 {
                     $group: {
                         _id: "$price",
-                        quantity: {$sum: "$quantity"},
-                        open_interest: {$sum: { $multiply: [ "$price", "$quantity" ] }},
+                        quantity: { $sum: "$quantity" },
+                        open_interest: { $sum: { $multiply: [ "$price", "$quantity" ] } },
+                        sellers: { $addToSet: "$owner" },
                     }
                 },
                 {
                     $sort : { "_id": 1 }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        price: "$_id",
+                        quantity: "$quantity",
+                        open_interest: "$open_interest",
+                        size: { $cond: { if: { $isArray: "$sellers" }, then: { $size: "$sellers" }, else: 0} }
+                    }
                 }
             ]);
         } else {

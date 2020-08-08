05aaa515115646dd2e09e41e3d339ed311558160
AlexZeDim <AlexZeDim@mail.ru>
2020-08-08 19:00:33 +0300

    updated models, TINA rollover point

--- db/messages_db.js
+++ db/messages_db.js
@@ -2,18 +2,28 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 let schema = new mongoose.Schema({
-    _id: {
+    author: String,
+    mentions: Array,
+    type: {
         type: String,
+        enum: ['profile', 'message'],
+        default: 'message'
     },
-    message: {
-        type: {
-            type: String,
-            enum: ['profile', 'message'],
-            default: 'message'
-        },
-        clearance: String,
-        context: String,
-    }
+    clearance: [
+        {
+            _id: false,
+            access: {
+                type: Number,
+                enum: [0, 1, 2, 3],
+                default: 0
+            },
+            codeword: {
+                type: String,
+                default: 'WoW',
+            }
+        }
+    ],
+    context: String,
 },{
     timestamps: true
 });

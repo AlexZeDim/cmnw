13fb0a5a147f5eba844438705492524fab23b8ca
AlexZeDim <AlexZeDim@mail.ru>
2020-08-10 01:57:46 +0300

    _itemBonusList.js

---
+++ dma/items/_itemBonusList.js
@@ -0,0 +1,90 @@
+/**
+ * Connection with DB
+ */
+
+const {connect, connection} = require('mongoose');
+require('dotenv').config();
+connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    bufferMaxEntries: 0,
+    retryWrites: true,
+    useCreateIndex: true,
+    w: "majority",
+    family: 4
+});
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => console.log('Connected to database on ' + process.env.hostname));
+
+/**
+ * Model importing
+ */
+
+const auctions_db = require("../../db/auctions_db");
+
+/**
+ * Modules
+ */
+
+
+/**
+ * This function parse items across B.net API with wrapper
+ * @returns {Promise<void>}
+ */
+
+async function _itemBonusList () {
+    try {
+        console.time(`DMA-${_itemBonusList.name}`);
+
+        const item = await auctions_db.aggregate([
+            {
+                $match: {
+                    'item.id': 175010
+                }
+            },
+            {
+                $limit: 1,
+            },
+            {
+                $lookup: {
+                    from: "bonus_lists",
+                    localField: "item.bonus_lists",
+                    foreignField: "_id",
+                    as: "item.bonus_lists"
+                }
+            },
+            {
+                $lookup: {
+                    from: "items",
+                    localField: "item.id",
+                    foreignField: "_id",
+                    as: "fromItems"
+                }
+            },
+            {
+                $addFields: {
+                    fromItems: { $arrayElemAt: [ "$fromItems", 0 ] },
+                }
+            },
+            {
+                $addFields: {
+                     item: { $mergeObjects: [ "$fromItems" , "$item"] },
+                }
+            },
+            {
+                $project: {
+                    fromItems: 0
+                }
+            }
+        ])
+        console.log(item[0].item);
+        connection.close();
+        console.timeEnd(`DMA-${_itemBonusList.name}`);
+    } catch (err) {
+        console.error(`${_itemBonusList.name},${err}`);
+    }
+}
+
+_itemBonusList();

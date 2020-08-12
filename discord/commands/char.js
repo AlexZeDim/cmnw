75f540b7c58876399c22f7954f22a185d33e2eb7
AlexZeDim <AlexZeDim@mail.ru>
2020-08-12 22:06:14 +0300

    discord: character

--- discord/commands/char.js
+++ discord/commands/char.js
@@ -1,9 +1,9 @@
 const { MessageEmbed } = require('discord.js');
 const axios = require('axios');
+const humanizeString = require('humanize-string');
 require('dotenv').config();
 
 /***
- * TODO refactor for Object.keys
  * @type {{args: boolean, name: string, description: string, execute(*, *): Promise<void>}}
  */
 module.exports = {
@@ -17,21 +17,9 @@ module.exports = {
         let character = await axios.get(encodeURI(`http://${process.env.localhost}:3030/api/characters/character/${name}@${realm}`)).then(({data}) => {
             let {
                 _id,
-                id,
-                lastModified,
+                name,
                 guild,
-                character_class,
-                level,
                 realm,
-                ilvl,
-                spec,
-                gender,
-                faction,
-                hash,
-                createdBy,
-                race,
-                media,
-                statusCode,
             } = data;
             if (guild) {
                 let guild_string = guild.name.toString().toUpperCase()
@@ -39,36 +27,43 @@ module.exports = {
                     guild_string = guild_string.concat(` // ${guild.rank === 0 ? 'GM' : 'R' + guild.rank}`)
                 }
                 embed.setTitle(guild_string);
+                embed.setURL(encodeURI(`https://${process.env.domain}/guild/${realm.slug}/${guild.name}`));
             }
             embed.setAuthor(_id.toUpperCase(), '', encodeURI(`https://${process.env.domain}/character/${realm.slug}/${name}`))
-            embed.setURL(encodeURI(`https://${process.env.domain}/guild/${realm.slug}/${guild.name}`));
-            if (media) {
-                embed.setThumbnail(media.avatar_url);
-            }
-            if (statusCode === 200) embed.addField('LVL', level, true);
-            if (ilvl) {
-                embed.addField('Equipped', ilvl.avg, true);
-                embed.addField('Actual', ilvl.eq, true);
-            }
-            if (faction === "Alliance") {
-                embed.setColor('#006aff');
-            }
-            if (faction === "Horde") {
-                embed.setColor('#ff0000');
-            }
-            if (statusCode === 200) {
-                embed.addField('Class', character_class, true);
-                embed.addField('Spec', spec, true);
-                embed.addField('Race', `${race}, ${gender[0]}`, true);
-            }
-            if (hash.a && hash.b && hash.ex) {
-                embed.addField('ID', id, true);
-                embed.addField('Hash A', hash.a, true);
-                embed.addField('Hash B', hash.b, true);
-                embed.addField('Hash EX', hash.ex, true);
-            }
-            embed.setTimestamp(lastModified);
-            embed.setFooter(`${createdBy} | Gonikon`);
+
+            const fieldsToCheck = ['id', 'character_class', 'spec', 'level', 'hash', 'ilvl', 'faction', 'media', 'race', 'gender', 'lastModified', 'createdBy']
+
+            fieldsToCheck.map(field => {
+                if (field in data) {
+                    if (typeof data[field] === 'object') {
+                        if (field === 'hash') {
+                            delete data[field].t
+                        }
+                        if (field === 'media') {
+                            embed.setThumbnail(data[field].avatar_url);
+                        } else {
+                            Object.entries(data[field]).map(([k, v]) => {
+                                embed.addField(`${humanizeString(field)} ${humanizeString(k)}`, v, true);
+                            });
+                        }
+                    } else {
+                        if (field === 'faction') {
+                            if (data[field] === "Alliance") {
+                                embed.setColor('#006aff');
+                            } else if (data[field] === "Horde") {
+                                embed.setColor('#ff0000');
+                            }
+                        } else if (field === 'lastModified') {
+                            embed.setTimestamp(data[field])
+                        } else if (field === 'createdBy') {
+                            embed.setFooter(`${data[field]} | Gonikon`);
+                        } else {
+                            embed.addField(humanizeString(field.replace('character_', '')), data[field], true);
+                        }
+
+                    }
+                }
+            })
             return embed
         });
         await message.channel.send(character);

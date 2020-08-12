41863df8f27b23c8f485db4aeedfad05798d62e1
AlexZeDim <AlexZeDim@mail.ru>
2020-08-12 22:32:28 +0300

    find command fix

--- discord/commands/findA.js
+++ discord/commands/findA.js
@@ -26,27 +26,34 @@ module.exports = {
                 let { _id, match } = data;
                 embed.setTitle(_id.toUpperCase());
                 embed.setURL(`https://${process.env.domain}/find/a/${query}`);
-                for (let i = 0; i < match.length; i++) {
-                    if (i === 19 ) {
+                if (match && match.length) {
+                    for (let i = 0; i < match.length; i++) {
+                        if (i === 19) {
+                            embed.addField(`─────────────`, `
+                            Want 
+                            More
+                            To
+                            Find?
+                            [Conglomerat](https://${process.env.domain}/find/a/${query})
+                            ─────────────
+                            `, true);
+                            break
+                        }
+                        let {guild} = match[i];
                         embed.addField(`─────────────`, `
-                        Want 
-                        More
-                        To
-                        Find?
-                        [Conglomerat](https://${process.env.domain}/find/a/${query})
+                        Name: [${match[i].name}](https://${process.env.domain}/character/${match[i].realm.slug}/${match[i].name})
+                        ${("realm" in match[i]) ? `Realm: ${match[i].realm.name}` : ``} 
+                        ${("faction" in match[i]) ? `Faction: ${match[i].faction}` : ``} 
+                        ${guild ? `Guild: [${guild.name}](https://${process.env.domain}/guild/${match[i].realm.slug}/${guild.slug})` : ``} 
+                        ${(guild && typeof guild.rank !== 'undefined') ? `Rank: ${parseInt(guild.rank) === 0 ? 'GM' : `R${guild.rank}`}` : ``} 
                         ─────────────
                         `, true);
-                        break
                     }
-                    let {guild} = match[i];
-                    embed.addField(`┌─────────────┐`, `
-                    Name: [${match[i].name}](https://${process.env.domain}/character/${match[i].realm.slug}/${match[i].name})
-                    ${("realm" in match[i]) ? `Realm: ${match[i].realm.name}` : ``} 
-                    ${("faction" in match[i]) ? `Faction: ${match[i].faction}` : ``} 
-                    ${guild ? `Guild: [${guild.name}](https://${process.env.domain}/guild/${match[i].realm.slug}/${guild.slug})` : ``} 
-                    ${(guild && typeof guild.rank !== 'undefined') ? `Rank: ${parseInt(guild.rank) === 0 ? 'GM' : `R${guild.rank}`}` : ``} 
-                    └─────────────┘
-                    `, true);
+                } else {
+                    embed.addField(`─────────────`, `
+                    No match found
+                    ─────────────
+                    `);
                 }
                 embed.setFooter(`OSINT-DB`);
                 return embed
--- discord/commands/findAll.js
+++ discord/commands/findAll.js
@@ -15,7 +15,7 @@ module.exports = {
         let embed = new MessageEmbed();
         let endpoint;
         if (query) {
-            endpoint = `http://${process.env.localhost}:3030/api/find/and/${query}`
+            endpoint = `http://${process.env.localhost}:3030/api/find/all/${query}`
             if (realm_arg) {
                 endpoint = endpoint.concat(`@${realm_arg}`)
             }
@@ -23,27 +23,34 @@ module.exports = {
                 let { _id, match } = data;
                 embed.setTitle(_id.toUpperCase());
                 embed.setURL(`https://${process.env.domain}/find/and/${query}`);
-                for (let i = 0; i < match.length; i++) {
-                    if (i === 19 ) {
+                if (match && match.length) {
+                    for (let i = 0; i < match.length; i++) {
+                        if (i === 19 ) {
+                            embed.addField(`─────────────`, `
+                                Want 
+                                More
+                                To
+                                Find?
+                                [Conglomerat](https://${process.env.domain}/find/and/${query})
+                                ─────────────
+                                `, true);
+                                break
+                            }
+                        let {guild} = match[i];
                         embed.addField(`─────────────`, `
-                        Want 
-                        More
-                        To
-                        Find?
-                        [Conglomerat](https://${process.env.domain}/find/and/${query})
-                        ─────────────
-                        `, true);
-                        break
+                            Name: [${match[i].name}](https://${process.env.domain}/character/${match[i].realm.slug}/${match[i].name})
+                            ${("realm" in match[i]) ? `Realm: ${match[i].realm.name}` : ``} 
+                            ${("faction" in match[i]) ? `Faction: ${match[i].faction}` : ``} 
+                            ${guild ? `Guild: [${guild.name}](https://${process.env.domain}/guild/${match[i].realm.slug}/${guild.slug})` : ``} 
+                            ${(guild && typeof guild.rank !== 'undefined') ? `Rank: ${parseInt(guild.rank) === 0 ? 'GM' : `R${guild.rank}`}` : ``} 
+                            ─────────────
+                            `, true);
                     }
-                    let {guild} = match[i];
-                    embed.addField(`┌─────────────┐`, `
-                    Name: [${match[i].name}](https://${process.env.domain}/character/${match[i].realm.slug}/${match[i].name})
-                    ${("realm" in match[i]) ? `Realm: ${match[i].realm.name}` : ``} 
-                    ${("faction" in match[i]) ? `Faction: ${match[i].faction}` : ``} 
-                    ${guild ? `Guild: [${guild.name}](https://${process.env.domain}/guild/${match[i].realm.slug}/${guild.slug})` : ``} 
-                    ${(guild && typeof guild.rank !== 'undefined') ? `Rank: ${parseInt(guild.rank) === 0 ? 'GM' : `R${guild.rank}`}` : ``} 
-                    └─────────────┘
-                    `, true);
+                } else {
+                    embed.addField(`─────────────`, `
+                    No match found
+                    ─────────────
+                    `);
                 }
                 embed.setFooter(`OSINT-DB`);
                 return embed

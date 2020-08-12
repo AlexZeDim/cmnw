2a384f723f34c32181a44120155a85bd222e0e42
AlexZeDim <AlexZeDim@mail.ru>
2020-08-12 22:17:52 +0300

    new contracts with aggregation wip

--- routes/api/contracts/last_month.js
+++ routes/api/contracts/last_month.js
@@ -21,10 +21,38 @@ router.get('/:itemName@:realmSlug', async (req, res) => {
         const [item, realm] = await itemRealmQuery(itemName, realmSlug)
 
         if (item && realm) {
-            let contracts = await contracts_db.find({item_id: item._id, connected_realm_id: realm.connected_realm_id, "date.month": moment().subtract(1,'month').get('month')+1}).lean();
+            const [contracts, data] = await contracts_db.aggregate([
+                {
+                    $match: {
+                        item_id: item._id,
+                        connected_realm_id: realm.connected_realm_id,
+                        "date.month": moment().subtract(1,'month').get('month')+1
+                    }
+                },
+                {
+                    $group: {
+                        _id: null,
+                        price_minimum: { $min: '$price' },
+                        price_average: { $avg: '$price' },
+                        price_maximum: { $max: '$price' },
+                        price_standard_deviation: { $stdDevPop: "$price" },
+                        quantity_minimum: { $min: '$quantity' },
+                        quantity_average: { $avg: '$quantity' },
+                        quantity_maximum: { $max: '$quantity' },
+                        contracts: { $push: '$$ROOT'}
+                    }
+                }
+            ]).then((aggregate) => {
+                let data = aggregate[0];
+                let contract = data.contracts
+                delete data._id
+                delete data.contracts
+                return [contract, data]
+            })
             await res.status(200).json({
                 item: item,
                 realm: realm,
+                snapshot: data,
                 contracts: contracts
             });
         }

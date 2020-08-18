5317be4f574dcb98075bdf4279c4aa489129b229
AlexZeDim <AlexZeDim@mail.ru>
2020-08-18 05:49:09 +0300

    xrs_item.js test endpoint new auctionsFeed

--- routes/api/items/xrs_item.js
+++ routes/api/items/xrs_item.js
@@ -11,6 +11,7 @@ const items_db = require("../../../db/items_db");
  * Modules
  */
 
+const auctionsFeed = require("../../../dma/auctions/auctionsFeed.js");
 const ClusterChartCrossRealmData = require("../../../dma/getClusterChartCrossRealmData.js");
 
 router.get('/:itemQuery', async function(req, res) {
@@ -23,8 +24,24 @@ router.get('/:itemQuery', async function(req, res) {
             item = await items_db.findById(itemQuery).lean()
         );
         if (item) {
-            Object.assign(response, {item: item})
-            await ClusterChartCrossRealmData(item._id).then(chart => Object.assign(response, {chart: chart}));
+            let is_commdty = false;
+
+            Object.assign(response, { item: item })
+
+            if (item.asset_class && item.asset_class.includes('COMMDTY')) {
+                is_commdty = true;
+            } else {
+                if (item.stackable && item.stackable > 1) {
+                    is_commdty = true;
+                }
+            }
+
+            if (is_commdty) {
+                await ClusterChartCrossRealmData(item._id).then(chart => Object.assign(response, { chart: chart }));
+            } else {
+                await auctionsFeed(item._id).then(feed => Object.assign(response, { feed: feed }))
+            }
+
             await res.status(200).json(response);
         } else {
             await res.status(404).json({error: "Not found"});

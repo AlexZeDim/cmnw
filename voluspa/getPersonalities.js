833ac9a52327e3670f4367e4afe6793f5681f2bb
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-08-22 05:32:22 +0300

    VOLUSPA indexes and getPersonalities

--- voluspa/getPersonalities.js
+++ voluspa/getPersonalities.js
@@ -30,9 +30,9 @@ connection.once('open', () =>
 const characters_db = require('../db/characters_db');
 const personalities_db = require('../db/personalities_db');
 
-async function T() {
+async function getPersonalities() {
   try {
-    console.time(`DMA-${T.name}`);
+    console.time(`VOLUSPA-${getPersonalities.name}`);
 
     await characters_db
       .aggregate([
@@ -49,6 +49,7 @@ async function T() {
               hash_c: '$hash.c',
             },
             characters: { $addToSet: '$_id' },
+            personality: { $addToSet: '$personality' },
             guild: {
               $addToSet: {
                 $concat: ['$guild.slug', '@', '$realm.slug'],
@@ -61,10 +62,11 @@ async function T() {
       .cursor({ batchSize: 10 })
       .exec()
       .eachAsync(
-        identity => {
+        async identity => {
+          let persona;
           /** Clearance by guild */
           let clearance = identity.guild.map(g => ({
-            access: 1,
+            access: 0,
             codeword: g,
           }));
           /** Default clearance */
@@ -72,25 +74,39 @@ async function T() {
             access: 0,
             codeword: 'WoW',
           });
-          /** Character aliases */
-          let aliases = identity.characters.map(a => ({
-            type: 'character',
-            value: a,
-          }));
-          let persona = new personalities_db({
-                codename: 'Unknown',
-                clearance: clearance,
-                aliases: aliases
+          /** Check is such persona ID exists */
+          if (identity.personality.length) {
+            /** If personas more then one, then report it */
+            if (identity.personality.length > 1) {
+              console.error(`E,${identity.personality.length},${identity._id.realm},${identity._id.hash_a},${identity._id.hash_c}`)
+            }
+            /** Find persona by ID */
+            persona = await personalities_db.findById(identity.personality[0])
+          } else {
+            /** Create new persona and save it */
+            persona = new personalities_db({
+              codename: 'Unknown',
+              clearance: clearance
             })
-          persona.save();
+            await persona.save()
+          }
+          let characters = await characters_db.updateMany(
+            { _id: { $in: identity.characters } },
+            { $set: { personality : persona._id } }
+          )
+          let flag = 'E'
+          if (persona.isNew) {
+            flag = 'C'
+          }
+          console.log(`${flag},${persona._id},${characters.n},${characters.nModified}`)
         },
         { parallel: 10 },
       );
     connection.close();
-    console.timeEnd(`DMA-${T.name}`);
+    console.timeEnd(`VOLUSPA-${getPersonalities.name}`);
   } catch (err) {
-    console.error(`${T.name},${err}`);
+    console.error(`${getPersonalities.name},${err}`);
   }
 }
 
-T();
+getPersonalities();

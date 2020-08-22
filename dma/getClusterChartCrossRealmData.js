1a522b2bb0e6457a8063cffef164a4c2703f26b1
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-08-22 06:40:16 +0300

    limit 25!

--- dma/getClusterChartCrossRealmData.js
+++ dma/getClusterChartCrossRealmData.js
@@ -28,7 +28,7 @@ async function auctionsCrossRealmData(item_id = 168487) {
           },
         },
         {
-          $limit: 35
+          $limit: 25
         },
         {
           $project: {

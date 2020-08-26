16ac4957ed8bbed9cd424a9a0b70919dc4efb521
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-08-26 12:41:22 +0300

    valuations_db TTL

--- db/valuations_db.js
+++ db/valuations_db.js
@@ -44,7 +44,7 @@ let schema = new mongoose.Schema(
   },
 );
 
-schema.index({ createdAt: -1 }, { name: 'TTL', expireAfterSeconds: 604800 });
+schema.index({ createdAt: -1 }, { name: 'TTL', expireAfterSeconds: 86400 });
 schema.index(
   { item_id: -1, last_modified: -1, connected_realm_id: 1 },
   { name: 'IVA' },

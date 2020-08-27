3b1b85e9f12707bcc6aa07c2c092615b41f08d21
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-08-27 14:08:56 +0300

    asset_Classes description

--- dma/items/indexAssetClass.js
+++ dma/items/indexAssetClass.js
@@ -43,6 +43,10 @@ async function indexAssetClass(arg = 'pricing_methods', bulkSize = 10) {
     console.time(`DMA-${indexAssetClass.name}`);
     switch (arg) {
       case 'pricing_methods':
+        /**
+         * This stage add asset_classes from pricing_methods
+         * such as REAGENT / DERIVATIVE
+         */
         console.info(`Stage: pricing methods`);
         console.time(`Stage: pricing methods`);
         await pricing_methods_db
@@ -99,6 +103,10 @@ async function indexAssetClass(arg = 'pricing_methods', bulkSize = 10) {
           );
         console.timeEnd(`Stage: pricing methods`);
       case 'auctions':
+        /**
+         * This stage add asset_classes from auction_db
+         * such as COMMDTY / ITEM and MARKET
+         */
         console.info(`Stage: auctions`);
         console.time(`Stage: auctions`);
         await auctions_db
@@ -142,6 +150,10 @@ async function indexAssetClass(arg = 'pricing_methods', bulkSize = 10) {
           );
         console.timeEnd(`Stage: auctions`);
       case 'contracts':
+        /**
+         * This stage check does item suits the
+         * contract criteria based on asset class
+         */
         console.info(`Stage: contracts`);
         console.time(`Stage: contracts`);
         await items_db.updateMany({}, { contracts: false });
@@ -160,6 +172,10 @@ async function indexAssetClass(arg = 'pricing_methods', bulkSize = 10) {
         );
         console.timeEnd(`Stage: contracts`);
       case 'items':
+        /**
+         * This stage define to items a special asset_class called PREMIUM
+         * based on loot_type and asset_class: REAGENT
+         */
         console.info(`Stage: items`);
         console.time(`Stage: items`);
         await items_db.updateMany(
@@ -169,6 +185,9 @@ async function indexAssetClass(arg = 'pricing_methods', bulkSize = 10) {
         console.timeEnd(`Stage: items`);
         break;
       case 'currency':
+        /**
+         * This stage define CURRENCY and WOWTOKEN asset classes to GOLD / WOWTOKEN
+         */
         console.info(`Stage: currency`);
         console.time(`Stage: currency`);
         await items_db.updateOne(

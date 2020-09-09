27008cb92888e8077ed69c40fd215346867ecec1
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-10 00:13:03 +0300

    remove comments in DBs

--- db/auctions_db.js
+++ db/auctions_db.js
@@ -1,18 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-/*require('dotenv').config();
-mongoose.connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    bufferMaxEntries: 0,
-    retryWrites: true,
-    useCreateIndex: true,
-    w: "majority",
-    family: 4
-});*/
-
 let schema = new mongoose.Schema(
   {
     id: {
--- db/contracts_db.js
+++ db/contracts_db.js
@@ -3,18 +3,6 @@ mongoose.Promise = global.Promise;
 
 const { Round2 } = require('./setters');
 
-/*require('dotenv').config();
-mongoose.connect(`mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    bufferMaxEntries: 0,
-    retryWrites: true,
-    useCreateIndex: true,
-    w: "majority",
-    family: 4
-});*/
-
 let schema = new mongoose.Schema(
   {
     _id: {

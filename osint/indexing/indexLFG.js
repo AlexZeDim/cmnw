a4e074a79d0384ab04eeb51101f39f1010798bae
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-10 22:36:31 +0300

    puppeteer evaluate logs for all

--- osint/indexing/indexLFG.js
+++ osint/indexing/indexLFG.js
@@ -114,8 +114,8 @@ const { toSlug } = require('../../db/setters');
               await page.goto(`https://www.warcraftlogs.com/character/eu/${character.realm.slug}/${character.name}`);
               const [getXpath] = await page.$x('//div[@class=\'best-perf-avg\']/b');
               const bestPrefAvg = await page.evaluate(name => name.innerText, getXpath);
+              console.log(bestPrefAvg)
               if (bestPrefAvg) {
-                console.log(bestPrefAvg)
                 character.wcl_percentile = parseFloat(bestPrefAvg)
               }
               character.updatedBy = 'OSINT-LFG'

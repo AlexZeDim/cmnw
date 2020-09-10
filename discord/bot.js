1fc1de91d3dafdf64aa55669df2c39a9ec950bc0
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-10 20:44:46 +0300

    recruiting command

--- discord/bot.js
+++ discord/bot.js
@@ -1,7 +1,45 @@
+/**
+ * Connection with DB
+ */
+
+const { connect, connection } = require('mongoose');
 require('dotenv').config();
+connect(
+  `mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    bufferMaxEntries: 0,
+    retryWrites: true,
+    useCreateIndex: true,
+    w: 'majority',
+    family: 4,
+  },
+);
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () =>
+  console.log('Connected to database on ' + process.env.hostname),
+);
+
+/**
+ * Model importing
+ */
+
+const discord_db = require('../db/discord_db')
+const characters_db = require('../db/characters_db')
+
+/**
+ * Modules
+ */
+
 const path = require('path');
 const fs = require('fs');
 const Discord = require('discord.js');
+const schedule = require('node-schedule');
+
+/** Init Bot Client */
 
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
@@ -9,6 +47,8 @@ bot.commands = new Discord.Collection();
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
+
+/** Init list of commands */
 
 const commandFiles = fs
   .readdirSync(path.normalize(`${__dirname}/commands/`))
@@ -74,6 +114,91 @@ bot.on('message', async message => {
   } catch (error) {
     console.error(error);
     await message.reply('There was an error trying to execute that command!');
+  }
+});
+
+schedule.scheduleJob('05/10 * * * *', async function() {
+  try {
+    await discord_db
+      .find({})
+      .lean()
+      .cursor({ batchSize: 10 })
+      .eachAsync(
+        async ({ _id, channel, coverage }) => {
+          let guild = await bot.guilds.cache.get(_id)
+          if (guild) {
+            let guild_channel = await guild.channels.cache.get(channel.id);
+            if (guild_channel) {
+              let query = { isWatched: true, updatedBy: 'OSINT-LFG-NEW' }
+              if (coverage) {
+                Object.keys(coverage).forEach(key => {
+                  if (key === 'ilvl') {
+                    Object.assign(query, {'ilvl.avg': { '$gte': coverage[key]}})
+                  } else {
+                    Object.assign(query, {'realm.slug': coverage[key]})
+                  }
+                })
+              }
+              const LFG_NEW = await characters_db.find(query).limit(3)
+              if (LFG_NEW && LFG_NEW.length) {
+                let embed = new Discord.MessageEmbed();
+                embed.setTitle(`Mark II`);
+                for (let character_lfg of LFG_NEW) {
+                  embed.addField(
+                    `─────────────`,
+                    `
+                            Name: [${character_lfg.name}](https://${
+                      process.env.domain
+                    }/character/${character_lfg.realm.slug}/${character_lfg.name})
+                            ${
+                      'realm' in character_lfg
+                        ? `Realm: ${character_lfg.realm.name}`
+                        : ``
+                    } 
+                            ${
+                      'faction' in character_lfg
+                        ? `Faction: ${character_lfg.faction}`
+                        : ``
+                    } 
+                            ${
+                      character_lfg.wcl_percentile
+                        ? `Best Perf. Avg: ${character_lfg.wcl_percentile}`
+                        : ``
+                    } 
+                            ${
+                      character_lfg.ilvl
+                        ? `Item Level: ${character_lfg.ilvl.avg}`
+                        : ``
+                    } 
+                            ${
+                      character_lfg.guild && character_lfg.guild.name
+                        ? `Guild: [${character_lfg.guild.name}](https://${process.env.domain}/guild/${character_lfg.realm.slug}/${character_lfg.guild.slug})`
+                        : ``
+                    } 
+                            ${
+                      character_lfg.guild && typeof character_lfg.guild.rank !== 'undefined'
+                        ? `Rank: ${
+                          parseInt(character_lfg.guild.rank) === 0
+                            ? 'GM'
+                            : `R${character_lfg.guild.rank}`
+                        }`
+                        : ``
+                    } 
+                            ─────────────
+                            `,
+                    true,
+                  );
+                }
+                embed.setFooter(`OSINT-LFG`);
+                guild_channel.send(embed)
+              }
+            }
+          }
+        },
+        { parallel: 10 },
+      );
+  } catch (e) {
+    console.error(e)
   }
 });
 

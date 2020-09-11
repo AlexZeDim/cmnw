ab57da1c6b01eac001466edd8dc66e88ec5f2040
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-11 12:51:43 +0300

    discord remove tabs at all

--- discord/commands/findAll.js
+++ discord/commands/findAll.js
@@ -49,11 +49,11 @@ module.exports = {
             let { guild } = match[i];
             embed.addField(`─────────────`,
               `Name: [${match[i].name}](https://${process.env.domain}/character/${match[i].realm.slug}/${match[i].name})
-        ${'realm' in match[i] ? `Realm: ${match[i].realm.name}` : ``} 
-        ${'faction' in match[i] ? `Faction: ${match[i].faction}` : ``} 
-        ${guild ? `Guild: [${guild.name}](https://${process.env.domain}/guild/${match[i].realm.slug}/${guild.slug})` : ``} 
-        ${guild && typeof guild.rank !== 'undefined' ? `Rank: ${parseInt(guild.rank) === 0 ? 'GM' : `R${guild.rank}`}` : ``} 
-        ─────────────`,
+${'realm' in match[i] ? `Realm: ${match[i].realm.name}` : ``} 
+${'faction' in match[i] ? `Faction: ${match[i].faction}` : ``} 
+${guild ? `Guild: [${guild.name}](https://${process.env.domain}/guild/${match[i].realm.slug}/${guild.slug})` : ``} 
+${guild && typeof guild.rank !== 'undefined' ? `Rank: ${parseInt(guild.rank) === 0 ? 'GM' : `R${guild.rank}`}` : ``} 
+ ─────────────`,
               true,
             );
           }

b6e2c45f1376a88c009911cfff2de6d4e92b6eda
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-14 21:38:53 +0300

    discord new recruiting and bot

--- discord/commands/recruiting.js
+++ discord/commands/recruiting.js
@@ -1,59 +1,79 @@
+require('dotenv').config();
+const parse_arguments = require('../parse_arguments')
+const { capitalize } = require('../../db/setters')
+
 const discord_db = require('../../db/discord_db')
 const realms_db = require('../../db/realms_db')
-require('dotenv').config();
 
 module.exports = {
   name: 'recruiting',
   description:
     'Subscribes Discord server and selected channel (requires channel_id, *uses current channel by default*) for announcements of characters from Kernel\'s WoWProgress, which have quene in Looking for Guild recently. ' +
-    'Also, you could clarify the request, by adding realm and item level. Example `recruiting -ch channel_number -realm twisted-nether -ilvl 450`. To update or modify the parameters just re-type the command with necessary filters. ' +
+    'Also, you could clarify the request, by adding realm and item level. ' +
+    '*If realm\'s name consist of two words, please remove space between by `-` symbol* Example `recruiting -ch channel_number -realm twisted-nether -ilvl 450`. To update or modify the parameters just re-type the command with necessary filters. ' +
     'To unsubscribe from updates, use: `recruiting -rm`',
   aliases: ['recruiting', 'recruting', 'Recruiting', 'Recruting', 'RECRUTING', "RECRUITING"],
-  cooldown: 60,
+  cooldown: 30,
   guildOnly: true,
   args: true,
   async execute(message, args) {
-    let params;
+    let params, is_deleted, filters = {};
     let notification = 'Your subscription has been successfully updated';
-    let channel = {
-      id: message.channel.id,
-      name: message.channel.name,
-    };
     let discord_server = await discord_db.findById(message.channel.guild.id)
+    /** If we don't have discord, then we create it */
     if (!discord_server) {
       discord_server = new discord_db({
         _id: message.channel.guild.id,
         name: message.channel.guild.name,
-        coverage: {}
+        channels: [{
+          _id: message.channel.id,
+          name: message.channel.name
+        }]
+
       })
       notification = 'You have been successfully subscribed';
+    } else {
+      discord_server.channels.addToSet({
+        _id: message.channel.id,
+        name: message.channel.name
+      })
     }
-    /** Set channelID */
+    /** Arguments */
     if (args) {
-      params = args.split(' ');
-      if (params.includes('-ch')) {
-        channel.id = params[params.indexOf('-ch') + 1];
-        let { name } = await message.channel.guild.channels.cache.get(channel.id);
-        channel.name = name;
-      }
-      if (params.includes('-realm')) {
-        let filter_realm = params[params.indexOf('-realm') + 1]
-        if (filter_realm) {
-          let realm = await realms_db.findOne({
-            $text: { $search: filter_realm },
+      /** realm */
+      let realm = parse_arguments(params, ['-realm', '-realms', '-r'])
+      if (realm) {
+        if (capitalize(realm) === 'All') {
+          delete filters.realm
+        } else {
+          let realm_ = await realms_db.findOne({
+            $text: { $search: realm },
           });
-          if (realm && discord_server) {
-            discord_server.coverage.realm = realm.slug
+          if (realm) {
+            filters.realm = realm_.slug
           }
         }
       }
-      if (params.includes('-ilvl')) {
-        let filter_ilvl = parseInt(params[params.indexOf('-ilvl') + 1]);
-        if (filter_ilvl && discord_server) {
-          discord_server.coverage.ilvl = filter_ilvl
+      /** item level */
+      let ilvl = parse_arguments(params, ['-ilvl', '-item_level', '-item-level'])
+      if (!isNaN(ilvl)) {
+        filters.ilvl = parseInt(ilvl)
+      }
+      /** faction */
+      let faction = parse_arguments(params, ['-faction', '-f'])
+      if (faction) {
+        faction = capitalize(faction)
+        if (faction === 'Alliance' || faction === 'A') {
+          filters.faction = 'Alliance'
+        }
+        if (faction === 'Horde' || faction === 'H') {
+          filters.faction = 'Horde'
         }
       }
-      if (params.includes('-rm')) {
+      /** remove */
+      let remove = parse_arguments(params, ['-rm', '-remove', '-d', '-delete'])
+      if (remove) {
+        is_deleted = true;
         if (discord_server) {
           await discord_db.findByIdAndRemove(message.channel.guild.id)
           notification = 'Your server has been successfully unsubscribed';
@@ -64,8 +84,18 @@ module.exports = {
         }
       }
     }
-    discord_server.channel = channel;
-    await discord_server.save()
+
+    let channel_index = discord_server.channels.findIndex(o => o._id === message.channel.id);
+    if (channel_index === -1) {
+      //TODO exception
+    } else {
+      //TODO for of
+      discord_server.channels[channel_index].filters
+    }
+
+    if (!is_deleted) {
+      await discord_server.save()
+    }
     return message.channel.send(notification);
   },
 };

a7f6e803f89ce8c023ba9f408cf33100345ec015
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-14 21:38:53 +0300

    discord new recruiting and bot

--- discord/bot.js
+++ discord/bot.js
@@ -29,13 +29,15 @@ connection.once('open', () =>
 
 const discord_db = require('../db/discord_db')
 const characters_db = require('../db/characters_db')
+const { fromSlug } = require('../db/setters')
 
 /**
  * Modules
  */
 
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
 const Discord = require('discord.js');
 const schedule = require('node-schedule');
 
@@ -124,40 +126,75 @@ schedule.scheduleJob('05/10 * * * *', async function() {
       .lean()
       .cursor({ batchSize: 10 })
       .eachAsync(
-        async ({ _id, channel, coverage }) => {
+        async ({ _id, channels }) => {
           let guild = await bot.guilds.cache.get(_id)
           if (guild) {
-            let guild_channel = await guild.channels.cache.get(channel.id);
-            if (guild_channel) {
-              let query = { isWatched: true, updatedBy: 'OSINT-LFG-NEW' }
-              if (coverage) {
-                Object.keys(coverage).forEach(key => {
-                  if (key === 'ilvl') {
-                    Object.assign(query, {'ilvl.avg': { '$gte': coverage[key]}})
-                  } else {
-                    Object.assign(query, {'realm.slug': coverage[key]})
+            for (let channel of channels) {
+              let guild_channel = await guild.channels.cache.get(channel.id);
+              if (guild_channel) {
+                let query = { isWatched: true, updatedBy: 'OSINT-LFG-NEW' }
+                if (channel.filters) {
+                  for (const property in channel.filters) {
+                    if (property === 'realm') {
+                      Object.assign(query, {'realm.slug': channel.filters[property]})
+                    }
+                    if (property === 'faction') {
+                      Object.assign(query, {'faction': channel.filters[property]})
+                    }
+                    if (property === 'ilvl') {
+                      Object.assign(query, {'ilvl.avg': { '$gte': channel.filters[property]}})
+                    }
                   }
-                })
-              }
-              const LFG_NEW = await characters_db.find(query).limit(10)
-              if (LFG_NEW && LFG_NEW.length) {
-                let embed = new Discord.MessageEmbed();
-                embed.setTitle(`WOWPROGRESS LFG`);
-                for (let character_lfg of LFG_NEW) {
-                  embed.addField(`─────────────`, `:page_with_curl: [WCL](https://www.warcraftlogs.com/character/eu/${character_lfg.realm.slug}/${character_lfg.name}) :speech_left: [WP](https://www.wowprogress.com/character/eu/${character_lfg.realm.slug}/${character_lfg.name}) :key: [RIO](https://raider.io/characters/eu/${character_lfg.realm.slug}/${character_lfg.name})
+                }
+                const LFG_NEW = await characters_db.find(query).limit(10)
+                if (LFG_NEW && LFG_NEW.length) {
+                  let embed = new Discord.MessageEmbed();
+                  embed.setTitle(`WOWPROGRESS LFG`);
+                  for (let character_lfg of LFG_NEW) {
+
+                    let raid_progress = '', raider_io_score;
+
+                    await axios.get(encodeURI(`https://raider.io/api/v1/characters/profile?region=eu&realm=howling-fjord&name=Акулов&fields=mythic_plus_scores_by_season:current,raid_progression`)).then(responce => {
+                      console.log(responce.data)
+                      if (responce.data) {
+                        if ('raid_progression' in responce.data) {
+                          let raid_progression = responce.data.raid_progression;
+                          if (raid_progression) {
+                            for (const property in raid_progression) {
+                              raid_progress += `${fromSlug(property)}: ${raid_progression[property].summary} \n`
+                            }
+                          }
+                        }
+                        if ('mythic_plus_scores_by_season' in responce.data) {
+                          let rio_score = responce.data.mythic_plus_scores_by_season
+                          if (rio_score && rio_score.length) {
+                            for (let rio of rio_score) {
+                              if ('scores' in rio) {
+                                raider_io_score = rio.scores.all
+                              }
+                            }
+                          }
+                        }
+                      }
+                    })
+                    //TODO wowprogress battle.tag and days
+                    embed.addField(`─────────────`, `:page_with_curl: [WCL](https://www.warcraftlogs.com/character/eu/${character_lfg.realm.slug}/${character_lfg.name}) :speech_left: [WP](https://www.wowprogress.com/character/eu/${character_lfg.realm.slug}/${character_lfg.name}) :key: [RIO](https://raider.io/characters/eu/${character_lfg.realm.slug}/${character_lfg.name})
 Name: [${character_lfg.name}](https://${process.env.domain}/character/${character_lfg.realm.slug}/${character_lfg.name})
 ${'realm' in character_lfg ? `Realm: ${character_lfg.realm.name}` : ``} 
 ${'faction' in character_lfg ? `Faction: ${character_lfg.faction}` : ``} 
 ${'ilvl' in character_lfg ? `Item Level: ${character_lfg.ilvl.avg}` : ``} 
 ${'character_class' in character_lfg ? `Class: ${character_lfg.character_class}` : ``} 
+${raider_io_score ? `RIO: ${raider_io_score}` : ``} 
+${raid_progress !== '' ? `${raid_progress}` : ``} 
 ${character_lfg.wcl_percentile ? `Best Perf. Avg: ${character_lfg.wcl_percentile}` : ``} 
 ${character_lfg.guild && character_lfg.guild.name ? `Guild: [${character_lfg.guild.name}](https://${process.env.domain}/guild/${character_lfg.realm.slug}/${character_lfg.guild.slug})` : ``} 
 ${character_lfg.guild && typeof character_lfg.guild.rank !== 'undefined' ? `Rank: ${parseInt(character_lfg.guild.rank) === 0 ? 'GM' : `R${character_lfg.guild.rank}`}` : ``} 
 ─────────────`, true,
-                  );
+                    );
+                  }
+                  embed.setFooter(`OSINT-LFG | Сакросантус | Форжспирит`);
+                  guild_channel.send(embed)
                 }
-                embed.setFooter(`OSINT-LFG | Сакросантус | Форжспирит`);
-                guild_channel.send(embed)
               }
             }
           }

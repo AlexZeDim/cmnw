544ad9219eee124b90adedd7cce2cdab5c67291f
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-15 15:56:20 +0300

    characters_db wrong schema fix, number=>string type for battle.tag

--- db/characters_db.js
+++ db/characters_db.js
@@ -108,7 +108,7 @@ let schema = new mongoose.Schema(
       default: false,
     },
     lfg: {
-      battle_tag: Number,
+      battle_tag: String,
       rio: Number,
       days_from: Number,
       days_to: Number,

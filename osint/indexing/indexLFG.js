c5e6702863d450051982f645f4c67643a3c72ef5
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-17 21:47:00 +0300

    OSINT indexLFG check exist

--- osint/indexing/indexLFG.js
+++ osint/indexing/indexLFG.js
@@ -92,127 +92,118 @@ const { toSlug } = require('../../db/setters');
         }
         /** Find character in OSINT DB */
         if (character_name && character_realm) {
+          await getCharacter(
+            character_realm,
+            character_name,
+            {},
+            token,
+            `OSINT-LFG`,
+            true,
+          )
           let character = await characters_db.findById(`${toSlug(character_name)}@${character_realm}`)
-          /** Create it, if it doesn't exist */
-          if (!character) {
-            await getCharacter(
-              character_realm,
-              character_name,
-              {},
-              token,
-              `OSINT-LFG`,
-              true,
-            );
-            character = await characters_db.findById(`${toSlug(character_name)}@${character_realm}`)
-          } else {
-            await getCharacter(
-              character_realm,
-              character_name,
-              {},
-              token,
-              `OSINT-LFG`,
-              true,
-            )
-          }
-          /** If we have something to compare with */
-          if (exist_flag) {
-            let player_flag = OSINT_LFG.some(({name, realm}) => (name === character.name && realm.slug === character.realm.slug));
-            if (player_flag) {
-              character.updatedBy = 'OSINT-LFG'
-            } else {
-              /** Evaluate Logs Performance */
-              const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-              const page = await browser.newPage();
-              await page.goto(`https://www.warcraftlogs.com/character/eu/${character.realm.slug}/${character.name}#difficulty=5`);
-              const [getXpath] = await page.$x('//div[@class=\'best-perf-avg\']/b');
-              const bestPrefAvg = await page.evaluate(name => name.innerText, getXpath);
-              if (bestPrefAvg && bestPrefAvg !== '-') {
-                character.lfg.wcl_percentile = parseFloat(bestPrefAvg)
-              }
-              await browser.close();
+          if (character) {
+            /** If we have something to compare with */
+            if (exist_flag) {
+              let player_flag = OSINT_LFG.some(({name, realm}) => (name === character.name && realm.slug === character.realm.slug));
+              if (player_flag) {
+                character.updatedBy = 'OSINT-LFG'
+              } else {
+                /** Evaluate Logs Performance */
+                const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+                const page = await browser.newPage();
+                await page.goto(`https://www.warcraftlogs.com/character/eu/${character.realm.slug}/${character.name}#difficulty=5`);
+                const [getXpath] = await page.$x('//div[@class=\'best-perf-avg\']/b');
+                const bestPrefAvg = await page.evaluate(name => name.innerText, getXpath);
+                if (bestPrefAvg && bestPrefAvg !== '-') {
+                  character.lfg.wcl_percentile = parseFloat(bestPrefAvg)
+                }
+                await browser.close();
 
-              /** Request wow_progress and RIO for m+, pve progress and contacts */
-              await Promise.allSettled([
-                await axios.get(encodeURI(`https://raider.io/api/v1/characters/profile?region=eu&realm=${character.realm.slug}&name=${character.name}&fields=mythic_plus_scores_by_season:current,raid_progression`)).then(response => {
-                  if (response.data) {
-                    if ('raid_progression' in response.data) {
-                      let raid_progress = response.data.raid_progression;
-                      let pve_progress = {};
-                      for (const [key, value] of Object.entries(raid_progress)) {
-                        pve_progress[key] = value.summary
+                /** Request wow_progress and RIO for m+, pve progress and contacts */
+                await Promise.allSettled([
+                  await axios.get(encodeURI(`https://raider.io/api/v1/characters/profile?region=eu&realm=${character.realm.slug}&name=${character.name}&fields=mythic_plus_scores_by_season:current,raid_progression`)).then(response => {
+                    if (response.data) {
+                      if ('raid_progression' in response.data) {
+                        let raid_progress = response.data.raid_progression;
+                        let pve_progress = {};
+                        for (const [key, value] of Object.entries(raid_progress)) {
+                          pve_progress[key] = value.summary
+                        }
+                        character.lfg.progress = pve_progress;
                       }
-                      character.lfg.progress = pve_progress;
-                    }
-                    if ('mythic_plus_scores_by_season' in response.data) {
-                      let rio_score = response.data.mythic_plus_scores_by_season
-                      if (rio_score && rio_score.length) {
-                        for (let rio of rio_score) {
-                          if ('scores' in rio) {
-                            character.lfg.rio = rio.scores.all
+                      if ('mythic_plus_scores_by_season' in response.data) {
+                        let rio_score = response.data.mythic_plus_scores_by_season
+                        if (rio_score && rio_score.length) {
+                          for (let rio of rio_score) {
+                            if ('scores' in rio) {
+                              character.lfg.rio = rio.scores.all
+                            }
                           }
                         }
                       }
                     }
-                  }
-                }).catch(e => e),
-                await x(
-                  encodeURI(`https://www.wowprogress.com/character/eu/${character.realm.slug}/${character.name}`),
-                  '.registeredTo',
-                  ['.language']
-                ).then(wow_progress => {
-                  if (wow_progress && wow_progress.length) {
-                    for (let info of wow_progress) {
-                      let [key, value] = info.split(':')
-                      if (key === 'Battletag') {
-                        character.lfg.battle_tag = value;
-                      }
-                      if (key === 'Looking for guild') {
-                        if (value.includes('ready to transfer')) {
-                          character.lfg.transfer = true;
+                  }).catch(e => e),
+                  await x(
+                    encodeURI(`https://www.wowprogress.com/character/eu/${character.realm.slug}/${character.name}`),
+                    '.registeredTo',
+                    ['.language']
+                  ).then(wow_progress => {
+                    if (wow_progress && wow_progress.length) {
+                      for (let info of wow_progress) {
+                        let [key, value] = info.split(':')
+                        if (key === 'Battletag') {
+                          character.lfg.battle_tag = value;
                         }
-                        if (value.includes('without transfer')) {
-                          character.lfg.transfer = false;
+                        if (key === 'Looking for guild') {
+                          if (value.includes('ready to transfer')) {
+                            character.lfg.transfer = true;
+                          }
+                          if (value.includes('without transfer')) {
+                            character.lfg.transfer = false;
+                          }
                         }
-                      }
-                      if (key === 'Raids per week') {
-                        if (value.includes(' - ')) {
-                          let [from, to] = value.split(' - ');
-                          character.lfg.days_from = parseInt(from);
-                          character.lfg.days_to = parseInt(to)
+                        if (key === 'Raids per week') {
+                          if (value.includes(' - ')) {
+                            let [from, to] = value.split(' - ');
+                            character.lfg.days_from = parseInt(from);
+                            character.lfg.days_to = parseInt(to)
+                          }
                         }
-                      }
-                      if (key === 'Specs playing') {
-                        character.lfg.role = value
+                        if (key === 'Specs playing') {
+                          character.lfg.role = value
+                        }
                       }
                     }
-                  }
-                })
-              ])
-
-              /** Update alias and codename for persona_db */
-              if (character.personality) {
-                let persona = await persona_db.findById(character.personality)
-                if (persona && character.lfg.battle_tag) {
-                  persona.aliases.addToSet({
-                    type: 'battle.tag',
-                    value: character.lfg.battle_tag
                   })
-                  if (!persona.codename) {
-                    persona.codename = character.lfg.battle_tag.toString().split('#')[0]
+                ])
+
+                /** Update alias and codename for persona_db */
+                if (character.personality) {
+                  let persona = await persona_db.findById(character.personality)
+                  if (persona && character.lfg.battle_tag) {
+                    persona.aliases.addToSet({
+                      type: 'battle.tag',
+                      value: character.lfg.battle_tag
+                    })
+                    if (!persona.codename) {
+                      persona.codename = character.lfg.battle_tag.toString().split('#')[0]
+                    }
+                    console.info(`U,${persona._id},${character.lfg.battle_tag}`)
+                    await persona.save();
                   }
-                  console.info(`U,${persona._id},${character.lfg.battle_tag}`)
-                  await persona.save();
                 }
+
+                /** Update status */
+                character.updatedBy = 'OSINT-LFG-NEW'
               }
-
-              /** Update status */
-              character.updatedBy = 'OSINT-LFG-NEW'
             }
-          }
 
-          character.isWatched = true;
-          await character.save()
-          console.info(`U,${character._id},${character.updatedBy}`)
+            character.isWatched = true;
+            await character.save()
+            console.info(`U,${character._id},${character.updatedBy}`)
+          } else {
+            console.info(`E,${character_name},${character_name}`)
+          }
         }
       }
     }

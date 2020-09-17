208719ace6e455b2618720bbf69a8f3f1449bccf
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-09-17 13:52:26 +0300

    discord whoami.js

--- discord/commands/whoami.js
+++ discord/commands/whoami.js
@@ -4,8 +4,6 @@ module.exports = {
     'Prints the effective username and ID of the current user. Check this [article](https://en.wikipedia.org/wiki/Whoami) for more info.',
   aliases: ['WHOAMI'],
   execute(message) {
-    message.channel.send(
-      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`,
-    );
+    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`,);
   },
 };

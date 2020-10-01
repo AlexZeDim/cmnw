d78fcf3dabdedd956248f208cf96b48839d9a8aa
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-10-01 09:28:24 +0300

    Export connection to MongoDB

---
+++ db/connection.js
@@ -0,0 +1,23 @@
+/**
+ * Connection with DB
+ */
+
+const { connect, connection } = require('mongoose');
+require('dotenv').config();
+connect(
+  `mongodb://${process.env.login}:${process.env.password}@${process.env.hostname}/${process.env.auth_db}`,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    bufferMaxEntries: 0,
+    useCreateIndex: true,
+    w: 'majority',
+    family: 4,
+  },
+).then(r => r);
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () =>
+  console.log('Connected to database on ' + process.env.hostname),
+);

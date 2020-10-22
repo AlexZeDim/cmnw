ad247238725fef678659f93af539a45a7f18556c
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-10-22 16:42:11 +0300

    OSINT index chars

--- osint/characters/index_characters.js
+++ osint/characters/index_characters.js
@@ -20,7 +20,7 @@ const getCharacter = require('./get_character');
  * @returns {Promise<void>}
  */
 
-schedule.scheduleJob('0 0/4 * * *', async (
+schedule.scheduleJob('50 15 */7 * *', async (
   t,
   queryFind = {},
   queryKeys = { tags: `OSINT-indexCharacters` },

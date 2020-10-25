daf1cf2250d1145d8d6729f80d6cf8bfd1c52d9b
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-10-25 07:17:47 +0300

    GraphQL init

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const character_db = require('/db/models/characters_db')
 const character = require('./routes/api/characters/character');
 const character_logs = require('./routes/api/characters/character_logs');
 const character_lfg = require('./routes/api/characters/lfg');
@@ -33,6 +37,52 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/** GraphQL */
+
+
+const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  type Query {
+    getDie(numSides: Int): RandomDie
+  }
+`);
+
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    let output = [];
+    for (let i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
+
+const root = {
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  }
+}
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 /** Contracts */
 

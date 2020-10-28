c7148b7364a7b01baee305926e4616e3b0b11be5
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-10-28 12:01:49 +0300

    debug osint index

--- osint/characters/index_characters_200.js
+++ osint/characters/index_characters_200.js
@@ -29,7 +29,6 @@ const getCharacter = require('./get_character');
     let { token } = await keys_db.findOne(queryKeys);
     let c = 0;
     const array = [];
-    let info_string = '';
     const characters = await characters_db
       .find(queryFind, { timeout: false })
       .maxTimeMS(0)
@@ -37,31 +36,19 @@ const getCharacter = require('./get_character');
       .lean()
       .cursor()
       .addCursorFlag('noCursorTimeout',true)
-    characters.on('data', async ({ _id, realm }) => {
-      const name = _id.split('@')[0]
-      info_string += `${c} ${name}@${realm.slug}\n`;
-      array.push(getCharacter({ name: name, realm: { slug: realm.slug }, updatedBy: `OSINT-indexCharacters` }, token, false, false, c))
+    for await (const { _id, realm } of characters) {
+      const name = _id.split("@")[0]
+      array.push(getCharacter({ name: name, realm: realm, updatedBy: `OSINT-indexCharacters` }, token, false, false, c))
       if (array.length >= 10) {
-        await characters.pause()
-        console.log(`===================`)
-        console.info(info_string)
-        console.log(`===================`)
         await Promise.allSettled(array)
-        await characters.resume()
         array.length = 0
-        info_string = ''
       }
       c++
-    })
-    characters.on('error', async (error) => {
-      console.error(c, error)
-      console.timeEnd(`OSINT-indexCharacters`);
-    })
-    characters.on('close', async () => {
-      console.timeEnd(`OSINT-indexCharacters`);
-      process.exit(0)
-    })
+    }
   } catch (error) {
     console.error(error);
+  } finally {
+    console.timeEnd(`OSINT-indexCharacters`);
+    process.exit(0)
   }
 })();

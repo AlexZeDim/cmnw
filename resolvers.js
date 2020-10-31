a99bc16a07fe6d9682f99231d920887036145a6e
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-10-31 15:20:18 +0300

    graphQL extend Item (optional: feed)

--- resolvers.js
+++ resolvers.js
@@ -3,10 +3,13 @@ const guild_db = require('./db/models/guilds_db');
 const realms_db = require('./db/models/realms_db');
 const osint_logs_db = require('./db/models/osint_logs_db');
 const keys_db = require('./db/models/keys_db');
+const valuations_db = require('./db/models/valuations_db');
+const auctions_db = require('./db/models/auctions_db');
 
 const getCharacter = require('./osint/characters/get_character');
 const getGuild = require('./osint/guilds/get_guild')
 const queryItemAndRealm = require('./routes/api/handle_item_realm');
+const iva = require('./dma/valuations/eva/iva.js');
 const clusterChartData = require('./dma/valuations/cluster/cluster_chart.js');
 const auctionsFeed = require('./dma/auctions/auctions_feed.js');
 const auctionsData = require('./dma/auctions/auctions_data.js');
@@ -63,7 +66,8 @@ const root = {
     }
     return guild
   },
-  item: async ({ id }) => {
+  item: async ({ id, valuations, chart, feed }) => {
+
     if (!id.includes('@')) {
       return
     }
@@ -74,6 +78,7 @@ const root = {
     }
     /** Commodity Block */
     let is_commdty = false;
+    const arrayPromises = [];
     if (item.asset_class && item.asset_class.includes('COMMDTY')) {
       is_commdty = true;
     } else {
@@ -87,6 +92,82 @@ const root = {
     if (item._id === 1) {
       is_commdty = true;
     }
+    /** End of Commodity block */
+
+    if (is_commdty) {
+      if (chart) {``
+        arrayPromises.push(
+          clusterChartData(item._id, realm.connected_realm_id).then(chart =>
+            Object.assign(item, { chart: chart }),
+          ),
+        );
+      }
+
+    } else {
+      if (feed) {
+        item.feed = await auctions_db.aggregate([
+          {
+            $match: {
+              'item.id': item._id,
+              'connected_realm_id': realm.connected_realm_id,
+              'last_modified': realm.auctions,
+            },
+          },
+          {
+            $limit: 2000
+          },
+          {
+            $lookup: {
+              from: 'realms',
+              localField: 'connected_realm_id',
+              foreignField: 'connected_realm_id',
+              as: 'connected_realm_id',
+            },
+          },
+          {
+            $addFields: {
+              max_last_modified: {
+                $arrayElemAt: ['$connected_realm_id.auctions', 0],
+              },
+            },
+          },
+          {
+            $match: {
+              $expr: { $eq: ['$last_modified', '$max_last_modified'] },
+            },
+          },
+          {
+            $addFields: {
+              connected_realm_id: '$connected_realm_id.name_locale',
+            },
+          },
+        ])
+      }
+    }
+
+    if (valuations) {
+      let valuations_ = await valuations_db
+        .find({
+          item_id: item._id,
+          connected_realm_id: realm.connected_realm_id,
+          last_modified: { $gte: realm.auctions },
+        })
+        .sort('value');
+      if (!valuations_.length) {
+        await iva(item, realm.connected_realm_id, realm.auctions, 0);
+        valuations_ = await valuations_db
+          .find({
+            item_id: item._id,
+            connected_realm_id: realm.connected_realm_id,
+            last_modified: { $gte: realm.auctions },
+          })
+          .sort('value');
+      }
+      item.valuations = valuations_;
+    }
+    item.realm = realm;
+    await Promise.allSettled(arrayPromises);
+    return item
   }
 }
 

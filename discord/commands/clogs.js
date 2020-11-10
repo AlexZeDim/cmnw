ae646404aadf68efa07c3573cd8acbaf6a712d06
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-11-10 12:56:00 +0300

    core-discord: character_logs

--- discord/commands/clogs.js
+++ discord/commands/clogs.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   name: 'clogs',
   description:
-    'Search for last 20 events about certain character in OSINT Logs. Like: race, gender, faction change, etc. Realm transfers with rename are not covered. Example usage: `clogs азгримм@soulflayer ` ',
+    'Search for last 20 events about certain character in OSINT Logs. Like: race, gender, faction change, etc. Realm transfers with rename are not covered. Example usage: `clogs азгримм@soulflayer` ',
   aliases: [
     'character_logs',
     'CLOGS',
@@ -15,21 +15,36 @@ module.exports = {
   ],
   args: true,
   async execute(message, args) {
-    const [name, realm] = args.split('@');
-    let embed = new MessageEmbed();
-    let character_logs = await axios
-      .get(
-        encodeURI(
-          `http://${process.env.localhost}:3030/api/characters/character_logs/${name}@${realm}`,
-        ),
-      )
-      .then(({ data }) => {
-        if (data.length) {
-          embed.setTitle(`${name}@${realm}`.toString().toUpperCase());
-          embed.setURL(
-            `https://${process.env.domain}/character/${realm}/${name}`,
-          );
-          for (let i = 0; i < data.length; i++) {
+    const id = args;
+    const embed = new MessageEmbed();
+    await axios.post('http://localhost:4000/graphql', {
+      query: `query Character($id: ID!) {
+          character(id: $id) {
+            _id
+            name
+            realm {
+              _id
+              name
+              slug
+            }
+            logs {
+              type
+              original_value
+              new_value
+              message
+              action
+              before
+              after
+            }
+          }  
+        }`,
+      variables: { id },
+    }).then(({ data: { data: { character } } }) => {
+      const { _id, name, realm, logs } = character;
+        if (logs.length) {
+          embed.setTitle(`${name}@${realm.name}`.toString().toUpperCase());
+          embed.setURL(`https://${process.env.domain}/character/${_id}`,);
+          for (let i = 0; i < logs.length; i++) {
             if (i === 19) {
               embed.addField(
         `─────────────`,
@@ -38,7 +53,7 @@ module.exports = {
                 Log
                 Available
                 At
-                [Conglomerat](https://${process.env.domain}/character/${realm}/${data[i].root_id})
+                [Conglomerat](https://${process.env.domain}/character/${_id})
                 ─────────────`,
         true,
               );
@@ -47,18 +62,17 @@ module.exports = {
             embed.addField(
         `─────────────`,
         `
-              Event: ${data[i].action}
-              Message: ${data[i].message}
-              After: ${new Date(data[i].after).toLocaleString('en-GB')}
-              Before: ${new Date(data[i].before).toLocaleString('en-GB')}
+              Event: ${logs[i].action}
+              Message: ${logs[i].message}
+              After: ${new Date(logs[i].after).toLocaleString('en-GB')}
+              Before: ${new Date(logs[i].before).toLocaleString('en-GB')}
               ─────────────`,
               true,
             );
           }
           embed.setFooter(`OSINT-Logs`);
         }
-        return embed;
       });
-    await message.channel.send(character_logs);
+    await message.channel.send(embed);
   },
 };

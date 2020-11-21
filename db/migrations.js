1d967d5027126a01f745aca3c8da337f27cafde5
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-11-21 19:12:50 +0300

    db: personalities_db to migrations

--- db/migrations.js
+++ db/migrations.js
@@ -4,6 +4,7 @@
 require('./connection')
 const items_db = require('./models/items_db');
 const characters_db = require('./models/characters_db');
+const personalities_db = require('./models/personalities_db');
 const realms_db = require('./models/realms_db');
 const guilds_db = require('./models/guilds_db');
 const auctions_db = require('./models/auctions_db');

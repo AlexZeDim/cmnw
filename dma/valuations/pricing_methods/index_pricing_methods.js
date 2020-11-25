1ad412829f6ed67c26b84db5b96849bcaa15e6b1
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-11-25 05:28:33 +0300

    dma: index_pricing_methods updated

--- dma/valuations/pricing_methods/index_pricing_methods.js
+++ dma/valuations/pricing_methods/index_pricing_methods.js
@@ -19,7 +19,7 @@ const BlizzAPI = require('blizzapi');
 
 (async () => {
   try {
-    console.time(`DMA-importMethodsBlizzardAPI`);
+    console.time(`DMA-API`);
     const professionsTicker = new Map([
       [164, 'BSMT'],
       [165, 'LTHR'],
@@ -59,7 +59,7 @@ const BlizzAPI = require('blizzapi');
       headers: { 'Battlenet-Namespace': 'static-eu' }
     });
     for (let profession of professions) {
-      let { skill_tiers } = await api.query(`/data/wow/profession/${profession.id}`, {
+      const { skill_tiers } = await api.query(`/data/wow/profession/${profession.id}`, {
         timeout: 10000,
         headers: { 'Battlenet-Namespace': 'static-eu' }
       });
@@ -75,7 +75,7 @@ const BlizzAPI = require('blizzapi');
           })
           if (categories) {
             for (let category of categories) {
-              let { recipes } = category;
+              const { recipes } = category;
               for (let recipe of recipes) {
                 const [RecipeData, RecipeMedia] = await Promise.allSettled([
                   api.query(`/data/wow/recipe/${recipe.id}`, {
@@ -89,64 +89,93 @@ const BlizzAPI = require('blizzapi');
                 ]);
                 if (RecipeData.value) {
                   const recipe_data = RecipeData.value;
-                  let pricing_method = await pricing_methods_db.findById(`P${recipe_data.id}`);
 
-                  if (!pricing_method) {
-                    pricing_method = new pricing_methods_db({
-                      _id: `P${recipe_data.id}`,
-                      recipe_id: parseInt(recipe_data.id),
-                      name: recipe_data.name,
-                      type: `primary`,
-                      createdBy: `DMA-importMethodsBlizzardAPI`,
-                      updatedBy: `DMA-importMethodsBlizzardAPI`,
-                    });
-                  }
+                  const writeConcerns = [];
 
-                  if (recipe.name) {
-                    pricing_method.name = recipe_data.name;
-                  }
-
-                  if (recipe_data.description) {
-                    pricing_method.description = recipe_data.description;
-                  }
                   if ('alliance_crafted_item' in recipe_data) {
-                    pricing_method.alliance_item_id = parseInt(
-                      recipe_data.alliance_crafted_item.id,
-                    );
+                    if ('id' in recipe_data.alliance_crafted_item) {
+                      writeConcerns.push({
+                        faction: 'Alliance',
+                        item_id: recipe_data.alliance_crafted_item.id
+                      })
+                    }
                   }
                   if ('horde_crafted_item' in recipe_data) {
-                    pricing_method.horde_item_id = parseInt(
-                      recipe_data.horde_crafted_item.id,
-                    );
+                    if ('id' in recipe_data.horde_crafted_item) {
+                      writeConcerns.push({
+                        faction: 'Horde',
+                        item_id: recipe_data.horde_crafted_item.id
+                      })
+                    }
                   }
-                  if (profession.id && professionsTicker.has(profession.id)) {
-                    pricing_method.profession = professionsTicker.get(profession.id);
+                  if ('crafted_item' in recipe_data) {
+                    if ('id' in recipe_data.crafted_item) {
+                      writeConcerns.push({
+                        item_id: recipe_data.crafted_item.id
+                      })
+                    }
                   }
-                  if (expansion_ticker) {
-                    pricing_method.expansion = expansion_ticker;
-                  }
-                  if (recipe_data.rank) {
-                    pricing_method.rank = recipe_data.rank;
-                  }
-                  if ('crafted_quantity' in recipe_data) {
-                    pricing_method.item_quantity =
-                      recipe_data.crafted_quantity.value;
-                  }
-                  if (recipe_data.reagents && recipe_data.reagents.length) {
-                    pricing_method.reagents = recipe_data.reagents.map(
-                      ({ reagent, quantity }) => {
-                        return {
+
+                  if (!writeConcerns.length) continue
+
+                  for (const concerns of writeConcerns) {
+                    const _id = `P${concerns.item_id}${(concerns.faction) ? (concerns.faction[0]) : ('')}`;
+
+                    let pricing_method = await pricing_methods_db.findById(_id);
+
+                    if (!pricing_method) {
+                      pricing_method = new pricing_methods_db({
+                        _id: _id,
+                        recipe_id: parseInt(recipe_data.id),
+                        item_id: concerns.item_id,
+                        type: `primary`,
+                        createdBy: `DMA-API`,
+                        updatedBy: `DMA-API`,
+                      });
+                    }
+
+                    if (recipe.name) pricing_method.name = recipe_data.name;
+
+                    if (recipe_data.description) pricing_method.description = recipe_data.description;
+
+                    if (profession.id && professionsTicker.has(profession.id)) pricing_method.profession = professionsTicker.get(profession.id);
+
+                    if (expansion_ticker) pricing_method.expansion = expansion_ticker;
+
+                    if (recipe_data.rank) pricing_method.rank = recipe_data.rank;
+
+                    if ('crafted_quantity' in recipe_data) {
+                      if ('value' in recipe_data.crafted_quantity) {
+                        pricing_method.item_quantity = recipe_data.crafted_quantity.value;
+                      } else if ('minimum' in recipe_data.crafted_quantity) {
+                        pricing_method.item_quantity = recipe_data.crafted_quantity.minimum;
+                      }
+                    }
+
+                    if (recipe_data.reagents && recipe_data.reagents.length) {
+                      pricing_method.reagents = recipe_data.reagents.map(({ reagent, quantity }) => ({
                           _id: parseInt(reagent.id),
                           quantity: parseInt(quantity),
-                        };
-                      },
-                    );
+                        })
+                      );
+                    }
+
+                    if (RecipeMedia.value) {
+                      pricing_method.media = RecipeMedia.value.assets[0].value;
+                    }
+
+
+                    if (recipe_data.modified_crafting_slots) {
+                      recipe_data.modified_crafting_slots.map(({slot_type, display_order}) => ({
+                        _id: slot_type.id,
+                        name: slot_type.name,
+                        display_order: display_order
+                      }))
+                    }
+
+                    await pricing_method.save();
+                    console.info(`F:U,${pricing_method.expansion}:${pricing_method.profession}:${pricing_method._id},${pricing_method.name.en_GB}`);
                   }
-                  if (RecipeMedia.value) {
-                    pricing_method.media = RecipeMedia.value.assets[0].value;
-                  }
-                  await pricing_method.save();
-                  console.info(`F:U,${pricing_method.expansion}:${pricing_method.profession}:${pricing_method._id},${pricing_method.name.en_GB}`);
                 }
               }
             }
@@ -158,7 +187,7 @@ const BlizzAPI = require('blizzapi');
   } catch (error) {
     console.error(error);
   } finally {
-    console.timeEnd(`DMA-importMethodsBlizzardAPI`);
+    console.timeEnd(`DMA-API`);
     await process.exit(0)
   }
 })();

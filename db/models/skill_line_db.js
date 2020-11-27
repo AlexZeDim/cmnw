4a9a2ee38555e4787e4ebbc3a83715d6c04ceffb
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-11-27 07:29:58 +0300

    dma: improved pricing from csv

---
+++ db/models/skill_line_db.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+/**
+ * ID - recipeID
+ * SkillLine - professionID
+ * Spell - spellID
+ * SupersedesSpell - determines RANK of currentSpell, supersedes weak rank
+ * MinSkillLineRank - require skill points
+ * Flags: 0 or 16 ??????
+ * NumSkillUps - skill points up on craft
+ * TrivialSkillLineRankHigh - greenCraftQ
+ * TrivialSkillLineRankLow - yellowCraftQ
+ * SkillUpSkillLineID represent subCategory in professions, for expansionTicker
+ *
+ */
+
+const schema = new mongoose.Schema(
+  {
+    _id: Number,
+    skill_line: Number,
+    spell_id: {
+        type: Number,
+        index: true
+    },
+    supersedes_spell: Number,
+    min_skill_rank: Number,
+    num_skill_ups: Number,
+    green_craft: Number,
+    yellow_craft: Number,
+    skill_up_skill_line_id: Number,
+  }
+);
+
+
+const skill_line = mongoose.model('skill_line', schema, 'skill_line');
+
+module.exports = skill_line;
---
+++ db/models/spell_effect_db.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+/**
+ *  Effect - effect flag
+ *  EffectItemType - item_id
+ *  EffectBasePointsF - item_quantity
+ *  spellID - spell_id
+ *
+ */
+
+const schema = new mongoose.Schema(
+  {
+    _id: Number,
+    effect: Number,
+    item_id: {
+        type: Number,
+        index: true
+    },
+    item_quantity: Number,
+    spell_id: {
+        type: Number,
+        index: true
+    },
+  }
+);
+
+
+const skill_line = mongoose.model('skill_effect', schema, 'skill_effect');
+
+module.exports = skill_line;

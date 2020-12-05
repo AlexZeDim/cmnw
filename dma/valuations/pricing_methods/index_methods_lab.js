0945b7a6ea6d80284cba37476af4d97017a3b8d1
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-12-05 15:26:58 +0300

    dma: index_methods_lab

--- dma/valuations/pricing_methods/index_methods_lab.js
+++ dma/valuations/pricing_methods/index_methods_lab.js
@@ -141,6 +141,72 @@ const disenchant = {
   ]
 }
 
+const decks = {
+  name: 'DECK',
+  methods: [
+    {
+      reagents: [
+        { _id: 173088, quantity: 1 },
+        { _id: 173089, quantity: 1 },
+        { _id: 173090, quantity: 1 },
+        { _id: 173091, quantity: 1 },
+        { _id: 173092, quantity: 1 },
+        { _id: 173093, quantity: 1 },
+        { _id: 173094, quantity: 1 },
+        { _id: 173095, quantity: 1 },
+      ],
+      derivatives: [
+        { _id: 173087, quantity: 1 }
+      ]
+    },
+    {
+      reagents: [
+        { _id: 173070, quantity: 1 },
+        { _id: 173071, quantity: 1 },
+        { _id: 173072, quantity: 1 },
+        { _id: 173073, quantity: 1 },
+        { _id: 173074, quantity: 1 },
+        { _id: 173075, quantity: 1 },
+        { _id: 173076, quantity: 1 },
+        { _id: 173077, quantity: 1 },
+      ],
+      derivatives: [
+        { _id: 173069, quantity: 1 }
+      ]
+    },
+    {
+      reagents: [
+        { _id: 173079, quantity: 1 },
+        { _id: 173080, quantity: 1 },
+        { _id: 173081, quantity: 1 },
+        { _id: 173082, quantity: 1 },
+        { _id: 173083, quantity: 1 },
+        { _id: 173084, quantity: 1 },
+        { _id: 173085, quantity: 1 },
+        { _id: 173086, quantity: 1 },
+      ],
+      derivatives: [
+        { _id: 173078, quantity: 1 }
+      ]
+    },
+    {
+      reagents: [
+        { _id: 173097, quantity: 1 },
+        { _id: 173098, quantity: 1 },
+        { _id: 173099, quantity: 1 },
+        { _id: 173100, quantity: 1 },
+        { _id: 173101, quantity: 1 },
+        { _id: 173102, quantity: 1 },
+        { _id: 173103, quantity: 1 },
+        { _id: 173104, quantity: 1 },
+      ],
+      derivatives: [
+        { _id: 173096, quantity: 1 }
+      ]
+    },
+  ]
+}
+
 async function MethodsLab ({ name, methods }) {
   try {
     if (!methods.length) return
@@ -155,34 +221,182 @@ async function MethodsLab ({ name, methods }) {
       updatedBy: 'DMA-LAB',
     };
 
-
     if (name === 'INSC') {
-      precursor.mask = `${precursor.mask}#51005`
       precursor.media = 'https://render-eu.worldofwarcraft.com/icons/56/ability_miling.jpg'
       precursor.spell_id = 51005
     }
 
-    if (name === 'ENC') {
-      precursor.mask = `${precursor.mask}#13262`
+    if (name === 'ENCH') {
       precursor.media = 'https://render-eu.worldofwarcraft.com/icons/56/inv_enchant_disenchant.jpg'
       precursor.spell_id = 13262
     }
+
+    if (name === 'DECK') precursor.media = 'https://render-eu.worldofwarcraft.com/icons/56/inv_inscription_darkmooncard_generic01.jpg'
 
     let y = 0;
 
     for (const method of methods) {
       if (!method.derivatives.length || !method.reagents.length) continue
       y++
+
       precursor.recipe_id = parseInt(`${precursor.spell_id}${method.reagents[0]._id}`)
       precursor.ticker = `${precursor.mask}:D${precursor.recipe_id}:${y}`
       precursor.reagents = method.reagents
       precursor.derivatives = method.derivatives
-      const pricing_method = await pricing_methods_db.findOneAndUpdate(
-        { item_id: precursor.item_id, recipe_id: precursor.recipe_id },
+
+      if (name === 'DECK') {
+        if (method.derivatives[0]._id === 173087) {
+          //Voracity
+          precursor.spell_id = 311628
+          method.reagents.map(async (card, j) => {
+            const blank_card = await pricing_methods_db.findOneAndUpdate({
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`
+              }, {
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`,
+                spell_id: precursor.spell_id,
+                profession: `${name}`,
+                expansion: 'SHDW',
+                type: 'derivative',
+                createdBy: 'DMA-LAB',
+                updatedBy: 'DMA-LAB',
+                reagents: [
+                  { _id: 177841, quantity: 1 },
+                  { _id: 175970, quantity: 1 },
+                ],
+                derivatives: [{ _id: card._id, quantity: card.quantity }]
+              },
+              {
+                upsert: true,
+                new: true,
+                setDefaultsOnInsert: true,
+                lean: true,
+              }
+            )
+            console.info(`U,${blank_card.expansion}:${blank_card.ticker},${blank_card._id}`);
+          })
+        }
+        if (method.derivatives[0]._id === 173069) {
+          //Putrescence
+          precursor.spell_id = 311629
+          method.reagents.map(async (card, j) => {
+            const blank_card = await pricing_methods_db.findOneAndUpdate({
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`,
+              }, {
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`,
+                spell_id: precursor.spell_id,
+                profession: `${name}`,
+                expansion: 'SHDW',
+                type: 'derivative',
+                createdBy: 'DMA-LAB',
+                updatedBy: 'DMA-LAB',
+                reagents: [
+                  { _id: 177843, quantity: 1 },
+                  { _id: 175970, quantity: 1 },
+                ],
+                derivatives: [{ _id: card._id, quantity: card.quantity }]
+              },
+              {
+                upsert: true,
+                new: true,
+                setDefaultsOnInsert: true,
+                lean: true,
+              }
+            )
+            console.info(`U,${blank_card.expansion}:${blank_card.ticker},${blank_card._id}`);
+          })
+        }
+        if (method.derivatives[0]._id === 173078) {
+          //Repose
+          precursor.spell_id = 311625
+          method.reagents.map(async (card, j) => {
+            const blank_card = await pricing_methods_db.findOneAndUpdate({
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`,
+              }, {
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`,
+                spell_id: precursor.spell_id,
+                profession: `${name}`,
+                expansion: 'SHDW',
+                type: 'derivative',
+                createdBy: 'DMA-LAB',
+                updatedBy: 'DMA-LAB',
+                reagents: [
+                  { _id: 177842, quantity: 1 },
+                  { _id: 175970, quantity: 1 },
+                ],
+                derivatives: [{ _id: card._id, quantity: card.quantity }]
+              },
+              {
+                upsert: true,
+                new: true,
+                setDefaultsOnInsert: true,
+                lean: true,
+              }
+            )
+            console.info(`U,${blank_card.expansion}:${blank_card.ticker},${blank_card._id}`);
+          })
+        }
+        if (method.derivatives[0]._id === 173096) {
+          //Indomitable
+          precursor.spell_id = 311626
+          method.reagents.map(async (card, j) => {
+            const blank_card = await pricing_methods_db.findOneAndUpdate({
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`,
+              }, {
+                ticker: `${precursor.mask}:D${precursor.recipe_id}B${card._id}:${j}:${y}`,
+                spell_id: precursor.spell_id,
+                profession: `${name}`,
+                expansion: 'SHDW',
+                type: 'derivative',
+                createdBy: 'DMA-LAB',
+                updatedBy: 'DMA-LAB',
+                reagents: [
+                  { _id: 177840, quantity: 1 },
+                  { _id: 175970, quantity: 1 },
+                ],
+                derivatives: [{ _id: card._id, quantity: card.quantity }]
+              },
+              {
+                upsert: true,
+                new: true,
+                setDefaultsOnInsert: true,
+                lean: true,
+              }
+            )
+            console.info(`U,${blank_card.expansion}:${blank_card.ticker},${blank_card._id}`);
+          })
+        }
+
+        method.reagents.map(async (card, i) => {
+          const card_of_death = await pricing_methods_db.findOneAndUpdate({
+              ticker: `${precursor.mask}:D${precursor.recipe_id}T${card._id}:${y}:${i}`
+            }, {
+              ticker: `${precursor.mask}:D${precursor.recipe_id}T${card._id}:${y}:${i}`,
+              spell_id: precursor.spell_id,
+              profession: `${name}`,
+              expansion: 'SHDW',
+              type: 'derivative',
+              createdBy: 'DMA-LAB',
+              updatedBy: 'DMA-LAB',
+              reagents: [{ _id: 173066, quantity: 1 }],
+              derivatives: [{ _id: card._id, quantity: card.quantity }]
+            },
+            {
+              upsert: true,
+              new: true,
+              setDefaultsOnInsert: true,
+              lean: true,
+            }
+          )
+          console.info(`U,${card_of_death.expansion}:${card_of_death.ticker},${card_of_death._id}`);
+        })
+      }
+
+      const pricing_method = await pricing_methods_db.findOneAndUpdate({
+          ticker: precursor.ticker
+        },
         precursor,
         {
           upsert: true,
-          strict: false,
           new: true,
           setDefaultsOnInsert: true,
           lean: true,
@@ -200,3 +414,4 @@ MethodsLab(disenchant)
 
 MethodsLab(milling)
 
+MethodsLab(decks)

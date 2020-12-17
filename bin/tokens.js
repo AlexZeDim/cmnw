6ee743c7dd70e8c9444583938742920a45f633db
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-12-17 15:03:15 +0300

    updated tokens.js

--- bin/tokens.js
+++ bin/tokens.js
@@ -19,7 +19,7 @@ const schedule = require('node-schedule');
 schedule.scheduleJob('0-59 * * * *', async () => {
   try {
     console.time('CORE-TOKEN')
-    await keys_db.find().cursor().eachAsync(async ({ _id, secret })=> {
+    await keys_db.find({ tags: 'BlizzardAPI' }).cursor().eachAsync(async key => {
       const { access_token, expires_in } = await axios({
         url: `https://eu.battle.net/oauth/token`,
         method: 'post',
@@ -30,15 +30,17 @@ schedule.scheduleJob('0-59 * * * *', async () => {
           grant_type: 'client_credentials'
         },
         auth: {
-          username: _id,
-          password: secret
+          username: key._id,
+          password: key.secret
         }
       }).then(res => { return res.data } );
-      let token = await keys_db.updateOne(
-        { _id: _id },
-        { token: access_token, expired_in: expires_in },
-      );
-      if (token) console.info(`U,${_id},${expires_in}`);
+
+      if (access_token && expires_in) {
+        key.token = access_token
+        key.expired_in = expires_in
+        await key.save()
+        console.info(`U,${_id},${expires_in}`)
+      }
     });
   } catch (e) {
     console.error(e);

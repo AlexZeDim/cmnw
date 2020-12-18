c77dcee1444de26aa3fac654a79602a563a6e750
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-12-18 13:46:57 +0300

    osint: index_characters.js

--- osint/characters/index_characters.js
+++ osint/characters/index_characters.js
@@ -29,27 +29,45 @@ const getCharacter = require('./get_character');
     await characters_db.syncIndexes()
     await characters_db.collection.createIndex({ 'hash.a': 1, 'updatedAt': 1, 'statusCode': 1 }, { name: `OSINT-${indexCharacters.name}` })
     await characters_db
-      .find()
-      .sort({ 'hash.a': 1, 'updatedAt': 1, 'statusCode': 1 })
-      .maxTimeMS(0)
-      .batchSize(bulkSize)
-      .lean()
-      .cursor()
+      .aggregate([
+        {
+          $sort: { updatedAt: 1 }
+        },
+        {
+          $group: {
+            _id: { hash_a: '$hash.a' },
+            characters: {
+              $addToSet: {
+                name: '$name',
+                realm: '$realm',
+                lastModified: '$lastModified'
+              }
+            },
+          }
+        },
+      ])
+      .allowDiskUse(true)
+      .cursor({ batchSize: bulkSize })
+      .exec()
       .addCursorFlag('noCursorTimeout',true)
-      .eachAsync(async ({ _id, realm }, iterations) => {
-          const [name] = _id.split('@')
-          await getCharacter({
-            name: name,
-            realm: realm,
-            updatedBy: `OSINT-indexCharacters`,
-            token: token,
-            guildRank: false,
-            createOnlyUnique: false,
-            iterations: iterations,
-            forceUpdate: true
-          });
+      .eachAsync(async (block, iterations) => {
+          if (block.characters.length) {
+            let i = 0;
+            for (const character of block.characters) {
+              await getCharacter({
+                name: character.name,
+                realm: character.realm,
+                updatedBy: `OSINT-indexCharacters`,
+                token: token,
+                guildRank: false,
+                createOnlyUnique: false,
+                iterations: iterations + (i++),
+                forceUpdate: true
+              });
+            }
+          }
         }, { parallel: bulkSize }
-      );
+      )
   } catch (error) {
     console.error(error);
   } finally {

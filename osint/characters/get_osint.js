b6d9ae8978b22ac37e6c8b3f1fe2cc592d353a0d
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-12-19 13:44:37 +0300

    osint: index_lua

--- osint/characters/get_osint.js
+++ osint/characters/get_osint.js
@@ -12,37 +12,47 @@ const keys_db = require('../../db/models/keys_db');
 const fs = require('fs');
 const getCharacter = require('./get_character');
 
-(async (queryKeys = { tags: `conglomerat` }) => {
+(async function indexLua (key = `conglomerat`, path = 'C:\\Games\\World of Warcraft\\_retail_\\WTF\\Account\\ALEXZEDIM\\SavedVariables\\OSINT.lua') {
   try {
     console.time(`OSINT-fromLua`);
-    let { token } = await keys_db.findOne(queryKeys);
-    let path = 'C:\\Games\\World of Warcraft\\_retail_\\WTF\\Account\\ALEXZEDIM\\SavedVariables\\OSINT.lua';
-    let array = [];
-    let osint = fs
-      .readFileSync(
-        path,
-        'utf8',
-      )
-      .split('["csv"] = ')[1];
-    let arrayOfLines = osint.match(/[^\r\n]+/g);
-    for (let csv of arrayOfLines) {
-      let csv_line = csv.split(/(,\s--\s\[\d)/)[0];
+    const { token } = await keys_db.findOne({ keys: key });
+    const array = [];
+    const osint = fs.readFileSync(path, 'utf8').split('["csv"] = ')[1];
+    const arrayOfLines = osint.match(/[^\r\n]+/g);
+    for (const csv of arrayOfLines) {
+      const csv_line = csv.split(/(,\s--\s\[\d)/)[0];
       if (csv_line.startsWith('\t\t"') && csv_line.endsWith('"')) {
         const [name, slug_locale] = csv_line
           .replace(/"/g, '')
           .replace('\t\t', '')
           .split(',');
         if (array.length <= 10) {
-          array.push(getCharacter({ name: name, realm: { slug: slug_locale}, updatedBy: `OSINT-fromLua`, token: token, guildRank: true, createOnlyUnique: true }))
+          array.push(
+            getCharacter({
+              name: name,
+              realm: { slug: slug_locale},
+              updatedBy: `OSINT-fromLua`,
+              token: token,
+              guildRank: true,
+              createOnlyUnique: true
+            })
+          )
         } else {
           await Promise.allSettled(array)
           array.length = 0;
-          array.push(getCharacter({ name: name, realm: { slug: slug_locale}, updatedBy: `OSINT-fromLua`, token: token, createOnlyUnique: true, guildRank: true }))
+          array.push(
+            getCharacter({
+              name: name,
+              realm: { slug: slug_locale},
+              updatedBy: `OSINT-fromLua`,
+              token: token,
+              createOnlyUnique: true,
+              guildRank: true })
+          )
         }
       }
     }
     await fs.unlinkSync(path)
-
   } catch (error) {
     console.error(error);
   } finally {

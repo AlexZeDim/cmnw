5a37d5d368922fd83061a827070c4a577dcfae67
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-12-30 16:44:45 +0300

    dma: timestamp

--- dma/auctions/get_auctions.js
+++ dma/auctions/get_auctions.js
@@ -53,7 +53,7 @@ schedule.scheduleJob('30,59 * * * *', async (
       .eachAsync(
         async ({ _id, name }) => {
           try {
-            const time = new Date();
+            const time = new Date().getTime();
             console.time(`DMA-getAuctionData-${time}:${_id.connected_realm_id}`);
             console.info(`R,${_id.connected_realm_id},${name},`);
             if (_id.timestamp) _id.timestamp = `${moment.unix(_id.timestamp).utc().format('ddd, DD MMM YYYY HH:mm:ss')} GMT`;

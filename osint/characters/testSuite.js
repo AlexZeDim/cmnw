968dba3914ed11070e5e02a941249ff9dc415f34
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-01-04 16:08:07 +0300

    osint: index_lfg to-do

--- osint/characters/testSuite.js
+++ osint/characters/testSuite.js
@@ -1,14 +1,18 @@
 require('./../../db/connection');
 const characters_db = require('../../db/models/characters_db');
 const { getLookingForGuild } = require('./get_lfg');
+const { updateWProgress, updateRaiderIO, updateWarcraftLogs } = require('./updaters');
 const { differenceBy } = require('lodash');
 
 
 (async function T () {
   try {
-    //const t = await getLookingForGuild();
-    const a = differenceBy([{ 'x': 3 }, { 'x': 1 }], [{ 'x': 1 }, {'x': 2 }], 'x')
-    console.log(a)
+    const x = await Promise.allSettled([
+      await updateWarcraftLogs('Лумиваара', 'howling-fjord'),
+      await updateWProgress('Лумиваара', 'howling-fjord'),
+      await updateRaiderIO('Лумиваара', 'howling-fjord'),
+    ])
+    console.log(x)
   } catch (e) {
     console.error(e)
   }

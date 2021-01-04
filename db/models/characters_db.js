039fc9f3dc0e2786924e472ee159e022d36412ab
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-01-04 07:32:57 +0300

    db: character_db for index_lfg

--- db/models/characters_db.js
+++ db/models/characters_db.js
@@ -135,7 +135,10 @@ const schema = new mongoose.Schema(
       progress: Object,
       role: String,
       transfer: Boolean,
-      languages: [String]
+      languages: {
+        type: [String],
+        lowercase: true
+      }
     }
   },
   {

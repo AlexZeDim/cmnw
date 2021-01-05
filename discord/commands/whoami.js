91004168b1086585550db86aeda87ada240c6077
Sasha <907696+AlexZeDim@users.noreply.github.com>
2021-01-05 11:12:46 +0300

    Revert "discord: questions and whoami"
    This reverts commit cc923108a621150996425b230cd34b3c1a9d3b16.

--- discord/commands/whoami.js
+++ discord/commands/whoami.js
@@ -3,7 +3,7 @@ module.exports = {
   description:
     'Prints the effective username and ID of the current user. Check this [article](https://en.wikipedia.org/wiki/Whoami) for more info.',
   aliases: ['WHOAMI'],
-  async execute(message) {
-    await message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`,);
+  execute(message) {
+    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`,);
   },
 };

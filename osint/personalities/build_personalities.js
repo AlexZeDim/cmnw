e9b66684c911c579a07e537fd7ba416c6f927f1e
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-01-07 15:59:26 +0300

    osint: personalities

--- osint/personalities/build_personalities.js
+++ osint/personalities/build_personalities.js
@@ -12,9 +12,6 @@ const personalities_db = require('../../db/models/personalities_db');
     await characters_db
       .aggregate([
         {
-          $limit: 3
-        },
-        {
           $match: {
             statusCode: 200,
             hash_a: { $ne: null },
@@ -50,6 +47,7 @@ const personalities_db = require('../../db/models/personalities_db');
                 value: alias._id
               }))
             })
+            console.info(file)
             await Promise.all(block.map(async character => await characters_db.findByIdAndUpdate(character._id, { hash_f: file._id.toString().substr(-6), personality: file._id } )));
           } else if (personalities.length === 1 && block.length) {
             const persona = await personalities_db.findById(personalities[0])
@@ -60,7 +58,7 @@ const personalities_db = require('../../db/models/personalities_db');
             persona.markModified('aliases')
             await persona.save()
             await Promise.all(block.map(async character => await characters_db.findByIdAndUpdate(character._id, { hash_f: persona._id.toString().substr(-6), personality: persona._id } )));
-            console.log(persona)
+            console.info(persona)
           } else if (personalities.length > 1 && block.length) {
             console.warn(`E:${group._id}:${personalities.length}#${block.length}`);
           }

faeb1fcd4acd29a31215f124e657634c5565c94a
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-01-09 16:45:07 +0300

    osint: index_lfg

--- osint/characters/index_lfg.js
+++ osint/characters/index_lfg.js
@@ -44,7 +44,7 @@ const index_LFG = async () => {
     console.info(`${charactersDiff.length} characters have been added to queue`)
     for (const { _id } of charactersDiff) {
       const character = characters_db.findById(_id);
-      console.log(character)
+
       if (!character) continue
       /** Request wow_progress and RIO for m+, pve progress and contacts */
       const [wcl, wp, rio] = await Promise.allSettled([

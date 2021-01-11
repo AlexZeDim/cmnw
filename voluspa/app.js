ea5c047d580e39c6539d0d90e75e597c7094f5d7
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-01-11 16:49:13 +0300

    voluspa: init

---
+++ voluspa/app.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+
+//TESSIE Nzk3OTIzNTIzOTEzMTg3MzM4.X_tj2w.JoqPua1xYeDKPE3SSanwLazp6tk
+//GOSSIP NzI3NjIyMDg2MzYwNjk0ODc1.XzCMuw.DllkWDlms4u0g7iLcyoRyXqjgwQ
+
+bot.login("Nzk3OTIzNTIzOTEzMTg3MzM4.X_tj2w.JoqPua1xYeDKPE3SSanwLazp6tk").then(r => r);
+
+bot.on('ready', () => console.log(`Logged in as ${bot.user.tag}!`));
+
+bot.on('message', async message => {
+  console.log(message.content)
+})

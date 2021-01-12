76edab866edc3ff129505032e6047bbe275fb43e
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-01-12 12:42:10 +0300

    discord: subscriptions

--- discord/subscriptions/services.js
+++ discord/subscriptions/services.js
@@ -201,7 +201,7 @@ const worker = async ({ _id, type, filters }, channel) => {
 
         const query = { 'lfg.status': true, 'lfg.new': true };
         if (filters.faction) Object.assign(query, { 'lfg.faction': filters.faction });
-        if (filters.item_level) Object.assign(query, { average_item_level: { '$gte': filters.item_level } });
+        if (filters.item_level) Object.assign(query, { 'average_item_level': { '$gte': filters.item_level } });
         if (filters.rio) Object.assign(query, { 'lfg.rio': { '$gte': filters.rio } });
         if (filters.days_from) Object.assign(query, { 'lfg.days_from': { '$gte': filters.days_from } });
         if (filters.days_to) Object.assign(query, { 'lfg.days_to': { '$lte': filters.days_to } });
@@ -516,54 +516,21 @@ const worker = async ({ _id, type, filters }, channel) => {
   }
 }
 
-
-const subscriptions = async (bot) => {
+/**
+ * @param _id {string}
+ * @param fault_tolerance {number}
+ * @param discord_name {string=}
+ * @param channel_name {string=}
+ * @returns {Promise<void>}
+ */
+const errorFaultTolerance = async ({ _id, fault_tolerance, discord_name, channel_name }) => {
   try {
-    /** Every discord subscriber */
-    await discord_db
-      .aggregate([
-        {
-          $sort: { message_sent: 1 }
-        },
-        {
-          $lookup: {
-            from: 'realms',
-            localField: 'filters.realms',
-            foreignField: '_id',
-            as: 'filters.realms',
-          },
-        },
-        {
-          $lookup: {
-            from: 'items',
-            localField: 'filters.items',
-            foreignField: '_id',
-            as: 'filters.items',
-          },
-        },
-      ])
-      .cursor({ batchSize: 10 })
-      .exec()
-      .eachAsync(async subscriber => {
-        const guild = await bot.guilds.cache.get(subscriber.discord_id);
-        if (!guild) {
-          await discord_db.findByIdAndUpdate(subscriber._id, { fault_tolerance: (subscriber.fault_tolerance || 0) + 1})
-          if (subscriber.fault_tolerance && subscriber.fault_tolerance > 9) await discord_db.deleteOne({ _id: subscriber._id })
-          console.warn(`DISCORD: ${subscriber.discord_name}@${subscriber.channel_name} unable to fetch`)
-          return
-        }
-        const guild_channel = await guild.channels.cache.get(subscriber.channel_id)
-        if (!guild_channel) {
-          await discord_db.findByIdAndUpdate(subscriber._id, { fault_tolerance: (subscriber.fault_tolerance || 0) + 1})
-          if (subscriber.fault_tolerance && subscriber.fault_tolerance > 9) await discord_db.deleteOne({ _id: subscriber._id })
-          console.warn(`DISCORD: ${subscriber.discord_name}@${subscriber.channel_name} unable to fetch`)
-          return
-        }
-        await worker(subscriber, guild_channel)
-      })
+    await discord_db.findByIdAndUpdate(_id, { fault_tolerance: (fault_tolerance || 0) + 1})
+    if (fault_tolerance && fault_tolerance > 9) await discord_db.deleteOne({ _id: _id })
+    console.warn(`DISCORD: ${discord_name}@${channel_name} unable to fetch`)
   } catch (error) {
-    console.error(`E,${subscriptions.name}:${error}`)
+    console.error(`E,${errorFaultTolerance.name}:${error}`)
   }
 }
 
-module.exports = { subscriptions, worker, installer };
+module.exports = { errorFaultTolerance, worker, installer };

c1f3042157062f6bd4ab4c5cc72219f312cc508d
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-01-24 23:24:40 +0300

    graphql: revert reject 1 quantity and fix floor with index

--- dma/valuations/cluster/price_range.js
+++ dma/valuations/cluster/price_range.js
@@ -8,11 +8,15 @@ const priceRange = (array, blocks = 40) => {
   try {
     if (!array.length) return []
     let L = array.length
+    let I = 1
+    if (L === 1) {
+      I = 0;
+    }
     if (L > 3) {
       L = L - 3
     }
     const ninety_percent = Math.floor(L * 0.9);
-    const floor = Math.floor(array[0]);
+    const floor = Math.floor(array[I]);
     const cap = Math.round(array[ninety_percent]);
     const price_range = cap - floor;
     /** Step represent 2.5% for each cluster */

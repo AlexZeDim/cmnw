68f47942db0377bc7609db8e43717eaefe043435
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-02-17 00:30:09 +0300

    osint: wip realms and queues

---
+++ src/osint/osint.queues.ts
@@ -0,0 +1,19 @@
+import {JobsOptions, Queue} from "bullmq";
+import {connectionRedis} from "../db/redis/connectionRedis";
+
+const osintOptions: JobsOptions = {
+  removeOnComplete: true,
+  removeOnFail: false,
+};
+
+const queueCharacters = new Queue('OSINT:Characters', {connection: connectionRedis, defaultJobOptions: osintOptions});
+const queueGuilds = new Queue('OSINT:Guilds', {connection: connectionRedis, defaultJobOptions: osintOptions});
+const queueRealms = new Queue('OSINT:Realms', {connection: connectionRedis, defaultJobOptions: osintOptions});
+const queueFiles = new Queue('OSINT:Files', {connection: connectionRedis, defaultJobOptions: osintOptions});
+
+export {
+  queueCharacters,
+  queueGuilds,
+  queueRealms,
+  queueFiles
+}

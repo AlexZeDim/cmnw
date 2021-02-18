c218bba9ce4fa137bbecd3092693571b04896f4c
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-02-18 04:10:06 +0300

    db: account model added

---
+++ src/db/mongo/models/accounts.model.ts
@@ -0,0 +1,32 @@
+import {prop, getModelForClass, modelOptions, index} from '@typegoose/typegoose';
+
+enum AliasKey {
+  Discord = "discord",
+  Bnet = "battle.tag",
+  Twitter = "twitter",
+  Name = "name",
+  Character = "character",
+  Nickname = "nickname",
+  Codename = "codename",
+}
+
+@modelOptions({ schemaOptions: { timestamps: true, collection: 'accounts' }, options: { customName: 'accounts' } })
+@index({ 'aliases.key': 1, 'aliases.value': 1 }, {  name: 'Aliases' })
+
+class Alias {
+  @prop({ enum: AliasKey })
+  public key!: AliasKey
+  @prop()
+  public value!: string
+}
+
+class Account {
+  @prop({ required: true, default: 'Anonymous' })
+  public cryptonym!: string;
+  @prop()
+  public tags?: string[];
+  @prop({ _id: false })
+  public Alias?: Alias[]
+}
+
+export const AccountModel = getModelForClass(Account);

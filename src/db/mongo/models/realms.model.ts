86ab2e9189d58c99f31b170f740731ecad032634
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-08 07:10:22 +0300

    refactor(db): realms model
    work in progress yet

--- src/db/mongo/models/realms.model.ts
+++ src/db/mongo/models/realms.model.ts
@@ -9,6 +9,12 @@ class Realm {
   public slug!: string;
   @prop()
   public name!: string;
+  @prop()
+  public name_locale?: string;
+  @prop()
+  public ticker?: string;
+  @prop()
+  public wcl_id?: number;
 }
 
 export const RealmModel = getModelForClass(Realm);

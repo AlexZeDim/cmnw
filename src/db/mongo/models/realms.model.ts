67d7e7eea3c18fe82738295d4c63bf26a76c448f
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-08 07:49:27 +0300

    refactor(db): updated realms model

--- src/db/mongo/models/realms.model.ts
+++ src/db/mongo/models/realms.model.ts
@@ -1,6 +1,49 @@
 import {prop, getModelForClass, modelOptions} from '@typegoose/typegoose';
+import {toSlug} from "../refs";
 
 @modelOptions({ schemaOptions: { timestamps: true, collection: 'realms' }, options: { customName: 'realms' } })
+
+class CharactersPopulation {
+  @prop()
+  public _id!: string;
+  @prop({ type: [Number] })
+  public value?: [number];
+}
+
+class RealmPopulation {
+  @prop({ type: [Number] })
+  public characters_total?: [number];
+  @prop({ type: [Number] })
+  public characters_active?: [number];
+  @prop({ type: [Number] })
+  public characters_active_alliance?: [number];
+  @prop({ type: [Number] })
+  public characters_active_horde?: [number];
+  @prop({ type: [Number] })
+  public characters_active_max_level?: [number];
+  @prop({ type: [Number] })
+  public characters_guild_members?: [number];
+  @prop({ type: [Number] })
+  public characters_guildless?: [number];
+  @prop({ type: [Number] })
+  public players_unique?: [number];
+  @prop({ type: [Number] })
+  public players_active_unique?: [number];
+  @prop({ type: [Number] })
+  public guilds_total?: [number];
+  @prop({ type: [Number] })
+  public guilds_alliance?: [number];
+  @prop({ type: [Number] })
+  public guilds_horde?: [number];
+  @prop({ type: CharactersPopulation })
+  public characters_classes?: CharactersPopulation;
+  @prop({ type: CharactersPopulation })
+  public characters_professions?: [CharactersPopulation];
+  @prop({ type: CharactersPopulation })
+  public characters_covenants?: [CharactersPopulation];
+  @prop({ type: [Number] })
+  public timestamps?: [number];
+}
 
 class Realm {
   @prop()
@@ -10,11 +53,47 @@ class Realm {
   @prop()
   public name!: string;
   @prop()
+  public region?: string;
+  @prop()
   public name_locale?: string;
+  @prop({ set: (val: string) => toSlug(val), get: (val: string) => toSlug(val) })
+  public slug_locale?: string;
   @prop()
   public ticker?: string;
   @prop()
+  public category?: string;
+  @prop()
+  public locale?: string;
+  @prop()
+  public timezone?: string;
+  @prop()
+  public status?: string;
+  @prop()
+  public type?: string;
+  @prop()
+  public population_status?: string;
+  @prop()
+  public connected_realm_id?: number;
+  @prop({ type:  [String] })
+  public connected_realm?: [string];
+  /**
+   * Kihra's WarcraftLogs realm ids
+   * for parsing logs via fromLogs
+   */
+  @prop()
   public wcl_id?: number;
+  /**
+   * String lastModified timestamp for auctions, gold and valuations
+   * Required for valuations, getAuctionData, getGold
+   */
+  @prop()
+  public auctions?: number;
+  @prop()
+  public valuations?: number;
+  @prop()
+  public golds?: number;
+  @prop({ type: RealmPopulation, _id: false })
+  public population?: RealmPopulation;
 }
 
 export const RealmModel = getModelForClass(Realm);

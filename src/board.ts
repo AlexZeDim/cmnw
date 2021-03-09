9884695961d7e26d1f0a997af60df17dd7201552
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-09 13:07:14 +0300

    refactor(core): import log queue for bull-board

--- src/board.ts
+++ src/board.ts
@@ -1,11 +1,12 @@
 import { setQueues, BullMQAdapter, router } from "bull-board/dist";
-import { queueRealms } from "./osint/osint.queue";
+import { queueRealms, queueLogs } from "./osint/osint.queue";
 import express from "express";
 
 const app = express();
 
 setQueues([
   new BullMQAdapter(queueRealms),
+  new BullMQAdapter(queueLogs),
 ]);
 
 app.use('/admin/queues', router)

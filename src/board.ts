fa39a2bca6aa85a95d4cfb117ce53daa973790b9
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-11 12:38:18 +0300

    feat(core): added item & gold queue

--- src/board.ts
+++ src/board.ts
@@ -1,6 +1,6 @@
 import { setQueues, BullMQAdapter, router } from "bull-board/dist";
 import { queueRealms, queueLogs } from "./osint/osint.queue";
-import { queueAuctions } from "./dma/dma.queue";
+import { queueAuctions, queueItems, queueGolds } from "./dma/dma.queue";
 import express from "express";
 
 const app = express();
@@ -9,6 +9,8 @@ setQueues([
   new BullMQAdapter(queueRealms),
   new BullMQAdapter(queueLogs),
   new BullMQAdapter(queueAuctions),
+  new BullMQAdapter(queueItems),
+  new BullMQAdapter(queueGolds),
 ]);
 
 app.use('/admin/queues', router)

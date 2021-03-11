46d8b0caad2bac0b98936ea6a6f65d7e734bbbbd
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-11 12:39:16 +0300

    feat(dma): added getter for items

--- src/dma/dma.getter.ts
+++ src/dma/dma.getter.ts
@@ -1,7 +1,7 @@
-import {RealmModel, AuctionsModel, GoldModel} from "../db/mongo/mongo.model";
+import {RealmModel, AuctionsModel, GoldModel, ItemModel} from "../db/mongo/mongo.model";
 import BlizzAPI, {BattleNetOptions} from 'blizzapi';
 import moment from "moment";
-import {ObjectProps} from "../interface/constant";
+import {ItemProps, ObjectProps} from "../interface/constant";
 import {round2} from "../db/mongo/refs";
 import Xray from "x-ray";
 
@@ -110,7 +110,88 @@ const getGold = async () => {
   }
 }
 
+const getItem = async <T extends { _id: number } & BattleNetOptions> (args: T) => {
+
+  const api = new BlizzAPI({
+    region: args.region,
+    clientId: args.clientId,
+    clientSecret: args.clientSecret,
+    accessToken: args.accessToken
+  });
+
+  /** Check is exits */
+  let item = await ItemModel.findById(args._id);
+  /** If not, create */
+  if (!item) {
+    item = new ItemModel({
+      _id: args._id,
+    });
+  }
+
+  /** Request item data */
+  const [getItemSummary, getItemMedia] = await (Promise as any).allSettled([
+    api.query(`/data/wow/item/${args._id}`, {
+      timeout: 10000,
+      headers: { 'Battlenet-Namespace': 'static-eu' }
+    }),
+    api.query(`/data/wow/media/item/${args._id} `, {
+      timeout: 10000,
+      headers: { 'Battlenet-Namespace': 'static-eu' }
+    })
+  ]);
+
+  if (getItemSummary.value) {
+    /** Schema fields */
+    const
+      requested_item: Partial<ItemProps> = {},
+      fields: string[] = [
+        'quality',
+        'item_class',
+        'item_subclass',
+        'inventory_type',
+      ],
+      gold: string[] = ['purchase_price', 'sell_price'];
+
+    /** key value TODO refactor merge */
+    for (const [key] of Object.entries(getItemSummary.value)) {
+      /** Loot type */
+      if (key === 'preview_item') {
+        if ('binding' in getItemSummary.value[key]) {
+          requested_item.loot_type = getItemSummary.value[key]['binding'].type;
+        }
+      }
+
+      if (fields.some(f => f === key)) {
+        requested_item[key] = getItemSummary.value[key].name['en_GB'];
+      } else {
+        requested_item[key] = getItemSummary.value[key];
+      }
+
+      if (gold.some(f => f === key)) {
+        if (key === 'sell_price') {
+          item.asset_class.addToSet('VSP');
+        }
+        requested_item[key] = round2(getItemSummary.value[key] / 10000);
+      }
+    }
+
+    Object.assign(item, requested_item)
+
+    if (
+      getItemMedia.value &&
+      getItemMedia.value.assets &&
+      getItemMedia.value.assets.length
+    ) {
+      item.icon = getItemMedia.value.assets[0].value;
+    }
+
+    await item.save();
+    return item
+  }
+}
+
 export {
   getAuctions,
-  getGold
+  getGold,
+  getItem
 }

2767591955977d0a30a576df7eebca62382fc34b
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-11 05:39:07 +0300

    refactor(core): added auctions queue to board

--- src/board.ts
+++ src/board.ts
@@ -1,5 +1,6 @@
 import { setQueues, BullMQAdapter, router } from "bull-board/dist";
 import { queueRealms, queueLogs } from "./osint/osint.queue";
+import { queueAuctions } from "./dma/dma.queue";
 import express from "express";
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 setQueues([
   new BullMQAdapter(queueRealms),
   new BullMQAdapter(queueLogs),
+  new BullMQAdapter(queueAuctions),
 ]);
 
 app.use('/admin/queues', router)

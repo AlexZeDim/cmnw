a8e1a61526ad88a1de06c4358e280e3ba6c12365
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-16 12:11:03 +0300

    feat(db): guild schema updated

--- src/db/mongo/schemas/guilds.schema.ts
+++ src/db/mongo/schemas/guilds.schema.ts
@@ -1,8 +1,46 @@
-import {prop, modelOptions} from '@typegoose/typegoose';
+import {prop, modelOptions, mongoose} from '@typegoose/typegoose';
+import {toSlug} from "../refs";
 
 @modelOptions({ schemaOptions: { timestamps: true, collection: 'guilds' }, options: { customName: 'guilds' } })
 
-export class Guild {
-  @prop({ lowercase: true })
+class GuildMember {
+  @prop({ required: true, lowercase: true, set: (val: string) => toSlug(val), get: (val: string) => toSlug(val) })
   public _id!: string;
+  @prop({ required: true })
+  public id!: number;
+  @prop({ required: true })
+  public rank!: number;
+}
+
+export class Guild {
+  @prop({ required: true, lowercase: true })
+  public _id!: string;
+  @prop()
+  public id?: number;
+  @prop({ required: true })
+  public name!: string;
+  @prop({ required: true, set: (val: string) => toSlug(val), get: (val: string) => toSlug(val) })
+  public realm!: string;
+  @prop({ required: true })
+  public realm_id!: number;
+  @prop({ required: true })
+  public realm_name!: string;
+  @prop({ required: true })
+  public faction!: string;
+  @prop({ required: true, default: [] })
+  public members!: mongoose.Types.Array<GuildMember>
+  @prop()
+  public achievement_points?: number;
+  @prop()
+  public member_count?: number;
+  @prop({ default: Date.now() })
+  public last_modified?: Date;
+  @prop()
+  public created_timestamp?: Date;
+  @prop()
+  public status_code?: number;
+  @prop()
+  public created_by?: string;
+  @prop()
+  public updatedBy?: string;
 }

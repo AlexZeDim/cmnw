48f36dff22af1e0060e59cd9daa92a1d94944259
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-19 14:31:07 +0300

    test(osint): cover update getters

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,11 +1,15 @@
-
+import {
+  updateCharacterMedia,
+  updateCharacterMounts,
+  updateCharacterPets,
+  updateCharacterProfessions
+} from "../src/osint/osint.getter";
 import { connect, connection } from 'mongoose';
-//import { getCharacter } from "../src/osint/osint.getter";
+import {KeysModel} from "../src/db/mongo/mongo.model";
+import BlizzAPI from 'blizzapi';
 import dotenv from 'dotenv'
 import path from 'path'
-import {KeysModel} from "../src/db/mongo/mongo.model";
 dotenv.config({ path: path.join(__dirname, '..', '.env') })
-
 
 beforeAll(async () => {
   await connect(
@@ -28,13 +32,66 @@ afterAll(async () => {
 });
 
 
-it('will check the values and pass', async () => {
-  const key = await KeysModel.findOne({ tags: 'BlizzardAPI' }).lean();
-  expect(key).toMatchSnapshot({
-    _id: expect.any(String),
-    secret: expect.any(String),
-    token: expect.any(String),
-    expired_in: expect.any(Number),
+describe('core', () => {
+  test('key', async () => {
+    const key = await KeysModel.findOne({ tags: 'BlizzardAPI' }).lean();
+    expect(key).toMatchSnapshot({
+      _id: expect.any(String),
+      secret: expect.any(String),
+      token: expect.any(String),
+      expired_in: expect.any(Number),
+    });
   });
-  //const character = await getCharacter({ _id: 'инициатива@gordunni', name: 'инициатива', realm: 'gordunni' })
 })
+
+describe('updaters', () => {
+  let key, api: BlizzAPI;
+  beforeAll(async () => {
+    key = await KeysModel.findOne({ tags: 'BlizzardAPI' }).lean();
+    if (!key) return
+    api = new BlizzAPI({
+      region: 'eu',
+      clientId: key._id,
+      clientSecret: key.secret,
+      accessToken: key.token
+    });
+  });
+
+  test('updateCharacterMedia', async () => {
+    const character_media = await updateCharacterMedia('инициатива', 'gordunni', api);
+    expect(character_media).toMatchSnapshot({
+      id: expect.any(Number),
+      avatar: expect.any(String),
+      inset: expect.any(String),
+      main: expect.any(String),
+    });
+  });
+
+  test('updateCharacterMounts', async () => {
+    const character_mounts = await updateCharacterMounts('инициатива', 'gordunni', api);
+    expect(character_mounts.mounts[0]).toMatchSnapshot({ _id: expect.any(Number), name: expect.any(String) });
+  });
+
+  test('updateCharacterPets', async () => {
+    const character_pets = await updateCharacterPets('инициатива', 'gordunni', api);
+    expect(character_pets).toMatchSnapshot({
+      hash_a: expect.any(String),
+      hash_b: expect.any(String),
+      pets: expect.any(Array)
+    });
+  });
+
+  test('updateCharacterProfessions', async () => {
+    const character_profession = await updateCharacterProfessions('инициатива', 'gordunni', api);
+    expect(character_profession.professions).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: expect.any(String),
+          id: expect.any(Number),
+          tier: expect.any(String)
+        })
+      ])
+    );
+  });
+})
+

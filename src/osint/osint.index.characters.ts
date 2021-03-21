2557ccec8bc5cd1fd81d1c4e32b003e4ccd55e7f
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-21 21:50:54 +0300

    feat(osint): indexCharacter

--- src/osint/osint.index.characters.ts
+++ src/osint/osint.index.characters.ts
@@ -1,9 +1,12 @@
 import '../db/mongo/mongo.connection';
 import {CharacterModel, KeysModel} from "../db/mongo/mongo.model";
+import {queueCharacters} from './osint.queue';
 
 const indexCharacter = async() => {
   try {
-    const key = await KeysModel.find({ tags: 'BlizzardAPI' });
+    const key = await KeysModel.findOne({ tags: 'BlizzardAPI' }).lean();
+    if (!key) return;
+
     await CharacterModel
       .find()
       .sort({ 'hash_a': 1 })
@@ -11,11 +14,30 @@ const indexCharacter = async() => {
       .cursor()
       .eachAsync(async character => {
         //TODO add limiter or something
-        const [name, realm] = character._id.split('@');
-
+        await queueCharacters.add(
+          character._id,
+          {
+            _id: character._id,
+            name: character.name,
+            realm: character.realm,
+            created_by: `OSINT-index`,
+            updated_by: `OSINT-index`,
+            guildRank: false,
+            createOnlyUnique: true,
+            region: 'eu',
+            clientId: key._id,
+            clientSecret: key.secret,
+            accessToken: key.token,
+          }, {
+            jobId: character._id,
+            priority: 5
+          }
+        );
       })
   } catch (e) {
     console.error(e)
+  } finally {
+    process.exit(0);
   }
 };
 

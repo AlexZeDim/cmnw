92801853864a2e6e740b697858a46a695919b76b
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-21 21:42:26 +0300

    test(osint): cover getCharacter

--- test/getters.test.ts
+++ test/getters.test.ts
@@ -1,5 +1,6 @@
 import {
-  getGuild
+  getGuild,
+  getCharacter,
 } from "../src/osint/osint.getter";
 import {connect, connection} from 'mongoose';
 import {KeysModel} from "../src/db/mongo/mongo.model";
@@ -54,13 +55,13 @@ describe('getters', () => {
     }
 
     const guild = await getGuild(guild_params);
-    expect(guild).toHaveProperty('_id', guild_params._id);
-    expect(guild).toHaveProperty('name', guild_params.name);
-    expect(guild).toHaveProperty('status_code', 200);
-    expect(guild).toHaveProperty('realm', guild_params.realm);
-    expect(guild).toHaveProperty('created_by');
-    expect(guild).toHaveProperty('updated_by');
     expect(guild).toMatchObject({
+      _id: guild_params._id,
+      name: guild_params.name,
+      status_code: 200,
+      realm: guild_params.realm,
+      created_by: expect.any(String),
+      updated_by: expect.any(String),
       members: expect.arrayContaining([
         expect.objectContaining({
           _id: expect.any(String),
@@ -70,4 +71,153 @@ describe('getters', () => {
       ])
     });
   })
+
+  test('getCharacter: инициатива@gordunni', async () => {
+    if (!key) return
+
+    const character_params = {
+      _id: 'инициатива@gordunni',
+      name: 'Инициатива',
+      realm: 'gordunni',
+      realm_id: 1602,
+      realm_name: 'Gordunni',
+      character_class: 'Rogue',
+      updated_by: 'Jest',
+      status_code: 200,
+      forceUpdate: true,
+      createOnlyUnique: false,
+      region: 'eu',
+      clientId: key._id,
+      clientSecret: key.secret,
+      accessToken: key.token
+    }
+
+    const character = await getCharacter(character_params);
+    expect(character).toMatchObject({
+      _id: character_params._id,
+      name: character_params.name,
+      realm: character_params.realm,
+      realm_id: character_params.realm_id,
+      realm_name: character_params.realm_name,
+      status_code: character_params.status_code,
+      updated_by: character_params.updated_by,
+      mounts: expect.arrayContaining([
+        expect.objectContaining({
+          _id: expect.any(Number),
+          name: expect.any(String)
+        })
+      ]),
+      pets: expect.arrayContaining([
+        expect.objectContaining({
+          _id: expect.any(Number),
+          name: expect.any(String)
+        })
+      ]),
+      professions: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          tier: expect.any(String)
+        })
+      ]),
+      id: expect.any(Number),
+      gender: expect.any(String),
+      faction: expect.any(String),
+      race: expect.any(String),
+      character_class: character_params.character_class,
+      active_spec:  expect.any(String),
+      level: expect.any(Number),
+      achievement_points: expect.any(Number),
+      average_item_level: expect.any(Number),
+      equipped_item_level: expect.any(Number),
+      hash_a: expect.any(String),
+      hash_b: expect.any(String),
+      avatar: expect.any(String),
+      inset: expect.any(String),
+      main: expect.any(String),
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    })
+  })
+
+  test('getCharacter: саске@gordunni', async () => {
+    if (!key) return
+
+    const character_params = {
+      _id: 'саске@gordunni',
+      name: 'Саске',
+      realm: 'gordunni',
+      realm_id: 1602,
+      realm_name: 'Gordunni',
+      character_class: 'Mage',
+      updated_by: 'Jest',
+      status_code: 200,
+      forceUpdate: true,
+      createOnlyUnique: false,
+      updateRIO: true,
+      updateWCL: true,
+      updateWP: true,
+      region: 'eu',
+      clientId: key._id,
+      clientSecret: key.secret,
+      accessToken: key.token
+    }
+
+    const character = await getCharacter(character_params);
+    expect(character).toMatchObject({
+      _id: character_params._id,
+      name: character_params.name,
+      realm: character_params.realm,
+      realm_id: character_params.realm_id,
+      realm_name: character_params.realm_name,
+      status_code: character_params.status_code,
+      updated_by: character_params.updated_by,
+      mounts: expect.arrayContaining([
+        expect.objectContaining({
+          _id: expect.any(Number),
+          name: expect.any(String)
+        })
+      ]),
+      pets: expect.arrayContaining([
+        expect.objectContaining({
+          _id: expect.any(Number),
+          name: expect.any(String)
+        })
+      ]),
+      professions: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          tier: expect.any(String)
+        })
+      ]),
+      id: expect.any(Number),
+      gender: expect.any(String),
+      faction: expect.any(String),
+      race: expect.any(String),
+      character_class: character_params.character_class,
+      active_spec:  expect.any(String),
+      level: expect.any(Number),
+      achievement_points: expect.any(Number),
+      average_item_level: expect.any(Number),
+      equipped_item_level: expect.any(Number),
+      hash_a: expect.any(String),
+      avatar: expect.any(String),
+      inset: expect.any(String),
+      main: expect.any(String),
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+      battle_tag: expect.any(String),
+      rio_score: expect.any(Number),
+      wcl_percentile: expect.any(Number),
+      role: expect.any(String),
+      transfer: expect.any(Boolean),
+      raid_progress: expect.arrayContaining([
+        expect.objectContaining({
+          _id: expect.any(String),
+          progress: expect.any(String),
+        })
+      ]),
+    })
+  }, 30000)
 })

b215274f19f5c5a3326efe7c571d5856c17f04d1
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-03-21 16:54:50 +0300

    fix(osint): require: true for logs

--- src/db/mongo/schemas/logs.schema.ts
+++ src/db/mongo/schemas/logs.schema.ts
@@ -1,23 +1,22 @@
 import {prop, modelOptions} from '@typegoose/typegoose';
-import {toSlug} from '../refs';
 
 @modelOptions({ schemaOptions: { timestamps: true, collection: 'logs' }, options: { customName: 'logs' } })
 
 export class Log {
-  @prop({ lowercase: true, index: true, set: (val: string) => toSlug(val), get: (val: string) => toSlug(val) })
+  @prop({ required: true, lowercase: true, index: true })
   public root_id!: string;
-  @prop({ type: () => [String] })
+  @prop({ required: true, type: String, default: [] })
   public root_history!: string[];
-  @prop()
+  @prop({ required: true })
   public original!: string;
-  @prop()
+  @prop({ required: true })
   public updated!: string;
-  @prop()
+  @prop({ required: true })
   public event!: string;
-  @prop()
+  @prop({ required: true })
   public action!: string;
-  @prop()
+  @prop({ required: true })
   public t0!: Date;
-  @prop()
+  @prop({ required: true })
   public t1!: Date;
 }

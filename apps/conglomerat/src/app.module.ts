28176648e500b59e040629cce04f63f7ead4d19f
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-05 22:50:45 +0300

    build(queue): add bull-mq

--- apps/conglomerat/src/app.module.ts
+++ apps/conglomerat/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mongoConfig } from '@app/configuration';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [MongooseModule.forRoot(mongoConfig.connection_string)],

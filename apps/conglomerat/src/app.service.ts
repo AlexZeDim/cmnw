60da1665548dd95ad43a8e068c54edadfd698161
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-08 20:16:25 +0300

    feat(bullmq): test passed successfully!

--- apps/conglomerat/src/app.service.ts
+++ apps/conglomerat/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CharacterQueueOsintService } from '@app/character-queue-osint';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly queueCharacters: CharacterQueueOsintService,
+  ) {
+    this.test('test');
+  }
+
+  async test (input: string): Promise<void> {
+    await this.queueCharacters.addJob(input)
   }
 }
--- libs/character-queue-osint/src/character-queue-osint.service.ts
+++ libs/character-queue-osint/src/character-queue-osint.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Job, Queue } from 'bullmq';
 import { BullQueueInject } from '@anchan828/nest-bullmq';
 import { queueCharacters } from '@app/core';
 
 @Injectable()
 export class CharacterQueueOsintService {
+
+  private readonly logger = new Logger(
+    CharacterQueueOsintService.name, true,
+  );
+
   constructor(@BullQueueInject(queueCharacters.name) public readonly queue: Queue) {}
 
-  public async addJob(): Promise<Job> {
-    return this.queue.add("job", { test: "test" });
+  public async addJob(input: string): Promise<Job> {
+    this.logger.debug('job added!')
+    return this.queue.add("job", { test: input });
   }
 }

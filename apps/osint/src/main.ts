18d03f6fb784db6acff1bcbe87948cfc1ba5bfa2
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-09 20:44:08 +0300

    feat(core): add queueRealms

--- apps/osint/src/main.ts
+++ apps/osint/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { OsintModule } from './osint.module';
 
+/**
+ * Main Worker for OSINT
+ */
 async function bootstrap() {
   const app = await NestFactory.create(OsintModule);
   await app.listen(3000);

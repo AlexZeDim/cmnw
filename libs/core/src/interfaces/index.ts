ad2691f733a51fc0c6cd5a10b801b08b5575c214
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-09 20:39:20 +0300

    feat(core): add more interfaces

--- libs/core/src/interfaces/index.ts
+++ libs/core/src/interfaces/index.ts
@@ -1,2 +1,2 @@
 export { QueueInterface } from './queue.interface';
-export { CharacterProps, CharactersPopulationRealm, RealmProps, GuildProps, LogProps, GuildMemberProps, Locales, GuildRosterProps } from './osint.interface';
+export { CharacterProps, CharactersPopulationRealm, RealmProps, GuildProps, LogProps, GuildMemberProps, Locales, GuildRosterProps, PopulationRealmProps } from './osint.interface';

c45d24a8a02c922262391e56af83b2c87f6637e7
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-10 20:47:07 +0300

    fix(conglomerat): remove unused service

--- apps/conglomerat/src/app.service.ts
+++ apps/conglomerat/src/app.service.ts
@@ -1,17 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CharacterQueueOsintService } from '@app/character-queue-osint';
-import { RealmQueueOsintService } from '@app/realm-queue-osint';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly queueCharacters: CharacterQueueOsintService,
-    private readonly queueRealms: RealmQueueOsintService,
-  ) {
-    this.test('test');
-  }
 
-  async test(input: string): Promise<void> {
-    await this.queueRealms.addJob(input);
-  }
 }

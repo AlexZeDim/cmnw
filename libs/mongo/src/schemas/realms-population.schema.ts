f699c31feb8df5daddf54320acf25942f8f0d788
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-11 16:58:39 +0300

    feat(mongo): add realm-population

---
+++ libs/mongo/src/schemas/realms-population.schema.ts
@@ -0,0 +1,115 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+class CharacterCovenants {
+  @Prop()
+  kyrian: number;
+
+  @Prop()
+  necrolord: number;
+
+  @Prop()
+  venthyr: number;
+
+  @Prop()
+  night_fae: number;
+}
+
+class CharacterClasses {
+  @Prop()
+  death_knight: number;
+
+  @Prop()
+  demon_hunter: number;
+
+  @Prop()
+  druid: number;
+
+  @Prop()
+  hunter: number;
+
+  @Prop()
+  mage: number;
+
+  @Prop()
+  monk: number;
+
+  @Prop()
+  paladin: number;
+
+  @Prop()
+  priest: number;
+
+  @Prop()
+  rogue: number;
+
+  @Prop()
+  shaman: number;
+
+  @Prop()
+  warlock: number;
+
+  @Prop()
+  warrior: number;
+}
+
+@Schema({ timestamps: true })
+export class RealmPopulation extends Document {
+  @Prop({ type: Number })
+  realm_id: number;
+
+  @Prop()
+  connected_realm_id: number;
+
+  /**
+   * TODO name probably? day?
+   */
+  @Prop()
+  characters_total: number;
+
+  @Prop()
+  characters_active: number;
+
+  @Prop()
+  characters_active_alliance: number;
+
+  @Prop()
+  characters_active_horde: number;
+
+  @Prop()
+  characters_active_max_level: number;
+
+  @Prop()
+  characters_guild_members: number
+
+  @Prop()
+  characters_guildless: number
+
+  @Prop()
+  players_unique: number
+
+  @Prop()
+  players_active_unique: number
+
+  @Prop()
+  guilds_total: number
+
+  @Prop()
+  guilds_alliance: number
+
+  @Prop()
+  guilds_horde: number
+
+  @Prop({ type: CharacterClasses })
+  characters_classes: CharacterClasses
+
+  /** TODO
+  @Prop({ type: CharacterProfessions })
+  characters_professions: CharacterProfessions
+  */
+
+  @Prop({ type: CharacterCovenants })
+  characters_covenants: CharacterCovenants
+}
+
+export const RealmsPopulationSchema = SchemaFactory.createForClass(RealmPopulation);

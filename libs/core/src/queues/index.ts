31cdd3301acb0e398d0ecee11e91b4eef7a12287
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-13 19:44:21 +0300

    feat(apps): add item queue

--- libs/core/src/queues/index.ts
+++ libs/core/src/queues/index.ts
@@ -1,3 +1,4 @@
 export { queueCharacters } from './queue-characters.config';
 export { queueRealms } from './queue-realms.config';
 export { queueAuctions } from './queue-auctions.config';
+export { queueItems } from './queue-items.config';

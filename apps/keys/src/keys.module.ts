ab39586501d65db0985144caaf91815e49508772
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-13 19:26:24 +0300

    feat(keys): add ScheduleModule

--- apps/keys/src/keys.module.ts
+++ apps/keys/src/keys.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { mongoConfig } from '@app/configuration';
 import { Key, KeysSchema } from '@app/mongo';
 import { KeysService } from './keys.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     MongooseModule.forRoot(mongoConfig.connection_string),
     MongooseModule.forFeature([{ name: Key.name, schema: KeysSchema }]),
   ],

b77b6fbeb36ffcbafe7c5a7842c7ca7191129a3a
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-17 12:14:36 +0300

    feat(guilds): init guilds

---
+++ apps/guilds/src/guilds.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { GuildsService } from './guilds.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [GuildsService],
+})
+export class GuildsModule {}

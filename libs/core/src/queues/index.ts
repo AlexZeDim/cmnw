d1250a1389672a2cd598f6f909a40c75032efa76
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-18 19:04:48 +0300

    fix(core): export guildsQueue

--- libs/core/src/queues/index.ts
+++ libs/core/src/queues/index.ts
@@ -2,3 +2,4 @@ export { charactersQueue } from './characters.queue';
 export { realmsQueue } from './realms.queue';
 export { auctionsQueue } from './auctions.queue';
 export { itemsQueue } from './items.queue';
+export { guildsQueue } from './guilds.queue';

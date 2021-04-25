4aaaf070ebb3bd2991f091b7703513d3197321ac
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-25 19:19:06 +0300

    fix(mongo): mongo-options

--- libs/configuration/src/mongo-options.config.ts
+++ libs/configuration/src/mongo-options.config.ts
@@ -4,5 +4,8 @@ export const mongoOptionsConfig: MongooseModuleOptions = {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
-  socketTimeoutMS: (1000 * 60 * 60 * 24)
+  useUnifiedTopology: true,
+  poolSize: 10,
+  serverSelectionTimeoutMS: 10,
+  socketTimeoutMS: 0
 }

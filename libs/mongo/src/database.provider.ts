ae3b92140bfbe0495951f254c939f7a22de1403c
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-25 23:02:46 +0300

    feat(mongo): database connection module

---
+++ libs/mongo/src/database.provider.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+import { mongoConfig, mongoOptionsConfig } from '@app/configuration';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect(mongoConfig.connection_string, mongoOptionsConfig),
+  },
+];

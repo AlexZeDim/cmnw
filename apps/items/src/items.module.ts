7c91731cc77c5403ab4215149fdf2a0bdb2b90af
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-04-30 19:07:36 +0300

    fix(items): mongo config

--- apps/items/src/items.module.ts
+++ apps/items/src/items.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { mongoConfig, redisConfig } from '@app/configuration';
+import { mongoConfig, mongoOptionsConfig, redisConfig } from '@app/configuration';
 import { BullModule } from '@anchan828/nest-bullmq';
 import { itemsQueue } from '@app/core';
 import { Item, ItemsSchema, Key, KeysSchema } from '@app/mongo';
@@ -9,7 +9,7 @@ import { ItemsWorker } from './items.worker';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(mongoConfig.connection_string),
+    MongooseModule.forRoot(mongoConfig.connection_string, mongoOptionsConfig),
     MongooseModule.forFeature([
       { name: Key.name, schema: KeysSchema },
       { name: Item.name, schema: ItemsSchema }

0951f3a0d3140cc1a5adf5e346676b61d65e3304
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-01 11:14:11 +0300

    feat(valuations): init buildAssetClass

--- apps/valuations/src/valuations.service.ts
+++ apps/valuations/src/valuations.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Key } from '@app/mongo';
+import { Model } from "mongoose";
 
 @Injectable()
 export class ValuationsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly logger = new Logger(
+    ValuationsService.name, true,
+  );
+
+  constructor(
+    @InjectModel(Key.name)
+    private readonly KeyModel: Model<Key>,
+  ) {
+    this.buildAssetClasses()
+  }
+
+  async buildAssetClasses(): Promise<void> {
+    // TODO init?
+    return;
   }
 }

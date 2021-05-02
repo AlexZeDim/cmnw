3f1391faf1040c9382fa63b99b3585a5cd095225
Gonikon <kstg12@mail.ru>
2021-05-02 23:08:19 +0300

    feat(conglomerat): add item valuations  to DMA

--- apps/conglomerat/src/dma/dma.controller.ts
+++ apps/conglomerat/src/dma/dma.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, HttpCode, HttpStatus, Logger, Param, Req } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiForbiddenResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiServiceUnavailableResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { DmaService } from './dma.service';
 
 @Controller('dma')
@@ -10,7 +11,14 @@ export class DmaController {
   constructor(
     private readonly dmaService: DmaService
   ) {}
-
+  
+  @ApiOperation({ description: 'Returns requested item' })
+  @ApiOkResponse({ description: 'Request item with selected _id' })
+  @ApiUnauthorizedResponse({ description: 'You need authenticate yourself before request' })
+  @ApiForbiddenResponse({ description: 'You don`t have clearance for that' })
+  @ApiBadRequestResponse({ description: 'The server could not understand the request due to invalid syntax' })
+  @ApiServiceUnavailableResponse({ description: 'Server is under maintenance or overloaded' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @HttpCode(HttpStatus.OK)
   @Get('/item/:id')
   getItem(@Param('id') id: string): string {
@@ -18,10 +26,33 @@ export class DmaController {
     const [item, realm] = id.split('@');
     return this.dmaService.getItem(item, realm);
   }
-
+  
+  @ApiOperation({ description: 'Returns requested WoWToken' })
+  @ApiOkResponse({ description: 'Request item with selected timestamp' })
+  @ApiUnauthorizedResponse({ description: 'You need authenticate yourself before request' })
+  @ApiForbiddenResponse({ description: 'You don`t have clearance for that' })
+  @ApiBadRequestResponse({ description: 'The server could not understand the request due to invalid syntax' })
+  @ApiServiceUnavailableResponse({ description: 'Server is under maintenance or overloaded' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @HttpCode(HttpStatus.OK)
   @Get('/token/:region/:limit')
   getWowToken(@Param('region') region: string, @Param('limit') limit: number): string {
     return this.dmaService.getWowToken(region, limit);
   }
+
+  @ApiOperation({ description: 'Returns requested item' })
+  @ApiOkResponse({ description: 'Request item with selected _id' })
+  @ApiUnauthorizedResponse({ description: 'You need authenticate yourself before request' })
+  @ApiForbiddenResponse({ description: 'You don`t have clearance for that' })
+  @ApiBadRequestResponse({ description: 'The server could not understand the request due to invalid syntax' })
+  @ApiServiceUnavailableResponse({ description: 'Server is under maintenance or overloaded' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @HttpCode(HttpStatus.OK)
+  @Get('/item/:id')
+  getItemValuations(@Param('id') id: string): string {
+    // TODO validate
+    const [item, realm] = id.split('@');
+    return this.dmaService.getItemValuations(item, realm);
+  }
+
 }

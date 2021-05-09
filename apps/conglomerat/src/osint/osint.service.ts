35173d73327c0469a8447d04823f51912ab04a20
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-09 13:42:42 +0300

    Revert "fix(conglomerat): findOneAndReplace"
    This reverts commit 1752c2de2d2b8cb0f24a595a6495073f4ec2a1d4.

--- apps/conglomerat/src/osint/osint.service.ts
+++ apps/conglomerat/src/osint/osint.service.ts
@@ -207,10 +207,10 @@ export class OsintService {
   }
 
   async subscribeDiscord(input: DiscordSubscriptionDto): Promise<LeanDocument<Subscription>> {
-    return this.SubscriptionModel.findOneAndReplace({
+    return this.SubscriptionModel.findOneAndUpdate({
       discord_id: input.discord_id,
       channel_id: input.channel_id
-    }, input as any, { new: true }).lean();
+    }, input as unknown, { upsert: true, new: true, setDefaultsOnInsert: true }).lean();
   }
 
   async unsubscribeDiscord(input: DiscordUidSubscriptionDto): Promise<void> {

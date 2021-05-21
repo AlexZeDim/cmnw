d7736b8a31e27cbfa4b628e21c71ea75f6bba875
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-21 21:49:59 +0300

    refactor(mongo): accounts schema

--- libs/mongo/src/schemas/accounts.schema.ts
+++ libs/mongo/src/schemas/accounts.schema.ts
@@ -1,17 +1,5 @@
-import { Document, Types } from 'mongoose';
+import { Document } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { ALIAS_KEY } from '@app/core';
-
-@Schema()
-class Alias extends Document {
-  @Prop()
-  _id: string;
-
-  @Prop({ type: String, enum: ALIAS_KEY })
-  type: string;
-}
-
-export const AliasSchema = SchemaFactory.createForClass(Alias);
 
 @Schema({ timestamps: true })
 export class Account extends Document {
@@ -21,10 +9,16 @@ export class Account extends Document {
   @Prop({ type: String })
   tags: string[];
 
-  @Prop({ type: [AliasSchema] })
-  alias: Types.Array<Alias>
+  @Prop({ type: String })
+  discord_id: string[];
+
+  @Prop({ type: String })
+  battle_tag: string[];
+
+  @Prop({ type: String })
+  characters_id: string[];
 }
 
 export const AccountsSchema = SchemaFactory.createForClass(Account);
-AccountsSchema.index({ 'aliases.key': 1, 'aliases.value': 1 }, {  name: 'Aliases' })
+AccountsSchema.index({ discord: 1, battlenet: 1 }, {  name: 'Aliases' })
 

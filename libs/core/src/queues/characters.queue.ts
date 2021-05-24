f5ae5b0bbf88c1049cbf9f4e32e280d7063234d0
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-24 20:26:11 +0300

    refactor(queues): fix concurrency

--- libs/core/src/queues/characters.queue.ts
+++ libs/core/src/queues/characters.queue.ts
@@ -8,7 +8,7 @@ const queueOptions: JobsOptions = {
 
 export const charactersQueue: QueueInterface = {
   name: 'OSINT:Characters',
-  workerOptions: { concurrency: 15 },
+  workerOptions: { concurrency: 5 },
   options: {
     defaultJobOptions: queueOptions,
   },

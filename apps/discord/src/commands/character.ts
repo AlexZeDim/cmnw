989c0cc49fdd7545bca4d8ac9fffb85784e6da05
Gonikon <kstg12@mail.ru>
2021-06-04 23:10:49 +0300

    feat(discord): add basename coverage

--- apps/discord/src/commands/character.ts
+++ apps/discord/src/commands/character.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { CharacterEmbedMessage } from '../embeds';
 import { discordConfig } from '@app/configuration';
+import { LeanDocument } from 'mongoose';
+import { Character } from '@app/mongo';
 
 module.exports = {
   name: 'character',
@@ -9,7 +11,7 @@ module.exports = {
   aliases: ['char', 'CHAR', 'CHARACTER', 'Char', 'Character'],
   args: true,
   async execute(message, args) {
-    const { data: character } = await axios.get(encodeURI(`${discordConfig.basename}/api/osint/character?_id=${args}`));
+    const { data: character } = await axios.get<LeanDocument<Character>>(encodeURI(`${discordConfig.basename}/api/osint/character?_id=${args}`));
     const embed = CharacterEmbedMessage(character);
     await message.channel.send(embed);
   }

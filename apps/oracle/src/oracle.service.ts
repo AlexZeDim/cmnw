dde3d07d9e1f31d8442293ed253ab314c8b510b8
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-07-18 19:12:22 +0300

    feat(oracle): key system

--- apps/oracle/src/oracle.service.ts
+++ apps/oracle/src/oracle.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { NlpManager } from 'node-nlp';
-// @ts-ignore Nzk3OTIzNTIzOTEzMTg3MzM4.YMyy7w.V_r9TAUGQdW7iCGz5wtJBQv6FW4
-import Discord from 'discord-agent';
 import path from "path";
 import fs from 'fs-extra';
+import { InjectModel } from '@nestjs/mongoose';
+import { Key } from '@app/mongo';
+import { Model } from 'mongoose';
+// @ts-ignore
+import Discord from 'discord-agent';
 
 @Injectable()
 export class OracleService implements OnApplicationBootstrap {
@@ -16,6 +19,11 @@ export class OracleService implements OnApplicationBootstrap {
     threshold: 0.8,
     builtinWhitelist: []
   });
+
+  constructor(
+    @InjectModel(Key.name)
+    private readonly KeysModel: Model<Key>
+  ) { }
 
   private readonly logger = new Logger(
     OracleService.name, true,
@@ -39,27 +47,65 @@ export class OracleService implements OnApplicationBootstrap {
 
     this.client = new Discord.Client({
       fetchAllMembers: true,
-      disabledEvents: ['READY'],
+      // disabledEvents: ['READY'],
       ws: {
         large_threshold: 1000000,
         compress: true,
       }
     });
+
+    const key = await this.KeysModel.findOne({ tags: { $all: [ 'discord', 'free' ] } });
+    if (!key) {
+      this.logger.warn('Available keys not found!');
+      return;
+    }
+
+    key.tags.pull('free');
+    key.tags.addToSet('taken');
+    await key.save();
+    await this.client.login(key.token);
+
+    this.logger.warn(`Key ${key.token} has been taken!`);
+
     this.commands = new Discord.Collection();
 
-    await this.client.login('Nzk3OTIzNTIzOTEzMTg3MzM4.YMyy7w.V_r9TAUGQdW7iCGz5wtJBQv6FW4');
-    this.bot()
+    await this.bot();
+
+    [`close`, `exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `SIGTERM`]
+      .forEach((eventType) => process.on(eventType,  async () => {
+        key.tags.pull('taken');
+        key.tags.addToSet('free');
+        key.save();
+        this.logger.warn(`Key ${key.token} has been released!`);
+      }));
   }
 
   private async bot(): Promise<void> {
     try {
       this.client.on('ready', () => this.logger.log(`Logged in as ${this.client.user.tag}!`));
 
+      this.client.on('message', async message => {
+
+        if (message.author.bot) return;
+
+        try {
+          console.log(message);
+          console.log(message.author);
+          // TODO execute command only for clearance personal
+          // if (message.author.id === '240464611562881024') await message.send('My watch is eternal');
+          // const match = await this.manager.extractEntities('ru', message.content);
+          // console.log(match)
+        } catch (e) {
+          this.logger.error(`Error: ${e}`);
+        }
+      })
+
+    /*
       for (const [guild_id] of this.client.guilds) {
         const guild = this.client.guilds.get(guild_id);
         const members = await guild.fetchMembers();
         console.log(members);
-      }
+      }*/
     } catch (e) {
       this.logger.error(e);
     }
@@ -69,18 +115,5 @@ export class OracleService implements OnApplicationBootstrap {
       console.log(user)
     }*/
 
-/*    this.client.on('message', async message => {
-
-      if (message.author.bot) return;
-
-      try {
-        // TODO execute command only for clearance personal
-        // if (message.author.id === '240464611562881024') await message.send('My watch is eternal');
-        // const match = await this.manager.extractEntities('ru', message.content);
-        // console.log(match)
-      } catch (e) {
-        this.logger.error(`Error: ${e}`);
-      }
-    })*/
   }
 }

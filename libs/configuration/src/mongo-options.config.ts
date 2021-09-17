3c2dc04529be4137813e842ce2024b33249129d5
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-09-18 02:55:38 +0300

    build(package): downgrade TS 4.3.5 & mongoOptions new params

--- libs/configuration/src/mongo-options.config.ts
+++ libs/configuration/src/mongo-options.config.ts
@@ -2,10 +2,7 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 
 export const mongoOptionsConfig: MongooseModuleOptions = {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
   useUnifiedTopology: true,
-  poolSize: 10,
   socketTimeoutMS: 1000 * 60 * 60,
   connectTimeoutMS: 1000 * 60 * 60
 }

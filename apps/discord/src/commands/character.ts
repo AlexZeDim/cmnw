488ad8b30d4d5d341c95e6e0aa1a536fa90d38bf
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-09-19 14:14:21 +0300

    refactor(discord): character command v13

--- apps/discord/src/commands/character.ts
+++ apps/discord/src/commands/character.ts
@@ -3,6 +3,7 @@ import { CharacterEmbedMessage } from '../embeds';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { discordConfig } from '@app/configuration';
 import { Message } from 'discord.js';
+import { CharacterDto } from '@app/core';
 
 module.exports = {
   name: 'character',
@@ -19,12 +20,15 @@ module.exports = {
         .setRequired(true)),
 
   async executeMessage(message: Message, args: string) {
-    const { data: character } = await axios.get(encodeURI(`${discordConfig.basename}/api/osint/character?_id=${args}`));
+    const { data: character } = await axios.get<Partial<CharacterDto>>(encodeURI(`${discordConfig.basename}/api/osint/character?_id=${args}`));
     const embed = CharacterEmbedMessage(character);
     await message.channel.send({ embeds: [ embed ] });
   },
 
   async executeInteraction(interaction) {
-    console.log(interaction);
+    const id = interaction.options.getString('query');
+    const { data: character } = await axios.get<Partial<CharacterDto>>(encodeURI(`${discordConfig.basename}/api/osint/character?_id=${id}`));
+    const embed = CharacterEmbedMessage(character);
+    interaction.reply({ embeds: [ embed ] })
   }
 }

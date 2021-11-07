95d043534f1477fdba6767a0b8b0ad1d0c76a14b
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-11-07 18:49:06 +0300

    feat(core): interface types extended

--- libs/core/src/interfaces/oraculum.interface.ts
+++ libs/core/src/interfaces/oraculum.interface.ts
@@ -29,6 +29,13 @@ export interface IEntities {
   readonly entities: IEntity[]
 }
 
+export interface IGuessLanguage {
+  readonly alpha3: string;
+  readonly alpha2: string;
+  readonly language: string;
+  readonly score: number;
+}
+
 export interface ICastingContext {
   readonly column: number | string;
   readonly empty_lines: number;
@@ -39,4 +46,58 @@ export interface ICastingContext {
   readonly lines: number;
   readonly records: number;
   readonly invalid_field_length: number;
+}
+
+export interface INerProcess {
+  readonly locale: string;
+  readonly utterance: string;
+  readonly settings: any;
+  readonly languageGuessed: boolean;
+  readonly localeIso2: string;
+  readonly language: string;
+  readonly nluAnswer: INluAnswer;
+  readonly classification: Array<IClassification>;
+  readonly intent: string;
+  readonly score: number;
+  readonly optionalUtterance: string;
+  readonly sourceEntities: INerEntity[];
+  readonly entities: INerEntity[];
+  readonly answers: string[];
+  readonly answer: string;
+  readonly actions: any[];
+  readonly sentiment: ISentiment;
+}
+
+interface INerEntity {
+  readonly start: number;
+  readonly end: number;
+  readonly len: number;
+  readonly levenshtein: number;
+  readonly accuracy: number;
+  readonly entity: string;
+  readonly type: string;
+  readonly option: string;
+  readonly sourceText: string;
+  readonly utteranceText: string;
+}
+
+interface ISentiment {
+  readonly score: number;
+  readonly numWords: number;
+  readonly numHits: number;
+  readonly average: number;
+  readonly type: string;
+  readonly locale: string;
+  readonly vote: string;
+}
+
+interface INluAnswer {
+  readonly classification: IClassification;
+  readonly entities: Array<INerEntity> | undefined;
+  readonly explanation: any;
+}
+
+interface IClassification {
+  readonly intent: string;
+  readonly score: number;
 }

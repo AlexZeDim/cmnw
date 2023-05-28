1eadbd8d04a5aee65bbe9af49e43be993781b1ef
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-05-28 09:15:15 +0300

    chore(mongo): eslint

--- libs/mongo/src/schemas/auctions.schema.ts
+++ libs/mongo/src/schemas/auctions.schema.ts
@@ -4,44 +4,44 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 @Schema({ timestamps: true })
 export class Auction extends Document {
   @Prop({ type: Number, required: true })
-  id: number;
+    id: number;
 
   @Prop({ type: Number, required: true, ref: 'Item' })
-  item_id: number;
+    item_id: number;
 
   @Prop({ type: SchemaTypes.Mixed })
-  item: Mixed;
+    item: Mixed;
 
   @Prop({ type: Number, ref: 'Realm' })
-  connected_realm_id: number;
+    connected_realm_id: number;
 
   @Prop({ type: Number, required: true })
-  last_modified: number;
+    last_modified: number;
 
   @Prop({ type: Number })
-  quantity: number;
+    quantity: number;
 
   @Prop({ type: Number })
-  bid: number;
+    bid: number;
 
   @Prop({ type: Number })
-  buyout: number;
+    buyout: number;
 
   @Prop({ type: Number })
-  price: number;
+    price: number;
 
   // TODO enum?
   @Prop({ type: String })
-  time_left: string;
+    time_left: string;
 
   @Prop({ type: Date })
-  updatedAt: Date;
+    updatedAt: Date;
 
   @Prop({ type: Date })
-  createdAt: Date;
+    createdAt: Date;
 }
 
 export const AuctionsSchema = SchemaFactory.createForClass(Auction);
 AuctionsSchema.index({ 'createdAt': -1 }, { name: 'TTL', expireAfterSeconds: 86400 });
-AuctionsSchema.index({ 'connected_realm_id': -1, 'item_id': -1, 'last_modified': -1,  }, { name: 'TSqPL' });
+AuctionsSchema.index({ 'connected_realm_id': -1, 'item_id': -1, 'last_modified': -1  }, { name: 'TSqPL' });
 
--- libs/mongo/src/schemas/token.schema.ts
+++ libs/mongo/src/schemas/token.schema.ts
@@ -1,19 +1,19 @@
-import { Document } from "mongoose";
+import { Document } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
 export class Token extends Document {
   @Prop({ required: true })
-  _id: number;
+    _id: number;
 
   @Prop({ required: true })
-  region: string;
+    region: string;
 
   @Prop({ required: true })
-  price: number;
+    price: number;
 
   @Prop({ required: true })
-  last_modified: Date;
+    last_modified: Date;
 }
 
 export const TokenSchema = SchemaFactory.createForClass(Token);

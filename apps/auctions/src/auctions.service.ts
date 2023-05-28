b1d0d6d86508779a1c2431a1b228eb7cfbb2c446
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-05-28 08:58:15 +0300

    chore(auctions): eslint

--- apps/auctions/src/auctions.service.ts
+++ apps/auctions/src/auctions.service.ts
@@ -5,15 +5,15 @@ import { Model } from 'mongoose';
 import { BullQueueInject } from '@anchan828/nest-bullmq';
 import { delay } from '@app/core/utils/converters';
 import { Queue } from 'bullmq';
-import { GLOBAL_DMA_KEY, auctionsQueue, IQAuction, IAARealm } from '@app/core';
+import { auctionsQueue, GLOBAL_DMA_KEY, IAARealm, IQAuction } from '@app/core';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import moment from 'moment';
 
 @Injectable()
 export class AuctionsService implements OnApplicationBootstrap {
-  private readonly logger = new Logger(
-    AuctionsService.name, { timestamp: true },
-  );
+  private readonly logger = new Logger(AuctionsService.name, {
+    timestamp: true,
+  });
 
   constructor(
     @InjectModel(Realm.name)
@@ -22,58 +22,60 @@ export class AuctionsService implements OnApplicationBootstrap {
     private readonly KeyModel: Model<Key>,
     @BullQueueInject(auctionsQueue.name)
     private readonly queue: Queue<IQAuction, number>,
-  ) { }
+  ) {}
 
   async onApplicationBootstrap(): Promise<void> {
     await this.indexCommodity(GLOBAL_DMA_KEY);
   }
 
-  private async indexAuctions(clearance: string = GLOBAL_DMA_KEY): Promise<void> {
+  private async indexAuctions(
+    clearance: string = GLOBAL_DMA_KEY,
+  ): Promise<void> {
     try {
       await delay(30);
 
       const key = await this.KeyModel.findOne({ tags: clearance });
       if (!key || !key.token) {
-        this.logger.error(`indexAuctions: clearance: ${clearance} key not found`);
-        return
+        this.logger.error(
+          `indexAuctions: clearance: ${clearance} key not found`,
+        );
+        return;
       }
 
       await this.queue.drain(true);
-      const offsetTime: number = parseInt(moment().subtract(30, 'minutes').format('x'));
+      const offsetTime: number = parseInt(
+        moment().subtract(30, 'minutes').format('x'),
+      );
 
-      await this.RealmModel
-        .aggregate<IAARealm>([
-          {
-            $match: {
-              auctions: { $lt: offsetTime }
-            }
+      await this.RealmModel.aggregate<IAARealm>([
+        {
+          $match: {
+            auctions: { $lt: offsetTime },
           },
-          {
-            $group: {
-              _id: {
-                connected_realm_id: '$connected_realm_id',
-                auctions: '$auctions',
-              },
-              name: { $first: "$name" }
+        },
+        {
+          $group: {
+            _id: {
+              connected_realm_id: '$connected_realm_id',
+              auctions: '$auctions',
             },
+            name: { $first: '$name' },
           },
-        ])
+        },
+      ])
         .cursor({ batchSize: 5 })
         .eachAsync(async (realm: IAARealm) => {
-          await this.queue.add(
-            `${realm.name}`,
-            {
-              connected_realm_id: realm._id.connected_realm_id,
-              auctions: realm._id.auctions,
-              region: 'eu',
-              clientId: key._id,
-              clientSecret: key.secret,
-              accessToken: key.token
-            }
-          )
+          await this.queue.add(`${realm.name}`, {
+            connected_realm_id: realm._id.connected_realm_id,
+            auctions: realm._id.auctions,
+            region: 'eu',
+            clientId: key._id,
+            clientSecret: key.secret,
+            accessToken: key.token,
+          });
         });
     } catch (errorException) {
-      this.logger.error(`indexAuctions: ${errorException}`)
+      this.logger.error(`indexAuctions: ${errorException}`);
     }
   }
 
@@ -82,21 +84,20 @@ export class AuctionsService implements OnApplicationBootstrap {
     try {
       const key = await this.KeyModel.findOne({ tags: clearance });
       if (!key || !key.token) {
-        this.logger.error(`indexCommodity: clearance: ${clearance} key not found`);
-        return
+        this.logger.error(
+          `indexCommodity: clearance: ${clearance} key not found`,
+        );
+        return;
       }
 
-      await this.queue.add(
-        `COMMDTY`,
-        {
-          region: 'eu',
-          clientId: key._id,
-          clientSecret: key.secret,
-          accessToken: key.token
-        }
-      )
+      await this.queue.add('COMMDTY', {
+        region: 'eu',
+        clientId: key._id,
+        clientSecret: key.secret,
+        accessToken: key.token,
+      });
     } catch (errorException) {
-      this.logger.error(`indexCommodity: ${errorException}`)
+      this.logger.error(`indexCommodity: ${errorException}`);
     }
   }
 }

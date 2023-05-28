e53ad4e6039d953a4f58267a2f7376d2431dc09b
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-05-28 21:14:09 +0300

    feat(libs): implement guid converter

--- libs/core/src/utils/converters.ts
+++ libs/core/src/utils/converters.ts
@@ -1,3 +1,4 @@
+export const toGuild = (name: string, realm: string) => `${name}@${realm}`;
 /**
  * @description Returns capitalized string
  * @param s {string}
@@ -5,7 +6,7 @@
  */
 export const capitalize = (s: string): string => {
   return s.charAt(0).toUpperCase() + s.slice(1);
-}
+};
 
 /**
  * @description returns Uppercased string, with replaces dash for spaces
@@ -14,16 +15,17 @@ export const capitalize = (s: string): string => {
  */
 export const fromSlug = (s: string): string => {
   return s.toString().charAt(0).toUpperCase() + s.slice(1).replace(/-+/g, ' ');
-}
+};
 
 /**
  * @description return number with precision .00
- * @param n {string}
+ * @param n {number}
+ * @param digits {number}
  * @return {string}
  */
-export const round2 = (n: number): number => {
-  return parseFloat(n.toFixed(2));
-}
+export const round = (n: number, digits = 2): number => {
+  return parseFloat(n.toFixed(digits));
+};
 
 /**
  * @description Return force lowercased slug format string
@@ -31,12 +33,8 @@ export const round2 = (n: number): number => {
  * @return {string}
  */
 export const toSlug = (s: string): string => {
-  return s
-    .replace(/\s+/g, '-')
-    .replace(/'+/g, '')
-    .toLowerCase();
-}
-
+  return s.replace(/\s+/g, '-').replace(/'+/g, '').toLowerCase();
+};
 
 /**
  * @description Return force lowercase with underscore format string
@@ -44,31 +42,33 @@ export const toSlug = (s: string): string => {
  * @return {string}
  */
 export const toKey = (s: string): string => {
-  return s
-    .replace(/\s+/g, '_')
-    .replace(/'+/g, '')
-    .toLowerCase();
-}
+  return s.replace(/\s+/g, '_').replace(/'+/g, '').toLowerCase();
+};
 
 /**
  * @description Delay for the selected amount of time in seconds
  * @param seconds {number}
  */
-export const delay = (seconds: number = 5): Promise<void> => {
-  return new Promise((resolve) => { setTimeout(resolve, seconds * 1000); });
-}
+export const delay = (seconds = 5): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
 
 /**
  * @description Return array of unique strings from object keys or enum.
  * @param obj
  */
-export const enumKeys = <O extends object, K extends keyof O = keyof O>(obj: O): K[] => {
-  return Object.keys(obj).filter(k => Number.isNaN(+k)) as K[];
-}
+export const enumKeys = <O extends object, K extends keyof O = keyof O>(
+  obj: O,
+): K[] => {
+  return Object.keys(obj).filter((k) => Number.isNaN(+k)) as K[];
+};
 
 /**
  * @description Return random integer between minimum and maximum value.
  * @param min {number}
  * @param max {number}
  */
-export const randomInt = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1) + min)
+export const randomInt = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min + 1) + min);

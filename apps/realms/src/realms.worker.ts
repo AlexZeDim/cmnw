fc3e972e099fe82282abe00fe6a33d606e9813b7
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-06-21 17:43:36 +0300

    refactor(realms): update realms worker

--- apps/realms/src/realms.worker.ts
+++ apps/realms/src/realms.worker.ts
@@ -70,13 +70,17 @@ export class RealmsWorker {
 
       await job.updateProgress(25);
 
-      realmEntity.name = isFieldNamed(response.name)
+      const name = isFieldNamed(response.name)
         ? response.name
         : get(response, 'name.name', null);
 
-      realmEntity.ticker = REALM_TICKER.has(realmEntity.name)
+      if (name) realmEntity.name = name;
+
+      const ticker = REALM_TICKER.has(realmEntity.name)
         ? REALM_TICKER.get(realmEntity.name)
         : null;
+
+      if (ticker) realmEntity.ticker = ticker;
 
       await job.updateProgress(30);
 
@@ -94,16 +98,26 @@ export class RealmsWorker {
 
         await job.updateProgress(40);
 
-        realmEntity.localeName = get(realmLocale, 'name', null);
-        realmEntity.localeSlug = get(realmLocale, 'name', null);
+        const localeName = get(realmLocale, `name.${realmEntity.locale}`, null);
+        if (localeName) realmEntity.localeName = localeName;
+
+        const localeSlug = get(realmLocale, `slug.${realmEntity.locale}`, null);
+        if (localeSlug) realmEntity.localeSlug = localeSlug;
       } else {
-        realmEntity.localeName = get(response, 'name', null);
-        realmEntity.localeSlug = get(response, 'name', null);
+        const localeNameSlug = get(response, 'name', null);
+        if (localeNameSlug) {
+          realmEntity.localeName = localeNameSlug;
+          realmEntity.localeSlug = localeNameSlug;
+        }
         await job.updateProgress(45);
       }
 
-      realmEntity.region = transformNamedField(response.region);
-      realmEntity.timezone = response.timezone || null;
+      const region = transformNamedField(response.region);
+      if (region) realmEntity.region = region;
+      if (response.timezone) realmEntity.timezone = response.timezone;
+
+      const category = get(response, 'category', null);
+      if (category) realmEntity.category = category;
 
       const connectedRealmId = transformConnectedRealmId(response);
       if (connectedRealmId) {
@@ -119,7 +133,6 @@ export class RealmsWorker {
         realmEntity.connectedRealmId = get(connectedRealm, 'id', null);
         realmEntity.status = get(connectedRealm, 'status.name', null);
         realmEntity.populationStatus = get(connectedRealm, 'population.name', null);
-        realmEntity.category = get(connectedRealm, 'category', null);
         await job.updateProgress(50);
 
         const isRealmsExists =

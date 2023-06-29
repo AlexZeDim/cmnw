6a0f06cb9c44cdcae82438c0bca1d2cde9fd2ada
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-06-30 01:17:43 +0300

    feat(pg): add characters-profile entity

--- libs/pg/src/entity/index.ts
+++ libs/pg/src/entity/index.ts
@@ -1,14 +1,20 @@
+/** OSINT ENTITY */
 export * from './characters.entity';
-export * from './keys.entity';
-export * from './pets.entity';
+export * from './characters-profile.entity';
 export * from './characters-pets.entity';
-export * from './mounts.entity';
-export * from './professions.entity';
 export * from './characters-professions.entity';
-export * from './guilds.entity';
 export * from './characters-guilds-members.entity';
 export * from './characters-mounts.entity';
-export * from './realms.entity';
+
+export * from './guilds.entity';
+
+export * from './pets.entity';
+export * from './mounts.entity';
+export * from './professions.entity';
 export * from './logs.entity';
+/** CORE ENTITY */
+export * from './keys.entity';
+export * from './realms.entity';
+/** DMA ENTITY */
 export * from './market.entity';
 export * from './items.entity';

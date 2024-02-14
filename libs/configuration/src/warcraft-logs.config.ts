01fc739e794eb707b729ff81da0724e25893cbdf
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2024-02-14 10:55:59 +0300

    build(ci/cd): build warcraft-logs-dev image

--- libs/configuration/src/warcraft-logs.config.ts
+++ libs/configuration/src/warcraft-logs.config.ts
@@ -5,8 +5,9 @@ const WARCRAFT_LOGS_CONFIG = config.get<IWarcraftLogs>('warcraft-logs');
 
 export const warcraftLogsConfig: IWarcraftLogs = {
   raidTier: WARCRAFT_LOGS_CONFIG.raidTier,
-  from: WARCRAFT_LOGS_CONFIG.from,
-  to: WARCRAFT_LOGS_CONFIG.to,
+  fromPage: WARCRAFT_LOGS_CONFIG.fromPage,
+  toPage: WARCRAFT_LOGS_CONFIG.toPage,
+  currentPage: WARCRAFT_LOGS_CONFIG.currentPage || 0,
   page: WARCRAFT_LOGS_CONFIG.page,
   logs: WARCRAFT_LOGS_CONFIG.logs,
 };

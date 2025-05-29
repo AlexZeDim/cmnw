b3a57d16ee018663b6e202dd9628df47db539ff7
Sasha <alexzedim@gmail.com>
2025-05-29 14:51:16 +0300

    refactor: port 3001

--- apps/keys/src/main.ts
+++ apps/keys/src/main.ts
@@ -3,6 +3,6 @@ import { KeysModule } from './keys.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(KeysModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();

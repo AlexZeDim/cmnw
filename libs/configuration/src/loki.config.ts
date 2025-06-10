a6054b39e5f0ba3ca077172aa9fa753c9e7b2fb8
Sasha <alexzedim@gmail.com>
2025-06-10 21:48:00 +0300

    feat: update loki configuration for env-based console logging, clean up package.json, and bump version to 6.2.0

--- libs/configuration/src/loki.config.ts
+++ libs/configuration/src/loki.config.ts
@@ -3,6 +3,6 @@ import { ILoki } from '@app/configuration/interfaces';
 export const lokiConfig: ILoki = {
   lokiUrl: process.env.LOKI_URL,
   logToLoki: process.env.NODE_ENV === 'production',
-  logToConsole: true,
+  logToConsole: process.env.NODE_ENV === 'production',
   gzip: false
 };

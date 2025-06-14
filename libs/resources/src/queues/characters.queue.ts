c532d37ede9c20a7d809e076705d572b85f2ea16
Sasha <alexzedim@gmail.com>
2025-06-14 06:05:52 +0300

    refactor: limiter to queue

--- libs/resources/src/queues/characters.queue.ts
+++ libs/resources/src/queues/characters.queue.ts
@@ -10,6 +10,10 @@ export const charactersQueue: IQueue = {
   name: 'OSINT_Characters',
   workerOptions: {
     concurrency: 20,
+    limiter: {
+      max: 75,
+      duration: 1000,
+    },
   },
   defaultJobOptions: options,
 };

07c87cebf12fd6a30e9eeb650a8312055d8cd3d5
Sasha <alexzedim@gmail.com>
2025-06-20 16:07:34 +0300

    refactor: implement `onApplicationBootstrap` lifecycle for gold indexing and refactor `indexGold` for improved entity creation and logging

--- apps/gold/src/gold.service.ts
+++ apps/gold/src/gold.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { from, lastValueFrom } from 'rxjs';
+import { from, lastValueFrom, toArray } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
 import { HttpService } from '@nestjs/axios';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,15 +11,17 @@ import * as cheerio from 'cheerio';
 import {
   DMA_SOURCE_GOLD,
   FACTION,
-  findRealm, GOLD_ITEM_ENTITY,
+  findRealm,
+  GOLD_ITEM_ENTITY,
   IGold,
   isGold,
-  MARKET_TYPE, REALM_ENTITY_ANY,
+  MARKET_TYPE,
+  REALM_ENTITY_ANY,
   round,
 } from '@app/resources';
 
 @Injectable()
-export class GoldService {
+export class GoldService implements OnApplicationBootstrap {
   private readonly logger = new Logger(GoldService.name, { timestamp: true });
 
   constructor(
@@ -29,6 +31,10 @@ export class GoldService {
     @InjectRepository(MarketEntity)
     private readonly marketRepository: Repository<MarketEntity>,
   ) {}
+
+  async onApplicationBootstrap() {
+      await this.indexGold();
+  }
 
   @Cron(CronExpression.EVERY_HOUR)
   private async indexGold(): Promise<void> {
@@ -65,103 +71,117 @@ export class GoldService {
         }),
       );
 
-      await lastValueFrom(
+      console.log(goldOrders);
+
+      const goldMarketEntities = await lastValueFrom(
         from(goldOrders).pipe(
-          mergeMap(async (order) => {
-            try {
-              const realmEntity = realmsEntity.has(order.realm)
-                ? realmsEntity.get(order.realm)
-                : await findRealm(this.realmsRepository, order.realm);
-
-              const connectedRealmId =
-                !realmEntity && order.realm === 'Любой'
-                  ? REALM_ENTITY_ANY.id
-                  : realmEntity
-                  ? realmEntity.connectedRealmId
-                  : 0;
-
-              const isValid = Boolean(
-                connectedRealmId && order.price && order.quantity,
-              );
-
-              if (!isValid) {
-                this.logger.log(order.realm);
-                return;
-              }
-
-              realmsEntity.set(order.realm, realmEntity);
-              connectedRealmIds.add(realmEntity.connectedRealmId);
-
-              const [url, orderId] = order.orderId.split('=') || null;
-              const price = parseFloat(order.price.replace(/ ₽/g, ''));
-              const quantity = parseInt(order.quantity.replace(/\s/g, ''));
-              const counterparty = order.owner.replace('\n', '').trim();
-
-              const isGoldValid = isGold({
-                orderId,
-                price,
-                quantity,
-                counterparty,
-              });
-
-              if (!isGoldValid) return;
-              const value = round(price * (quantity / 1000), 2);
-
-              const isQuantityLimit = quantity > 100_000_000;
-              if (isQuantityLimit) return;
-
-              let faction = FACTION.ANY;
-
-              const isOnline = order.status;
-              const isHorde = [FACTION.H, 'Орда'].includes(order.faction);
-              const isAlliance = [FACTION.A, 'Альянсa', 'Альянс'].includes(
-                order.faction,
-              );
-
-              if (isAlliance) faction = FACTION.A;
-              if (isHorde) faction = FACTION.H;
-
-              const marketEntity = this.marketRepository.create({
-                connectedRealmId,
-                itemId: GOLD_ITEM_ENTITY.id,
-                type: MARKET_TYPE.G,
-                orderId,
-                faction,
-                value,
-                quantity,
-                isOnline,
-                counterparty,
-                price,
-                timestamp,
-              });
-
-              marketOrders.push(marketEntity);
-            } catch (error) {
-              this.logger.error(
-                {
-                  context: 'goldOrders',
-                  error: JSON.stringify(error),
-                }
-              );
-            }
-          }, 5),
+          mergeMap((order) =>
+            this.createMarketEntity(order, realmsEntity, connectedRealmIds, timestamp), 5),
+          toArray()
         ),
       );
 
+      marketOrders.push(...goldMarketEntities);
+
       const ordersCount = marketOrders.length;
-      this.logger.log(
-        `indexGold: ${marketOrders.length} orders on timestamp: ${timestamp} inserted`,
-      );
 
       if (!ordersCount) return;
 
-      await this.marketRepository.save(marketOrders);
+      const marketEntities = await this.marketRepository.save(marketOrders);
+      const marketEntitiesCount = marketEntities.length;
+
+      this.logger.log(
+        `indexGold: ${ordersCount} of ${marketEntitiesCount} orders on timestamp ${timestamp} inserted`,
+      );
+
       await this.realmsRepository.update({}, { goldTimestamp: timestamp });
     } catch (errorOrException) {
       this.logger.error(
         {
           context: 'indexGold',
-          error: JSON.stringify(errorOrException),
+          error: errorOrException,
+        }
+      );
+    }
+  }
+
+  private async createMarketEntity(
+    order: Partial<IGold>,
+    realmsEntity: Map<string, RealmsEntity>,
+    connectedRealmIds: Set<number>,
+    timestamp: number,
+  ): Promise<MarketEntity> {
+    try {
+      const realmEntity = realmsEntity.has(order.realm)
+        ? realmsEntity.get(order.realm)
+        : await findRealm(this.realmsRepository, order.realm);
+
+      const connectedRealmId =
+        !realmEntity && order.realm === 'Любой'
+          ? REALM_ENTITY_ANY.id
+          : realmEntity
+            ? realmEntity.connectedRealmId
+            : 0;
+
+      const isValid = Boolean(
+        connectedRealmId && order.price && order.quantity,
+      );
+
+      if (!isValid) {
+        this.logger.warn(`createMarketEntity: is not valid | realm ${order.realm}`);
+        return;
+      }
+
+      realmsEntity.set(order.realm, realmEntity);
+      connectedRealmIds.add(realmEntity.connectedRealmId);
+
+      const [url, orderId] = order.orderId.split('=') || null;
+      const price = parseFloat(order.price.replace(/ ₽/g, ''));
+      const quantity = parseInt(order.quantity.replace(/\s/g, ''));
+      const counterparty = order.owner.replace('\n', '').trim();
+
+      const isGoldValid = isGold({
+        orderId,
+        price,
+        quantity,
+        counterparty,
+      });
+
+      if (!isGoldValid) return;
+      const value = round(price * (quantity / 1000), 2);
+
+      const isQuantityLimit = quantity > 100_000_000;
+      if (isQuantityLimit) return;
+
+      let faction = FACTION.ANY;
+
+      const isOnline = order.status;
+      const isHorde = [FACTION.H, 'Орда'].includes(order.faction);
+      const isAlliance = [FACTION.A, 'Альянсa', 'Альянс'].includes(
+        order.faction,
+      );
+
+      if (isAlliance) faction = FACTION.A;
+      if (isHorde) faction = FACTION.H;
+
+      return this.marketRepository.create({
+        connectedRealmId,
+        itemId: GOLD_ITEM_ENTITY.id,
+        type: MARKET_TYPE.G,
+        orderId,
+        faction,
+        value,
+        quantity,
+        isOnline,
+        counterparty,
+        price,
+        timestamp,
+      });
+    } catch (error) {
+      this.logger.error(
+        {
+          context: 'createMarketEntity',
+          error: error,
         }
       );
     }
